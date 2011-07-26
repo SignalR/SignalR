74c297da84cbed8f883d760e315645b26f3d79df
damianedwards <damian@damianedwards.com>
2011-07-25 22:49:40 -0700

    added home page from old SignalR project, much more to do yet :)

---
+++ SignalR.Samples/SignalR.Master.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace SignalR.Samples {
+    public partial class SignalRMaster : System.Web.UI.MasterPage {
+        protected void Page_Load(object sender, EventArgs e) {
+
+        }
+    }
+}
---
+++ SignalR.Samples/SignalR.Samples.master.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace SignalR.Samples.SignalRSamples
+{
+	public partial class SignalRSamples : System.Web.UI.MasterPage
+	{
+		protected void Page_Load(object sender, EventArgs e)
+		{
+
+		}
+	}
+}
