908159c6472dbb395644f80969c711a2ddf50e85
David Fowler <davidfowl@gmail.com>
2011-08-01 00:11:30 -0700

    Fixed typo in assembly info

--- SignalR.Client/Properties/AssemblyInfo.cs
+++ SignalR.Client/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("SignalR.Client")]
-[assembly: AssemblyDescription(",NET client for SignalR")]
+[assembly: AssemblyDescription(".NET client for SignalR")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Microsoft")]
 [assembly: AssemblyProduct("SignalR.Client")]
