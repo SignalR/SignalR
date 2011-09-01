ba8d48930e1893a27f5b7b3f0cb58e4075959fab
David Fowler <davidfowl@gmail.com>
2011-08-31 19:52:01 -0700

    Fixed formatting

--- SignalR/Routing/IncomingOnlyRouteConstraint.cs
+++ SignalR/Routing/IncomingOnlyRouteConstraint.cs
@@ -5,14 +5,10 @@ using System.Text;
 using System.Web.Routing;
 using System.Web;
 
-namespace SignalR.Routing
-{
-    public class IncomingOnlyRouteConstraint : IRouteConstraint
-    {
-        public bool Match(HttpContextBase httpContext, Route route, string parameterName, RouteValueDictionary values, RouteDirection routeDirection)
-        {
-            if (routeDirection == RouteDirection.IncomingRequest)
-            {
+namespace SignalR.Routing {
+    public class IncomingOnlyRouteConstraint : IRouteConstraint {
+        public bool Match(HttpContextBase httpContext, Route route, string parameterName, RouteValueDictionary values, RouteDirection routeDirection) {
+            if (routeDirection == RouteDirection.IncomingRequest) {
                 return true;
             }
             return false;
