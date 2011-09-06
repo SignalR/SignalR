d514bfb0791f516fb3a9970b107b74574b1b8c25
Ayende Rahien <ayende@ayende.com>
2011-09-06 10:44:56 +0300

    Better approach (no locking)

--- SignalR/Infrastructure/SafeSet.cs
+++ SignalR/Infrastructure/SafeSet.cs
@@ -1,37 +1,42 @@
-﻿using System.Collections.Generic;
+﻿using System.Collections.Concurrent;
+using System.Collections.Generic;
 using System.Linq;
 
-namespace SignalR.Infrastructure {
-    internal class SafeSet<T> {
-        private readonly HashSet<T> _items = new HashSet<T>();
+namespace SignalR.Infrastructure
+{
+	internal class SafeSet<T>
+	{
+		private readonly ConcurrentDictionary<T, object> _items;
 
-        public SafeSet() {
-        }
+		public SafeSet()
+		{
+			_items = new ConcurrentDictionary<T, object>();
+		}
 
-        public SafeSet(IEqualityComparer<T> comparer) {
-            _items = new HashSet<T>(comparer);
-        }
+		public SafeSet(IEqualityComparer<T> comparer)
+		{
+			_items = new ConcurrentDictionary<T, object>(comparer);
+		}
 
-        public SafeSet(IEnumerable<T> items) {
-            _items = new HashSet<T>(items);
-        }
+		public SafeSet(IEnumerable<T> items)
+		{
+			_items = new ConcurrentDictionary<T, object>(items.Select(x => new KeyValuePair<T, object>(x, null)));
+		}
 
-        public IEnumerable<T> GetSnapshot() {
-            lock (_items) {
-                return _items.ToArray();
-            }
-        }
+		public IEnumerable<T> GetSnapshot()
+		{
+			return _items.Keys;
+		}
 
-        public void Add(T item) {
-            lock (_items) {
-                _items.Add(item);
-            }
-        }
+		public void Add(T item)
+		{
+			_items.TryAdd(item, null);
+		}
 
-        public void Remove(T item) {
-            lock (_items) {
-                _items.Remove(item);
-            }
-        }
-    }
+		public void Remove(T item)
+		{
+			object _;
+			_items.TryRemove(item, out _);
+		}
+	}
 }
