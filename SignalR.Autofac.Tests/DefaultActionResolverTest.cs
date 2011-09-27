444c94b3e73a509961f3318d925baf97c98137fb
Brendan Forster <me@brendanforster.com>
2011-09-28 01:16:39 +1000

    added simple tests for GetService

---
+++ SignalR.Autofac.Tests/DefaultActionResolverTest.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using Xunit;
+using Autofac;
+using SignalR.Autofac;
+
+namespace SignalR.Tests
+{
+    public class DefaultActionResolverTest
+    {
+        [Fact]
+        public void GetService_WithSingleRegistration_ReturnsNotNull()
+        {
+            var builder = new ContainerBuilder();
+            builder.RegisterType<SimpleType>().AsImplementedInterfaces();
+            var container = builder.Build();
+
+            var resolver = new AutofacDependencyResolver(container);
+            var entity = resolver.GetService(typeof(ISomeType));
+
+            Assert.NotNull(entity);
+        }
+
+        [Fact]
+        public void GetService_WithNoRegistration_ReturnsNull()
+        {
+            var builder = new ContainerBuilder();
+            var container = builder.Build();
+
+            var resolver = new AutofacDependencyResolver(container);
+            var entity = resolver.GetService(typeof(ISomeType));
+
+            Assert.Null(entity);
+        }
+    }
+
+    public interface ISomeType { }
+
+    public class SimpleType : ISomeType { }
+}
