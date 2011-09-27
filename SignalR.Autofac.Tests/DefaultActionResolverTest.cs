69a6f31c9414b6c4ebd4e5ff13eaded4db9ae58a
Brendan Forster <me@brendanforster.com>
2011-09-28 01:21:00 +1000

    phew, tests pass

--- SignalR.Autofac.Tests/DefaultActionResolverTest.cs
+++ SignalR.Autofac.Tests/DefaultActionResolverTest.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System.Linq;
+using System.Collections.Generic;
 using Xunit;
 using Autofac;
 using SignalR.Autofac;
@@ -31,9 +32,25 @@ namespace SignalR.Tests
 
             Assert.Null(entity);
         }
+
+        [Fact]
+        public void GetServices_WithTwoRegistration_ReturnsTwoEntities()
+        {
+            var builder = new ContainerBuilder();
+            builder.RegisterType<SimpleType>().AsImplementedInterfaces();
+            builder.RegisterType<AnotherType>().AsImplementedInterfaces();
+            var container = builder.Build();
+
+            var resolver = new AutofacDependencyResolver(container);
+            var result = resolver.GetServices(typeof(ISomeType));
+
+            Assert.True(result.Count() == 2);
+        }
     }
 
     public interface ISomeType { }
 
     public class SimpleType : ISomeType { }
+
+    public class AnotherType : ISomeType { }
 }
