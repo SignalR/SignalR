41be1f77b3bc9527f43edec333f08b9ce80d83ab
David Fowler <davidfowl@gmail.com>
2011-10-09 13:30:45 -0700

    Set BuildPackage flag in project files

--- build.cmd
+++ build.cmd
@@ -3,4 +3,4 @@ set config=%1
 if "%config%" == "" (
    set config=Debug
 )
-%WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild Build\Build.proj /p:Configuration="%config%";BuildPackage=true /m
+%WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild Build\Build.proj /p:Configuration="%config%" /m /v:M /fl /flp:LogFile=msbuild.log;Verbosity=Normal /nr:false
