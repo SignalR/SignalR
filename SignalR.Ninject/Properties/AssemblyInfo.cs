354e7cb313a6a6482ba6dbf4a829638e4d3683a2
David Fowler <davidfowl@gmail.com>
2011-10-09 13:46:28 -0700

    Removed SignalR.Ninject

--- SignalR.Ninject/Properties/AssemblyInfo.cs
+++
@@ -1,7 +0,0 @@
-﻿using System.Reflection;
-
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("SignalR.Ninject")]
-[assembly: AssemblyDescription("A ninject dependency resolver implementation for SignalR.")]
