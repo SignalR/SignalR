16253585fc135abb80738de4e225f79a09d5f2c7
David Fowler <davidfowl@gmail.com>
2011-11-02 07:00:50 -0700

    Make ObservableConnection depend on IConnection instead of Connection.

--- SignalR.Client/ConnectionExtensions.cs
+++ SignalR.Client/ConnectionExtensions.cs
@@ -5,17 +5,17 @@ namespace SignalR.Client
 {
     public static class ConnectionExtensions
     {
-        public static IObservable<string> AsObservable(this Connection connection)
+        public static IObservable<string> AsObservable(this IConnection connection)
         {
             return connection.AsObservable(value => value);
         }
 
-        public static IObservable<T> AsObservable<T>(this Connection connection)
+        public static IObservable<T> AsObservable<T>(this IConnection connection)
         {
             return connection.AsObservable(value => JsonConvert.DeserializeObject<T>(value));
         }
 
-        public static IObservable<T> AsObservable<T>(this Connection connection, Func<string, T> selector)
+        public static IObservable<T> AsObservable<T>(this IConnection connection, Func<string, T> selector)
         {
             return new ObservableConnection<T>(connection, selector);
         }
