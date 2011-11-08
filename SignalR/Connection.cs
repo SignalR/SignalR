SignalR/SignalR

commit a830ce821aab64abf31428acbc447954aa2ee635

Remove timeout info after signal is triggered. Don't return a null persistent response.

SignalR/Connection.cs
@@ -133,25 +133,29 @@ namespace SignalR
         {
             if (signalTask.Result.TimedOut)
             {
-                // If we timed out waiting for a signal we have a message id then return null
-                PersistentResponse response = null;
-
-                // Otherwise ee need to return 0 so that the next request we'll get all messages
-                // on the next try
-                if (messageId == null)
+                var response = new PersistentResponse
                 {
-                    response = new PersistentResponse
-                    {
-                        MessageId = 0
-                    };
-                }
+                    MessageId = messageId ?? 0
+                };
 
                 // Return a task wrapping the result
                 return TaskAsyncHelper.FromResult(response);
             }
 
             // Get the response for this message id
-            return GetResponse(messageId ?? 0);
+            return GetResponse(messageId ?? 0).Success(task =>
+            {
+                if (task.Result == null)
+                {
+                    // If there's no messages an empty response with the message id
+                    return new PersistentResponse
+                    {
+                        MessageId = messageId ?? 0
+                    };
+                }
+
+                return task.Result;
+            });
         }
 
         private Task<PersistentResponse> GetResponse(long messageId)
@@ -163,7 +167,7 @@ namespace SignalR
                     var results = messageTask.Result;
                     if (!results.Any())
                     {
-                        return WaitForSignal(messageId);
+                        return null;
                     }
 
                     var response = new PersistentResponse();
@@ -185,9 +189,8 @@ namespace SignalR
                     // Set the groups on the outgoing transport data
                     response.TransportData["Groups"] = _groups;
 
-                    return TaskAsyncHelper.FromResult(response);
-                })
-                .Unwrap();
+                    return response;
+                });
         }
 
         private void ProcessCommands(IEnumerable<Message> messages)

SignalR/Signaler.cs
@@ -9,10 +9,11 @@ namespace SignalR
 {
     public class Signaler
     {
-        // REVIEW: Should we make this configurable?
-        private static readonly TimeSpan _timeOutInterval = TimeSpan.FromSeconds(10);
+        private static TimeSpan _timeOutInterval;
+        private static TimeSpan _defaultTimeOut;
+
         private static readonly object _timeOutCreationLock = new object();
-        private static readonly SafeSet<TimeoutInfo> _timeOutInfos = new SafeSet<TimeoutInfo>();
+        private static readonly SafeSet<SafeHandleEventAndSetResultAction> _signalActions = new SafeSet<SafeHandleEventAndSetResultAction>();
         private static bool _timeOutCheckRunning;
 
         // Timer that runs on an interval to check for Subscription timeouts
@@ -41,7 +42,26 @@ namespace SignalR
             }
         }
 
-        public TimeSpan DefaultTimeout { get; set; }
+        public TimeSpan DefaultTimeout
+        {
+            get
+            {
+                return _defaultTimeOut;
+            }
+            set
+            {
+                _defaultTimeOut = value;
+
+                // Make the interval run for 1/2 the default timeout
+                _timeOutInterval = TimeSpan.FromSeconds(value.TotalSeconds / 2);
+
+                if (_timeOutTimer != null)
+                {
+                    // TODO: Adjust interval based on all timeouts
+                    _timeOutTimer.Change(TimeSpan.Zero, _timeOutInterval);
+                }
+            }
+        }
 
         public virtual Task Signal(string eventKey)
         {
@@ -82,10 +102,7 @@ namespace SignalR
         {
             var tcs = new TaskCompletionSource<SignalResult>();
 
-            // Make sure the timer that checks for Subscription timeouts is running
-            EnsureTimeoutTimer();
-
-            var signalAction = new SafeHandleEventAndSetResultAction(SignalBus, tcs, eventKeys);
+            var signalAction = new SafeHandleEventAndSetResultAction(SignalBus, tcs, eventKeys, timeout);
 
             foreach (var eventKey in eventKeys)
             {
@@ -97,10 +114,10 @@ namespace SignalR
                 cancellationToken.Register(signalAction.SetCanceled);
             }
 
-            // Create a new timeout info for this event
-            var timeOutInfo = new TimeoutInfo(signalAction, DateTime.UtcNow, timeout);
+            _signalActions.Add(signalAction);
 
-            _timeOutInfos.Add(timeOutInfo);
+            // Make sure the timer that checks for Subscription timeouts is running
+            EnsureTimeoutTimer();
 
             return tcs.Task;
         }
@@ -113,7 +130,7 @@ namespace SignalR
                 {
                     if (_timeOutTimer == null)
                     {
-                        _timeOutTimer = new Timer(_ => CheckTimeouts(), null, TimeSpan.Zero, _timeOutInterval);
+                        _timeOutTimer = new Timer(_ => CheckTimeouts(), null, _timeOutInterval, _timeOutInterval);
                     }
                 }
             }
@@ -128,15 +145,12 @@ namespace SignalR
 
             _timeOutCheckRunning = true;
 
-            foreach (TimeoutInfo timeoutInfo in _timeOutInfos.GetSnapshot())
+            foreach (var signalAction in _signalActions.GetSnapshot())
             {
-                if (timeoutInfo.TimedOut)
+                if (signalAction.TimeoutInfo.TimedOut)
                 {
                     // If we timed out the call the SetTimedOut method to complete the task
-                    timeoutInfo.SignalAction.SetTimedOut();
-
-                    // Remove this timeout info from the list
-                    _timeOutInfos.Remove(timeoutInfo);
+                    signalAction.SetTimedOut();
                 }
             }
 
@@ -146,20 +160,20 @@ namespace SignalR
         private class TimeoutInfo
         {
             private readonly DateTime _subscriptionTime;
-            private readonly TimeSpan _timeout;
+            private readonly TimeSpan _timeOut;
 
             public TimeoutInfo(SafeHandleEventAndSetResultAction signalAction,
                                DateTime subscriptionTime,
                                TimeSpan timeout)
             {
                 SignalAction = signalAction;
+                _timeOut = timeout;
                 _subscriptionTime = subscriptionTime;
-                _timeout = timeout;
             }
 
             public SafeHandleEventAndSetResultAction SignalAction { get; private set; }
 
-            private TimeSpan Elapsed
+            public TimeSpan Elapsed
             {
                 get
                 {
@@ -171,7 +185,7 @@ namespace SignalR
             {
                 get
                 {
-                    return Elapsed > _timeout;
+                    return Elapsed >= _timeOut;
                 }
             }
         }
@@ -185,9 +199,13 @@ namespace SignalR
             private bool _timedOut;
             private bool _handlerCalled;
 
-            public SafeHandleEventAndSetResultAction(ISignalBus signalBus, TaskCompletionSource<SignalResult> tcs, IEnumerable<string> eventKeys)
+            public SafeHandleEventAndSetResultAction(ISignalBus signalBus,
+                                                     TaskCompletionSource<SignalResult> tcs,
+                                                     IEnumerable<string> eventKeys,
+                                                     TimeSpan timeout)
             {
                 _locker = new object();
+                TimeoutInfo = new TimeoutInfo(this, DateTime.UtcNow, timeout);
                 Handler = (sender, args) =>
                 {
                     SafeHandleEventAndSetResult(args.EventKey);
@@ -198,6 +216,8 @@ namespace SignalR
             }
 
             public EventHandler<SignaledEventArgs> Handler { get; private set; }
+
+            public TimeoutInfo TimeoutInfo { get; private set; }
 
             private TaskCompletionSource<SignalResult> Tcs { get; set; }
 
@@ -223,8 +243,14 @@ namespace SignalR
                     }
                     else
                     {
-                        Tcs.SetResult(new SignalResult { TimedOut = _timedOut, EventKey = signaledEventKey });
+                        Tcs.SetResult(new SignalResult
+                        {
+                            TimedOut = _timedOut,
+                            EventKey = signaledEventKey
+                        });
                     }
+
+                    _signalActions.Remove(this);
                 }
             }
 

commit f54352d32ec3790ac113a8c13839e9d65a067fee

Fixed issues caused by a830ce821aab64a. - Populate transport data before sending PersistentResponse back.

SignalR/Connection.cs
@@ -133,29 +133,26 @@ namespace SignalR
         {
             if (signalTask.Result.TimedOut)
             {
-                var response = new PersistentResponse
-                {
-                    MessageId = messageId ?? 0
-                };
+                PersistentResponse response = GetEmptyResponse(messageId);
 
                 // Return a task wrapping the result
                 return TaskAsyncHelper.FromResult(response);
             }
 
             // Get the response for this message id
-            return GetResponse(messageId ?? 0).Success(task =>
-            {
-                if (task.Result == null)
-                {
-                    // If there's no messages an empty response with the message id
-                    return new PersistentResponse
-                    {
-                        MessageId = messageId ?? 0
-                    };
-                }
+            return GetResponse(messageId ?? 0).Success(task => task.Result ?? GetEmptyResponse(messageId));
+        }
 
-                return task.Result;
-            });
+        private PersistentResponse GetEmptyResponse(long? messageId)
+        {
+            var response = new PersistentResponse
+            {
+                MessageId = messageId ?? 0
+            };
+
+            PopulateResponseState(response);
+
+            return response;
         }
 
         private Task<PersistentResponse> GetResponse(long messageId)
@@ -186,8 +183,7 @@ namespace SignalR
                     response.MessageId = messageId;
                     response.Messages = messageValues;
 
-                    // Set the groups on the outgoing transport data
-                    response.TransportData["Groups"] = _groups;
+                    PopulateResponseState(response);
 
                     return response;
                 });
@@ -237,6 +233,12 @@ namespace SignalR
 
             // Wait until all of the tasks are done before we return
             return pendingMessagesTasks.AllSucceeded(() => (IEnumerable<Message>)pendingMessagesTasks.SelectMany(t => t.Result).OrderBy(m => m.Id).ToList());
+        }
+
+        private void PopulateResponseState(PersistentResponse response)
+        {
+            // Set the groups on the outgoing transport data
+            response.TransportData["Groups"] = _groups;
         }
     }
 }