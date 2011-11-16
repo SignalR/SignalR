SignalR/SignalR

commit b953b2334ebc2d67722d1b7ddec86b1c92a766de

Deleted RemoveConnection from ITransportHeartBeat as it's only used internally in TransportHeartBeat and doesn't need to be called from elsewhere. Made TransportHeatBeat.Beat re-entrant and trace out if it gets called while previous iteration is still running

SignalR/Transports/TransportHeartBeat.cs
@@ -15,6 +15,7 @@ namespace SignalR.Transports
         private readonly ConcurrentDictionary<ITrackingDisconnect, DateTime> _connectionMetadata = new ConcurrentDictionary<ITrackingDisconnect, DateTime>(new ClientIdEqualityComparer());
         private readonly Timer _timer;
         private TimeSpan _heartBeatInterval;
+        private bool _running;
 
         private TransportHeartBeat()
         {
@@ -63,7 +64,7 @@ namespace SignalR.Transports
             _connectionMetadata.TryRemove(connection, out removed);
         }
 
-        public void RemoveConnection(ITrackingDisconnect connection)
+        private void RemoveConnection(ITrackingDisconnect connection)
         {
             // Remove the connection and associated metadata
             _connections.Remove(connection);
@@ -79,12 +80,22 @@ namespace SignalR.Transports
 
         private void Beat()
         {
+            if (_running)
+            {
+                Debug.WriteLine("SIGNALR: TransportHeatBeat timer handler took longer than current interval");
+                return;
+            }
+
+            _running = true;
             try
             {
                 Parallel.ForEach(_connections.GetSnapshot(), (connection) =>
                 {
                     if (!connection.IsAlive)
                     {
+                        // The transport is currently disconnected, it could just be reconnecting though
+                        // so we need to check it's last active time to see if it's over the disconnect
+                        // threshold
                         DateTime lastUsed;
                         if (_connectionMetadata.TryGetValue(connection, out lastUsed))
                         {
@@ -92,6 +103,8 @@ namespace SignalR.Transports
                             var elapsed = DateTime.UtcNow - lastUsed;
 
                             // The threshold for disconnect is the long poll delay + (potential network issues)
+                            // TODO: Refactor to not take a dependency on LongPollingTransport here, might want to
+                            //       re-use this for other transports, e.g. ForeverTransport
                             var threshold = TimeSpan.FromMilliseconds(LongPollingTransport.LongPollDelay) +
                                             DisconnectTimeout;
 
@@ -117,6 +130,7 @@ namespace SignalR.Transports
             {
                 Trace.TraceError("SignalR error during transport heart beat: {0}", ex);
             }
+            _running = true;
         }
 
         private class ClientIdEqualityComparer : IEqualityComparer<ITrackingDisconnect>

.gitignore
@@ -17,3 +17,4 @@ packages/*
 Kudu.Services.Web/App_Data/*
 artifacts/*
 msbuild.log
+PublishProfiles/

SignalR/Transports/ITransportHeartBeat.cs
@@ -3,7 +3,6 @@
     public interface ITransportHeartBeat
     {
         void AddConnection(ITrackingDisconnect connection);
-        void RemoveConnection(ITrackingDisconnect connection);
         void MarkConnection(ITrackingDisconnect connection);
     }
 }

commit fb0c2137c0cf49c710d11bb4c8fd7bd6d4f739d5

Fix bug in disconnect logic caused by b953b2334e.

SignalR/Transports/TransportHeartBeat.cs
@@ -87,6 +87,7 @@ namespace SignalR.Transports
             }
 
             _running = true;
+
             try
             {
                 Parallel.ForEach(_connections.GetSnapshot(), (connection) =>
@@ -130,7 +131,8 @@ namespace SignalR.Transports
             {
                 Trace.TraceError("SignalR error during transport heart beat: {0}", ex);
             }
-            _running = true;
+
+            _running = false;
         }
 
         private class ClientIdEqualityComparer : IEqualityComparer<ITrackingDisconnect>