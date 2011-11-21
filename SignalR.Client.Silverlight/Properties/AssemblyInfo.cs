cb937d6b9e081e79abe964d1540c32a666121fb0
Einar Ingebrigtsen <einar@dolittle.com>
2011-11-21 12:54:58 +0100

    Added CommonAssemblyInfo into the Silverlight project - updating the generated AssemblyInfo with the only things needed

--- SignalR.Client.Silverlight/Properties/AssemblyInfo.cs
+++ SignalR.Client.Silverlight/Properties/AssemblyInfo.cs
@@ -1,35 +1,5 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
+
 [assembly: AssemblyTitle("SignalR.Client.Silverlight")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("SignalR.Client.Silverlight")]
-[assembly: AssemblyCopyright("Copyright ©  2011")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("31b30afd-7462-44c3-8e94-91b0a11ed548")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Revision and Build Numbers 
-// by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyDescription("Silverlight client for SignalR")]
