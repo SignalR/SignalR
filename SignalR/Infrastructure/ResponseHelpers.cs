358d9b2e2251af8e14f1a2e64dd8c374c2f8ff2f
damianedwards <damian@damianedwards.com>
2011-12-13 15:59:56 -0800

    Changed Response.WriteAsync to actually write sync again due to unobserved exception leaking.

--- SignalR/Infrastructure/ResponseHelpers.cs
+++ SignalR/Infrastructure/ResponseHelpers.cs
@@ -1,6 +1,7 @@
 ﻿using System.Text;
 using System.Threading.Tasks;
 using System.Web;
+using System;
 
 namespace SignalR.Infrastructure
 {
@@ -9,8 +10,9 @@ namespace SignalR.Infrastructure
         public static Task WriteAsync(this HttpResponseBase response, string s)
         {
             // The OutputStream on HttpResponse/HttpReponseBase does not implement a true async write,
-            // so we're just going to do it on a new Task here.
-            return Task.Factory.StartNew(() => response.Write(s));
+            // so we're just going to do it sync and return an Empty task here.
+            response.Write(s);
+            return TaskAsyncHelper.Empty;
         }
     }
 }
