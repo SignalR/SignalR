8edf5461b7c4c21e92efc0cca7e468cc46e63e50
David Fowler <davidfowl@gmail.com>
2012-01-07 09:16:40 -0800

    Added SignalR self hosting samples

---
+++ SignalR.SelfHost.Samples/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace SignalR.SelfHost.Samples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Debug.Listeners.Add(new ConsoleTraceListener());
+            Debug.AutoFlush = true;
+
+            string url = "http://localhost:8081/";
+            var server = new Server(url);
+
+            // Map /echo to the persistent connection
+            server.MapConnection<MyConnection>("/echo");
+
+            // Enable the hubs route (/signalr)
+            server.EnableHubs();
+
+            server.Start();
+
+            Console.WriteLine("Server running on {0}", url);
+
+            Console.ReadKey();
+        }
+
+        public class MyConnection : PersistentConnection
+        {
+            protected override Task OnReceivedAsync(string connectionId, string data)
+            {
+                return Connection.Broadcast(data);
+            }
+        }
+    }
+}
