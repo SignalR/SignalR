b3d62ba5ff37f05f14cbe52aa18f9133c44b96ab
Greg Shackles <gshackles@gmail.com>
2012-01-07 21:03:12 -0500

    Adding license for TaskExtensions Fixing TaskAsyncHelper for Android Renaming MonoTouch solution to match others

--- SignalR/TaskAsyncHelper.cs
+++ SignalR/TaskAsyncHelper.cs
@@ -31,7 +31,7 @@ namespace SignalR
                 {
                     var ex = innerTask.Exception;
                     // observe Exception
-#if !WINDOWS_PHONE && !SILVERLIGHT && !__MONOTOUCH__
+#if !WINDOWS_PHONE && !SILVERLIGHT && !__ANDROID__ && !__MONOTOUCH__
                     Trace.TraceError("SignalR exception thrown by Task: {0}", ex);
 #endif
                 }, TaskContinuationOptions.OnlyOnFaulted);
