777fc0332b41c99f9f70e86241a54e981b415a5a
David Fowler <davidfowl@gmail.com>
2012-01-13 00:39:33 -0800

    Added WP7 sample.

---
+++ SignalR.Client.WP7.Sample/MainPage.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using Microsoft.Phone.Controls;
+using SignalR.Client.Transports;
+
+namespace SignalR.Client.WP7.Sample
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+
+            // Set the data context of the listbox control to the sample data
+            DataContext = App.ViewModel;
+            this.Loaded += new RoutedEventHandler(MainPage_Loaded);
+
+            var connection = new Connection("http://localhost:40476/Streaming/streaming");
+            connection.Received += data =>
+            {
+                Dispatcher.BeginInvoke(() =>
+                {
+                    App.ViewModel.Items.Add(new ItemViewModel { LineOne = data });
+                });
+            };
+
+            connection.Start(Transport.LongPolling).ContinueWith(task =>
+            {
+                Debug.WriteLine("ERROR: {0}", task.Exception.GetBaseException().Message);
+            }, 
+            TaskContinuationOptions.OnlyOnFaulted);
+        }
+
+        // Handle selection changed on ListBox
+        private void MainListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            // If selected index is -1 (no selection) do nothing
+            if (MainListBox.SelectedIndex == -1)
+                return;
+
+            // Navigate to the new page
+            NavigationService.Navigate(new Uri("/DetailsPage.xaml?selectedItem=" + MainListBox.SelectedIndex, UriKind.Relative));
+
+            // Reset selected index to -1 (no selection)
+            MainListBox.SelectedIndex = -1;
+        }
+
+        // Load data for the ViewModel Items
+        private void MainPage_Loaded(object sender, RoutedEventArgs e)
+        {
+            
+        }
+    }
+}
