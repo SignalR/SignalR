a85c0a57f90698626ce526439d4181e8888410a6
David Fowler <davidfowl@gmail.com>
2012-01-18 02:49:54 -0800

    Go back to a handlers per client per signal.

--- SignalR/InProcessMessageBus.cs
+++ SignalR/InProcessMessageBus.cs
@@ -3,6 +3,7 @@ using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using SignalR.Infrastructure;
 
@@ -12,8 +13,8 @@ namespace SignalR
     {
         private static List<InProcessMessage> _emptyMessageList = new List<InProcessMessage>();
 
-        private readonly ConcurrentDictionary<string, TaskCompletionSource<IEnumerable<InProcessMessage>>> _waitingTasks =
-            new ConcurrentDictionary<string, TaskCompletionSource<IEnumerable<InProcessMessage>>>();
+        private readonly ConcurrentDictionary<string, LockedList<Action<IEnumerable<Message>>>> _waitingTasks =
+            new ConcurrentDictionary<string, LockedList<Action<IEnumerable<Message>>>>();
 
         private readonly ConcurrentDictionary<string, LockedList<InProcessMessage>> _cache =
             new ConcurrentDictionary<string, LockedList<InProcessMessage>>();
@@ -26,7 +27,7 @@ namespace SignalR
         {
             if (id == null)
             {
-                // wait
+                // Wait for new messages
                 return WaitForMessages(eventKeys);
             }
 
@@ -36,8 +37,8 @@ namespace SignalR
                 // Messages already in store greater than last received id so return them
                 return TaskAsyncHelper.FromResult((IEnumerable<Message>)messages.OrderBy(msg => msg.MessageId));
             }
-            
-            // wait
+
+            // Wait for new messages
             return WaitForMessages(eventKeys);
         }
 
@@ -45,27 +46,32 @@ namespace SignalR
         {
             var list = _cache.GetOrAdd(eventKey, _ => new LockedList<InProcessMessage>());
 
+            InProcessMessage message = null;
             lock (_messageCreationLock)
             {
                 // Only 1 save allowed at a time, to ensure messages are added to the list in order
-                var message = new InProcessMessage(eventKey, GenerateId(), value);
+                message = new InProcessMessage(eventKey, GenerateId(), value);
                 list.Add(message);
+            }
 
-                // Send to waiting callers
-                TaskCompletionSource<IEnumerable<InProcessMessage>> tcs;
-                if (_waitingTasks.TryRemove(eventKey, out tcs))
+            // Send to waiting callers
+            LockedList<Action<IEnumerable<Message>>> taskCompletionSources;
+            if (_waitingTasks.TryGetValue(eventKey, out taskCompletionSources))
+            {
+                var delegates = taskCompletionSources.Copy();
+                var messages = new[] { message };
+
+                foreach (var callback in delegates)
                 {
-                    tcs.SetResult(new[] { message });
+                    if (callback != null)
+                    {
+                        callback.Invoke(messages);
+                    }
                 }
             }
 
             return TaskAsyncHelper.Empty;
         }
-
-        //public Task Send(string eventKey, IEnumerable<string> value)
-        //{
-
-        //}
 
         private ulong GenerateId()
         {
@@ -112,8 +118,34 @@ namespace SignalR
 
         private Task<IEnumerable<Message>> WaitForMessages(IEnumerable<string> eventKeys)
         {
-            var tasks = eventKeys.Select(key => _waitingTasks.GetOrAdd(key, _ => new TaskCompletionSource<IEnumerable<InProcessMessage>>()).Task);
-            return Task.Factory.ContinueWhenAny(tasks.ToArray(), t => (IEnumerable<Message>)t.Result);
+            var tcs = new TaskCompletionSource<IEnumerable<Message>>();
+            int callbackCalled = 0;
+            Action<IEnumerable<Message>> callback = null;
+
+            callback = messages =>
+            {
+                if (Interlocked.Exchange(ref callbackCalled, 1) == 0)
+                {
+                    tcs.SetResult(messages);
+                }
+
+                foreach (var eventKey in eventKeys)
+                {
+                    LockedList<Action<IEnumerable<Message>>> callbacks;
+                    if (_waitingTasks.TryGetValue(eventKey, out callbacks))
+                    {
+                        callbacks.Remove(callback);
+                    }
+                }
+            };
+
+            foreach (var eventKey in eventKeys)
+            {
+                var handlers = _waitingTasks.GetOrAdd(eventKey, _ => new LockedList<Action<IEnumerable<Message>>>());
+                handlers.Add(callback);
+            }
+
+            return tcs.Task;
         }
 
         private class InProcessMessage : Message
