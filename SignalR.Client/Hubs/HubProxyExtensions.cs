8a51be83563dd191c0377772c0e553e3b5ad1b9b
David Fowler <davidfowl@gmail.com>
2012-02-05 02:46:26 -0800

    Make On extension methods return IDisposable to handle event un-registration.

--- SignalR.Client/Hubs/HubProxyExtensions.cs
+++ SignalR.Client/Hubs/HubProxyExtensions.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
+using SignalR.Client.Infrastructure;
 
 namespace SignalR.Client.Hubs
 {
@@ -12,57 +12,70 @@ namespace SignalR.Client.Hubs
             return Convert<T>(value);
         }
 
-        public static Subscription On(this IHubProxy proxy, string eventName, Action onData)
+        public static IDisposable On(this IHubProxy proxy, string eventName, Action onData)
         {
             Subscription subscription = proxy.Subscribe(eventName);
-            subscription.Data += args =>
+
+            Action<object[]> handler = args =>
             {
                 onData();
             };
 
-            return subscription;
+            subscription.Data += handler;
+
+            return new DisposableAction(() => subscription.Data -= handler);
         }
 
-        public static Subscription On<T>(this IHubProxy proxy, string eventName, Action<T> onData)
+        public static IDisposable On<T>(this IHubProxy proxy, string eventName, Action<T> onData)
         {
             Subscription subscription = proxy.Subscribe(eventName);
-            subscription.Data += args =>
+
+            Action<object[]> handler = args =>
             {
                 onData(Convert<T>(args[0]));
             };
 
-            return subscription;
+            subscription.Data += handler;
+
+            return new DisposableAction(() => subscription.Data -= handler);
         }
 
-        public static Subscription On<T1, T2>(this IHubProxy proxy, string eventName, Action<T1, T2> onData)
+        public static IDisposable On<T1, T2>(this IHubProxy proxy, string eventName, Action<T1, T2> onData)
         {
             Subscription subscription = proxy.Subscribe(eventName);
-            subscription.Data += args =>
+
+            Action<object[]> handler = args =>
             {
                 onData(Convert<T1>(args[0]),
                        Convert<T2>(args[1]));
             };
 
-            return subscription;
+            subscription.Data += handler;
+
+            return new DisposableAction(() => subscription.Data -= handler);
         }
 
-        public static Subscription On<T1, T2, T3>(this IHubProxy proxy, string eventName, Action<T1, T2, T3> onData)
+        public static IDisposable On<T1, T2, T3>(this IHubProxy proxy, string eventName, Action<T1, T2, T3> onData)
         {
             Subscription subscription = proxy.Subscribe(eventName);
-            subscription.Data += args =>
+
+            Action<object[]> handler = args =>
             {
                 onData(Convert<T1>(args[0]),
                        Convert<T2>(args[1]),
                        Convert<T3>(args[2]));
             };
 
-            return subscription;
+            subscription.Data += handler;
+
+            return new DisposableAction(() => subscription.Data -= handler);
         }
 
-        public static Subscription On<T1, T2, T3, T4>(this IHubProxy proxy, string eventName, Action<T1, T2, T3, T4> onData)
+        public static IDisposable On<T1, T2, T3, T4>(this IHubProxy proxy, string eventName, Action<T1, T2, T3, T4> onData)
         {
             Subscription subscription = proxy.Subscribe(eventName);
-            subscription.Data += args =>
+
+            Action<object[]> handler = args =>
             {
                 onData(Convert<T1>(args[0]),
                        Convert<T2>(args[1]),
@@ -70,19 +83,22 @@ namespace SignalR.Client.Hubs
                        Convert<T4>(args[3]));
             };
 
-            return subscription;
+            subscription.Data += handler;
+
+            return new DisposableAction(() => subscription.Data -= handler);
         }
 
 #if !WINDOWS_PHONE && !SILVERLIGHT
-        public static Subscription On(this IHubProxy proxy, string eventName, Action<dynamic> onData)
+        public static IDisposable On(this IHubProxy proxy, string eventName, Action<dynamic> onData)
         {
             return On<dynamic>(proxy, eventName, onData);
         }
 
-        public static Subscription On<T1, T2, T3, T4, T5>(this IHubProxy proxy, string eventName, Action<T1, T2, T3, T4, T5> onData)
+        public static IDisposable On<T1, T2, T3, T4, T5>(this IHubProxy proxy, string eventName, Action<T1, T2, T3, T4, T5> onData)
         {
             Subscription subscription = proxy.Subscribe(eventName);
-            subscription.Data += args =>
+
+            Action<object[]> handler = args =>
             {
                 onData(Convert<T1>(args[0]),
                        Convert<T2>(args[1]),
@@ -91,13 +107,16 @@ namespace SignalR.Client.Hubs
                        Convert<T5>(args[4]));
             };
 
-            return subscription;
+            subscription.Data += handler;
+
+            return new DisposableAction(() => subscription.Data -= handler);
         }
 
-        public static Subscription On<T1, T2, T3, T4, T5, T6>(this IHubProxy proxy, string eventName, Action<T1, T2, T3, T4, T5, T6> onData)
+        public static IDisposable On<T1, T2, T3, T4, T5, T6>(this IHubProxy proxy, string eventName, Action<T1, T2, T3, T4, T5, T6> onData)
         {
             Subscription subscription = proxy.Subscribe(eventName);
-            subscription.Data += args =>
+
+            Action<object[]> handler = args =>
             {
                 onData(Convert<T1>(args[0]),
                        Convert<T2>(args[1]),
@@ -107,13 +126,16 @@ namespace SignalR.Client.Hubs
                        Convert<T6>(args[5]));
             };
 
-            return subscription;
+            subscription.Data += handler;
+
+            return new DisposableAction(() => subscription.Data -= handler);
         }
 
-        public static Subscription On<T1, T2, T3, T4, T5, T6, T7>(this IHubProxy proxy, string eventName, Action<T1, T2, T3, T4, T5, T6, T7> onData)
+        public static IDisposable On<T1, T2, T3, T4, T5, T6, T7>(this IHubProxy proxy, string eventName, Action<T1, T2, T3, T4, T5, T6, T7> onData)
         {
             Subscription subscription = proxy.Subscribe(eventName);
-            subscription.Data += args =>
+
+            Action<object[]> handler = args =>
             {
                 onData(Convert<T1>(args[0]),
                        Convert<T2>(args[1]),
@@ -124,7 +146,9 @@ namespace SignalR.Client.Hubs
                        Convert<T7>(args[6]));
             };
 
-            return subscription;
+            subscription.Data += handler;
+
+            return new DisposableAction(() => subscription.Data -= handler);
         }
 
         public static IObservable<object[]> Observe(this IHubProxy proxy, string eventName)
