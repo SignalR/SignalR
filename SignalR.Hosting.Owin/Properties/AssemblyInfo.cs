1dcf5c164d007ec2483bc56be41d521e8e75170d
David Fowler <davidfowl@gmail.com>
2012-02-06 19:31:08 -0800

    Include owin in packages.

--- SignalR.Hosting.Owin/Properties/AssemblyInfo.cs
+++ SignalR.Hosting.Owin/Properties/AssemblyInfo.cs
@@ -1,12 +1,6 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
+
 [assembly: AssemblyTitle("SignalR.Owin")]
 [assembly: AssemblyDescription("Owin host for SignalR")]
-
-// Synced with OWIN version
-[assembly: AssemblyVersion("0.2.1")]
+[assembly: AssemblyVersion("0.4.0.0")]
