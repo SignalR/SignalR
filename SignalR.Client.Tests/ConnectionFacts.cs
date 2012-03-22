ba23d922c7fa0eb32f090513a21a94f04cbd2d4d
David Fowler <davidfowl@gmail.com>
2012-03-22 02:56:21 -0700

    - Made ChunkBuffer thread safe. - Added tests for ChunkBuffer as well as one for sending big data.
    Fixes #256.

--- SignalR.Client.Tests/ConnectionFacts.cs
+++ SignalR.Client.Tests/ConnectionFacts.cs
@@ -1,6 +1,9 @@
 ﻿using System;
+using System.Linq;
+using System.Threading;
 using Moq;
 using SignalR.Client.Transports;
+using SignalR.Hosting.Memory;
 using Xunit;
 
 namespace SignalR.Client.Tests
@@ -23,6 +26,59 @@ namespace SignalR.Client.Tests
                 var ex = aggEx.GetBaseException();
                 Assert.IsType(typeof(InvalidOperationException), ex);
                 Assert.Equal("Incompatible protocol version.", ex.Message);
+            }
+        }
+
+        public class Received
+        {
+            [Fact]
+            public void SendingBigData()
+            {
+                var host = new MemoryHost();
+                host.MapConnection<SampleConnection>("/echo");
+
+                var connection = new Connection("http://foo/echo");
+
+                var wh = new ManualResetEventSlim();
+                var n = 0;
+                var target = 20;
+
+                connection.Received += data =>
+                {
+                    n++;
+                    if (n == target)
+                    {
+                        wh.Set();
+                    }
+                };
+
+                connection.Start(host).Wait();
+
+                var conn = host.ConnectionManager.GetConnection<SampleConnection>();
+
+                for (int i = 0; i < target; ++i)
+                {
+                    var node = new BigData();
+                    conn.Broadcast(node).Wait();
+                    Thread.Sleep(1000);
+                }
+
+                Assert.True(wh.Wait(TimeSpan.FromMinutes(1)), "Timed out");
+            }
+
+            public class BigData
+            {
+                public string[] Dummy
+                {
+                    get
+                    {
+                        return Enumerable.Range(0, 1000).Select(x => new String('*', 500)).ToArray();
+                    }
+                }
+            }
+
+            public class SampleConnection : PersistentConnection
+            {
             }
         }
     }
