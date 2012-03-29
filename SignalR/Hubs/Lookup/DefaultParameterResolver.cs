13ff714cf6a3eb4e98f1b2d64b864417965736ec
David Fowler <davidfowl@gmail.com>
2012-03-29 09:23:06 -0700

    Minimize parameter resolver interface.

--- SignalR/Hubs/Lookup/DefaultParameterResolver.cs
+++ SignalR/Hubs/Lookup/DefaultParameterResolver.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Linq;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
@@ -13,36 +12,19 @@ namespace SignalR.Hubs
         /// <param name="descriptor">Parameter descriptor.</param>
         /// <param name="value">Value to resolve the parameter value from.</param>
         /// <returns>The parameter value.</returns>
-        public virtual object ResolveParameter(ParameterDescriptor descriptor, object value)
+        public virtual object ResolveParameter(ParameterDescriptor descriptor, JToken value)
         {
-            if (value == null)
-            {
-                if (TypeAllowsNull(descriptor.Type))
-                {
-                    return null;
-                }
-
-                throw new InvalidOperationException(String.Format("Unable to resolve value for parameter '{0}'.", descriptor.Name));
-            }
-
-            var token = value as JToken;
-
-            if (token != null)
-            {
-                // A non generic implementation of ToObject<T> on JToken
-                using (var jsonReader = new JTokenReader(token))
-                {
-                    var serializer = new JsonSerializer();
-                    return serializer.Deserialize(jsonReader, descriptor.Type);
-                }
-            }
-
             if (value.GetType() == descriptor.Type)
             {
                 return value;
             }
 
-            return Convert.ChangeType(value, descriptor.Type);
+            // A non generic implementation of ToObject<T> on JToken
+            using (var jsonReader = new JTokenReader(value))
+            {
+                var serializer = new JsonSerializer();
+                return serializer.Deserialize(jsonReader, descriptor.Type);
+            }
         }
 
         /// <summary>
@@ -51,16 +33,11 @@ namespace SignalR.Hubs
         /// <param name="method">Method descriptor.</param>
         /// <param name="values">List of values to resolve parameter values from.</param>
         /// <returns>Array of parameter values.</returns>
-        public virtual object[] ResolveMethodParameters(MethodDescriptor method, params object[] values)
+        public virtual object[] ResolveMethodParameters(MethodDescriptor method, params JToken[] values)
         {
             return method.Parameters
                 .Select((p, index) => ResolveParameter(p, values[index]))
                 .ToArray();
-        }
-
-        private bool TypeAllowsNull(Type type)
-        {
-            return Nullable.GetUnderlyingType(type) != null || !type.IsValueType;
         }
     }
 }
--- SignalR/Hubs/Lookup/IParameterResolver.cs
+++ SignalR/Hubs/Lookup/IParameterResolver.cs
@@ -1,4 +1,6 @@
-﻿namespace SignalR.Hubs
+﻿using Newtonsoft.Json.Linq;
+
+namespace SignalR.Hubs
 {
     /// <summary>
     /// Describes a parameter resolver for resolving parameter-matching values based on provided information.
@@ -6,19 +8,11 @@
     public interface IParameterResolver
     {
         /// <summary>
-        /// Resolves a parameter value based on the provided object.
-        /// </summary>
-        /// <param name="descriptor">Parameter descriptor.</param>
-        /// <param name="value">Value to resolve the parameter value from.</param>
-        /// <returns>The parameter value.</returns>
-        object ResolveParameter(ParameterDescriptor descriptor, object value);
-
-        /// <summary>
         /// Resolves method parameter values based on provided objects.
         /// </summary>
         /// <param name="method">Method descriptor.</param>
         /// <param name="values">List of values to resolve parameter values from.</param>
         /// <returns>Array of parameter values.</returns>
-        object[] ResolveMethodParameters(MethodDescriptor method, params object[] values);
+        object[] ResolveMethodParameters(MethodDescriptor method, params JToken[] values);
     }
 }
