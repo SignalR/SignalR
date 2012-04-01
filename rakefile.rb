78be3c30250a69c6935403ba309a360678f713d6
Einar Ingebrigtsen <einar@dolittle.com>
2012-04-01 17:11:28 +0200

    Modified Samples project to support both Mono and .net
    On Mono on non-windows, it deletes the Microsoft.Web.Infrastructure
    assembly
    Also introducing a rake file for recovering Nuget packages.

--- rakefile.rb
+++ rakefile.rb
@@ -1,5 +1,5 @@
 task :default do
-  Dir["*/"].map do |dir|
+  Dir["*/"]+Dir["samples/*/"].map do |dir|
     puts "Restore packages for '#{dir}'"
     sh "mono --runtime='v4.0' .nuget/NuGet.exe install #{dir}/packages.config -o 'packages'" if File::exists?("#{dir}/packages.config")
   end
