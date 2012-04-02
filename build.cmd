96738770e611cac3ad6be24565ed3d1b21c74580
David Fowler <davidfowl@gmail.com>
2012-04-02 05:51:34 -0700

    Remove parallel building. Causes issues for nuget restore

--- build.cmd
+++ build.cmd
@@ -3,4 +3,4 @@ set config=%1
 if "%config%" == "" (
    set config=Debug
 )
-%WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild Build\Build.proj /p:Configuration="%config%" /m /v:M /fl /flp:LogFile=msbuild.log;Verbosity=Normal /nr:false
+%WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild Build\Build.proj /p:Configuration="%config%" /v:M /fl /flp:LogFile=msbuild.log;Verbosity=Normal /nr:false
