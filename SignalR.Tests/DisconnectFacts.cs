e2223c0ed5cc9a6b8bec1546f5141a60caede865
David Fowler <davidfowl@gmail.com>
2012-04-03 02:27:11 -0700

    Added test to verify farm disconnect behavior.

--- SignalR.Tests/DisconnectFacts.cs
+++ SignalR.Tests/DisconnectFacts.cs
@@ -1,9 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
+using SignalR.Client.Http;
 using SignalR.Hosting.Memory;
 using SignalR.Hubs;
+using SignalR.Infrastructure;
 using Xunit;
 
 namespace SignalR.Tests
@@ -56,6 +60,111 @@ namespace SignalR.Tests
             connection.Stop();
 
             Assert.True(disconnectWh.Wait(disconnectWait), "Disconnect never fired");
+        }
+
+        [Fact]
+        public void FarmDisconnectOnlyRaisesEventOnce()
+        {
+            // Each node shares the same bus but are indepenent servers
+            var bus = new InProcessMessageBus(new TraceManager(), garbageCollectMessages: false);
+            var nodeCount = 3;
+            var nodes = new List<ServerNode>();
+            for (int i = 0; i < nodeCount; i++)
+            {
+                nodes.Add(new ServerNode(bus));
+            }
+
+            var timeout = TimeSpan.FromSeconds(5);
+            foreach (var node in nodes)
+            {
+                node.Server.Configuration.HeartBeatInterval = timeout;
+                node.Server.Configuration.DisconnectTimeout = TimeSpan.Zero;
+                node.Server.MapConnection<FarmConnection>("/echo");
+            }
+
+            var loadBalancer = new LoadBalancer(nodes.Select(f => f.Server).ToArray());
+            var transport = new Client.Transports.LongPollingTransport(loadBalancer);
+
+            var connection = new Client.Connection("http://goo/echo");
+
+            connection.Start(transport).Wait();
+
+            for (int i = 0; i < nodes.Count; i++)
+            {
+                nodes[i].Broadcast(String.Format("From Node {0}: {1}", i, i + 1));
+                Thread.Sleep(TimeSpan.FromSeconds(1));
+            }
+
+            Thread.Sleep(TimeSpan.FromTicks(timeout.Ticks * nodes.Count));
+
+            Assert.Equal(1, nodes.Sum(n => n.Connection.DisconnectCount));
+        }
+
+        private class ServerNode
+        {
+            public MemoryHost Server { get; private set; }
+            public FarmConnection Connection { get; private set; }
+
+            private IConnection _connection;
+            
+            public ServerNode(IMessageBus bus)
+            {
+                // Give each server it's own dependency resolver
+                Server = new MemoryHost(new DefaultDependencyResolver());
+                Connection = new FarmConnection();
+
+                Server.DependencyResolver.Register(typeof(FarmConnection), () => Connection);
+                Server.DependencyResolver.Register(typeof(IMessageBus), () => bus);
+
+                _connection = Server.ConnectionManager.GetConnection<FarmConnection>();
+            }
+
+            public void Broadcast(string message)
+            {
+                _connection.Broadcast(message).Wait();
+            }
+        }
+
+        private class LoadBalancer : IHttpClient
+        {
+            private int _counter;
+            private readonly IHttpClient[] _servers;
+            public LoadBalancer(params IHttpClient[] servers)
+            {
+                _servers = servers;
+            }
+
+            public Task<IResponse> GetAsync(string url, Action<IRequest> prepareRequest)
+            {
+                Debug.WriteLine("Server {0}: GET {1}", _counter, url);
+                int index = _counter;
+                _counter = (_counter + 1) % _servers.Length;
+                return _servers[index].GetAsync(url, prepareRequest);
+            }
+
+            public Task<IResponse> PostAsync(string url, Action<IRequest> prepareRequest, Dictionary<string, string> postData)
+            {
+                Debug.WriteLine("Server {0}: POST {1}", _counter, url);
+                int index = _counter;
+                _counter = (_counter + 1) % _servers.Length;
+                return _servers[index].PostAsync(url, prepareRequest, postData);
+            }
+        }
+
+        private class FarmConnection : PersistentConnection
+        {
+            public int DisconnectCount { get; set; }
+
+            protected override Task OnDisconnectAsync(string connectionId)
+            {
+                DisconnectCount++;
+                return base.OnDisconnectAsync(connectionId);
+            }
+
+            protected override Task OnReceivedAsync(string connectionId, string data)
+            {
+                return Connection.Broadcast(data);
+            }
         }
 
         private class MyHub : Hub, IDisconnect, IConnected
