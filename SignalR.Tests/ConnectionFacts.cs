585cded973c0d4db57a1d0adcecb7f9d694a8fda
David Fowler <davidfowl@gmail.com>
2012-04-03 02:31:14 -0700

    Remove sending big data test.

--- SignalR.Tests/ConnectionFacts.cs
+++ SignalR.Tests/ConnectionFacts.cs
@@ -28,58 +28,5 @@ namespace SignalR.Client.Tests
                 Assert.Equal("Incompatible protocol version.", ex.Message);
             }
         }
-
-        public class Received
-        {
-            [Fact]
-            public void SendingBigData()
-            {
-                var host = new MemoryHost();
-                host.MapConnection<SampleConnection>("/echo");
-
-                var connection = new Connection("http://foo/echo");
-
-                var wh = new ManualResetEventSlim();
-                var n = 0;
-                var target = 20;
-
-                connection.Received += data =>
-                {
-                    n++;
-                    if (n == target)
-                    {
-                        wh.Set();
-                    }
-                };
-
-                connection.Start(host).Wait();
-
-                var conn = host.ConnectionManager.GetConnection<SampleConnection>();
-
-                for (int i = 0; i < target; ++i)
-                {
-                    var node = new BigData();
-                    conn.Broadcast(node).Wait();
-                    Thread.Sleep(1000);
-                }
-
-                Assert.True(wh.Wait(TimeSpan.FromMinutes(1)), "Timed out");
-            }
-
-            public class BigData
-            {
-                public string[] Dummy
-                {
-                    get
-                    {
-                        return Enumerable.Range(0, 1000).Select(x => new String('*', 500)).ToArray();
-                    }
-                }
-            }
-
-            public class SampleConnection : PersistentConnection
-            {
-            }
-        }
     }
 }
