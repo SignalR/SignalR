2463f12c6e2905deae3d4c9b613e330b5fcdf667
heck0045 <tim@slatebox.com>
2012-04-07 15:08:22 -0500

    removed Clients from IHub.cs

--- SignalR/Hubs/IHub.cs
+++ SignalR/Hubs/IHub.cs
@@ -5,7 +5,6 @@ namespace SignalR.Hubs
     {
         IClientAgent Agent { get; set; }
         dynamic Caller { get; set; }
-        dynamic Clients { get; }
         HubContext Context { get; set; }
         IGroupManager GroupManager { get; set; }
     }
