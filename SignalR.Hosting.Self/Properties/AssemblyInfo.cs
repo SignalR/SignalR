74aa14bb5ea6d7c2c1b54b2a7b900bfed36e81b5
David Fowler <davidfowl@gmail.com>
2012-04-21 15:09:08 -0700

    Change the title of the self host package/assembly.

--- SignalR.Hosting.Self/Properties/AssemblyInfo.cs
+++ SignalR.Hosting.Self/Properties/AssemblyInfo.cs
@@ -1,6 +1,6 @@
 ﻿using System.Reflection;
 
 
-[assembly: AssemblyTitle("SignalR.SelfHost")]
+[assembly: AssemblyTitle("SignalR.Hosting.Self")]
 [assembly: AssemblyDescription("HttpListener host for SignalR")]
 [assembly: AssemblyVersion("0.5.0.0")]
