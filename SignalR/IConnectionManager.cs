d1fe80e385c3aa440642397cc4c75054f287b969
David Fowler <davidfowl@gmail.com>
2012-04-22 01:30:09 -0700

    Added more documentation and generated xml files for some projects for intellisense.

--- SignalR/IConnectionManager.cs
+++ SignalR/IConnectionManager.cs
@@ -10,10 +10,11 @@ namespace SignalR
         /// <summary>
         /// Returns a dynamic object representing all clients connected to the specified <see cref="IHub"/>.
         /// </summary>
-        /// <typeparam name="T">Type of the hub</typeparam>
+        /// <typeparam name="T">Type of the <see cref="IHub"/></typeparam>
         /// <returns>a dynamic object representing all clients connected to the specified <see cref="IHub"/></returns>
         dynamic GetClients<T>() where T : IHub;
 
+        /// <summary>
         /// Returns a dynamic object representing all clients connected to the specified hub.
         /// </summary>
         /// <param name="hubName">Name of the hub</param>
@@ -23,7 +24,7 @@ namespace SignalR
         /// <summary>
         /// Returns a connection representing all clients connected to the specified <see cref="PersistentConnection"/>.
         /// </summary>
-        /// <typeparam name="T">Type of the connection</typeparam>
+        /// <typeparam name="T">Type of the <see cref="PersistentConnection"/></typeparam>
         /// <returns>An IConnection representing all clients connected to the specified <see cref="PersistentConnection"/></returns>
         IConnection GetConnection<T>() where T : PersistentConnection;
     }
