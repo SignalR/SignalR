f5ab0e3983bf763c77922c2e6dee514c6a08c658
David Fowler <davidfowl@gmail.com>
2012-05-17 16:47:48 -0700

    Return the task in LongPollingTransport from send. Fixes #399.

--- SignalR/Transports/LongPollingTransport.cs
+++ SignalR/Transports/LongPollingTransport.cs
@@ -218,7 +218,7 @@ namespace SignalR.Transports
                     OnDisconnect();
                 }
 
-                Send(response);
+                return Send(response);
             });
         }
 
