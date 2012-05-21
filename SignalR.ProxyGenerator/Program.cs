2072cb11680ff5168acd8fc654e64a8d6e64341b
David Fowler <davidfowl@gmail.com>
2012-05-21 03:23:19 -0700

    Added hubify.exe to extract and minify from hub js from url.

---
+++ SignalR.ProxyGenerator/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Net;
+using Microsoft.Ajax.Utilities;
+
+namespace SignalR.ProxyGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length < 1)
+            {
+                Console.WriteLine("Usage: {0} [url]", typeof(Program).Assembly.GetName().Name);
+                return;
+            }
+
+            string url = args[0];
+            if (!url.EndsWith("/"))
+            {
+                url += "/";
+            }
+
+            if (!url.EndsWith("hubs", StringComparison.OrdinalIgnoreCase))
+            {
+                url += "hubs";
+            }
+
+            var uri = new Uri(url);
+
+            var minifier = new Minifier();
+            var wc = new WebClient();
+            Console.WriteLine(minifier.MinifyJavaScript(wc.DownloadString(uri)));
+        }
+    }
+}
