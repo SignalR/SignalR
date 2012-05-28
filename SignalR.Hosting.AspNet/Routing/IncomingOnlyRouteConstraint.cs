18d92ab5ab335ecdf66476040a029024164e44bd
Rune Soerensen <runesoerensen@gmail.com>
2012-05-27 18:37:06 -0700

    Make sure file name matches contained class

--- SignalR.Hosting.AspNet/Routing/IncomingOnlyRouteConstraint.cs
+++ SignalR.Hosting.AspNet/Routing/IncomingOnlyRouteConstraint.cs
@@ -1,20 +1,17 @@
-﻿using System;
-using System.IO;
-using System.Web;
+﻿using System.Web;
 using System.Web.Routing;
 
 namespace SignalR.Hosting.AspNet.Routing
 {
-    public class IgnoreJsRouteConstraint : IRouteConstraint
+    public class IncomingOnlyRouteConstraint : IRouteConstraint
     {
         public bool Match(HttpContextBase httpContext, Route route, string parameterName, RouteValueDictionary values, RouteDirection routeDirection)
         {
-            if (Path.GetExtension(httpContext.Request.AppRelativeCurrentExecutionFilePath).Equals(".js", StringComparison.OrdinalIgnoreCase))
+            if (routeDirection == RouteDirection.IncomingRequest)
             {
-                return false;
+                return true;
             }
-
-            return true;
+            return false;
         }
     }
 }
