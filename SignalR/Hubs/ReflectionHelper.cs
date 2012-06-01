8724a8430042dc4b416fc5efe870df2043123f50
David Fowler <davidfowl@gmail.com>
2012-06-01 03:20:37 -0700

    Moved ReflectionHelper into Hubs namespace and folder.

--- SignalR/Hubs/ReflectionHelper.cs
+++ SignalR/Hubs/ReflectionHelper.cs
@@ -2,9 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
-using SignalR.Hubs;
 
-namespace SignalR.Infrastructure
+namespace SignalR.Hubs
 {
     public static class ReflectionHelper
     {
