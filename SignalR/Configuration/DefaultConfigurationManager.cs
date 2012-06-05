47e17b68ce93d74a690dd9a7f918fddeb842fb02
David Fowler <davidfowl@gmail.com>
2012-06-04 19:51:30 -0700

    Turn keep alive on by default and make the interval 30s.

--- SignalR/Configuration/DefaultConfigurationManager.cs
+++ SignalR/Configuration/DefaultConfigurationManager.cs
@@ -9,6 +9,7 @@ namespace SignalR
             ConnectionTimeout = TimeSpan.FromSeconds(110);
             DisconnectTimeout = TimeSpan.FromSeconds(20);
             HeartBeatInterval = TimeSpan.FromSeconds(10);
+            KeepAlive = TimeSpan.FromSeconds(30);
         }
 
         public TimeSpan ConnectionTimeout
