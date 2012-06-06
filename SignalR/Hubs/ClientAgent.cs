f97dc6bfa0de6344a79e80a03b95492d6b54d7ea
David Fowler <davidfowl@gmail.com>
2012-06-05 19:55:44 -0700

    Made invoke methods public. Fixes #412.

--- SignalR/Hubs/ClientAgent.cs
+++ SignalR/Hubs/ClientAgent.cs
@@ -34,7 +34,7 @@ namespace SignalR.Hubs
             return true;
         }
 
-        private Task Invoke(string method, params object[] args)
+        public Task Invoke(string method, params object[] args)
         {
             var invocation = new
             {
