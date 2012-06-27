fbce848ac2bc4c1624c7615ad0c1bff377305f5d
David Fowler <davidfowl@gmail.com>
2012-06-26 20:01:49 -0700

    Updated site for 0.5.2

---
+++ docs/api/v0.5.2/js/navigation.js
@@ -0,0 +1,11 @@
+﻿$(document).ready(function() {
+  var scroll = function(selector) {
+    var currentItem = $(selector + ' .current');
+    
+    if (currentItem)
+      $(selector + ' div.scroll').scrollTo(currentItem);
+  };
+  
+  scroll('#namespaces');
+  scroll('#types');
+});
