2adbfae14937d624e1e986b3e7303793eb959a84
David Fowler <davidfowl@gmail.com>
2012-07-20 08:45:02 -0700

    Fix potential unobserved task.

--- SignalR/Hubs/HubDispatcher.cs
+++ SignalR/Hubs/HubDispatcher.cs
@@ -39,7 +39,7 @@ namespace SignalR.Hubs
                 return _trace["SignalR.HubDispatcher"];
             }
         }
-        
+
         public override void Initialize(IDependencyResolver resolver)
         {
             _proxyGenerator = resolver.Resolve<IJavaScriptProxyGenerator>();
@@ -123,9 +123,8 @@ namespace SignalR.Hubs
                 resultTask = ProcessResponse(state, null, hubRequest, e);
             }
 
-            return resultTask
-                .ContinueWith(_ => base.OnReceivedAsync(request, connectionId, data))
-                .FastUnwrap();
+            return resultTask.Then(() => base.OnReceivedAsync(request, connectionId, data))
+                             .Catch();
         }
 
         public override Task ProcessRequestAsync(HostContext context)
