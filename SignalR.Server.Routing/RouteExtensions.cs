2e48cdd6f605023e6abd2b787efdc82ff88589bd
Louis DeJardin <louis.dejardin@gmail.com>
2012-08-31 21:22:28 -0700

    Removing commented code

--- SignalR.Server.Routing/RouteExtensions.cs
+++ SignalR.Server.Routing/RouteExtensions.cs
@@ -107,54 +107,9 @@ namespace SignalR
 
             var routeUrl = url.TrimStart('~').TrimStart('/');
 
-            // TODO: IgnoreJsRouteConstraint?
-
             return routes.MapOwinRoute("signalr.hubs", routeUrl, map => map.MapHubs(resolver));
-            //var existing = routes["signalr.hubs"];
-            //if (existing != null)
-            //{
-            //    routes.Remove(existing);
-            //}
-
-            //string routeUrl = path;
-            //if (!routeUrl.EndsWith("/"))
-            //{
-            //    routeUrl += "/{*operation}";
-            //}
-
-            //routeUrl = routeUrl.TrimStart('~').TrimStart('/');
-
-            //var locator = new Lazy<IAssemblyLocator>(() => new AspNetAssemblyLocator());
-            //resolver.Register(typeof(IAssemblyLocator), () => locator.Value);
-
-            //var route = new Route(routeUrl, new HubDispatcherRouteHandler(path, resolver));
-            //route.Constraints = new RouteValueDictionary();
-            //route.Constraints.Add("Incoming", new IncomingOnlyRouteConstraint());
-            //route.Constraints.Add("IgnoreJs", new IgnoreJsRouteConstraint());
-            //routes.Add("signalr.hubs", route);
-            //return route;
         }
 
-        //static Func<AppDelegate,AppDelegate> TraceCalls()
-        //{
-        //    var count = 0;
-        //    return app => call =>
-        //    {
-        //        var number = Interlocked.Increment(ref count);
-        //        var req = new Gate.Request(call);
-        //        Trace.WriteLine(string.Format("#{0} {1} {2}{3} {4}", number, req.Method, req.PathBase, req.Path, req.QueryString));
-        //        return app(call).Then(result=>
-        //        {
-        //            Trace.WriteLine(string.Format("#{0} {1}", number, result.Status));
-        //            return result;
-        //        });
-        //    };
-        //}
-
-        static void NormalizePath(string appDomainAppVirtualPath)
-        {
-            throw new NotImplementedException();
-        }
 
         /// <summary>
         /// Maps a <see cref="PersistentConnection"/> with the default dependency resolver to the specified path.
