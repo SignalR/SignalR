4717b73ead8618185488758ca4cd7e5bb0a76395
David Fowler <davidfowl@gmail.com>
2012-08-31 17:58:21 -0700

    Don't call flush in MemoryHost since it's a noop.

--- SignalR.Hosting.Memory/Response.cs
+++ SignalR.Hosting.Memory/Response.cs
@@ -239,8 +239,6 @@ namespace SignalR.Hosting.Memory
 
             public override void Close()
             {
-                Flush();
-
                 _cancellationTokenSource.Cancel(throwOnFirstException: false);
 
                 if (_onClosed != null)
