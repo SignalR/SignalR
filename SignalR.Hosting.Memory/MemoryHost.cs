80c96448c7432179af16f81c627dd2fd13ad06eb
damianedwards <damian@damianedwards.com>
2012-09-06 22:41:46 -0700

    MemoryHost creates instance name for perf counters.

--- SignalR.Hosting.Memory/MemoryHost.cs
+++ SignalR.Hosting.Memory/MemoryHost.cs
@@ -7,12 +7,16 @@ using SignalR.Hosting.Common;
 
 using IClientRequest = SignalR.Client.Http.IRequest;
 using IClientResponse = SignalR.Client.Http.IResponse;
+using System.Diagnostics;
 
 namespace SignalR.Hosting.Memory
 {
     public class MemoryHost : RoutingHost, IHttpClient, IDisposable
     {
         private readonly CancellationTokenSource _shutDownToken = new CancellationTokenSource();
+        private readonly string _mutexPrefix;
+        private readonly string _instanceNamePrefix;
+        private Mutex _instanceNameMutex;
 
         public MemoryHost()
             : this(new DefaultDependencyResolver())
@@ -23,8 +27,13 @@ namespace SignalR.Hosting.Memory
         public MemoryHost(IDependencyResolver resolver)
             : base(resolver)
         {
-
+            var process = Process.GetCurrentProcess();
+            _mutexPrefix = process.ProcessName;
+            _instanceNamePrefix = process.ProcessName + " (PID " + process.Id + "): ";
+            InstanceName = GetNextInstanceId();
         }
+
+        public string InstanceName { get; set; }
 
         Task<IClientResponse> IHttpClient.GetAsync(string url, Action<IClientRequest> prepareRequest)
         {
@@ -51,6 +60,8 @@ namespace SignalR.Hosting.Memory
                 Response response = null;
                 response = new Response(clientTokenSource.Token, () => tcs.TrySetResult(response));
                 var hostContext = new HostContext(request, response);
+                var instanceName = GetInstanceName();
+                hostContext.Items[HostConstants.InstanceName] = instanceName;
                 hostContext.Items[HostConstants.ShutdownToken] = _shutDownToken.Token;
 
                 connection.Initialize(DependencyResolver);
@@ -79,9 +90,45 @@ namespace SignalR.Hosting.Memory
             return TaskAsyncHelper.FromError<IClientResponse>(new InvalidOperationException("Not a valid end point"));
         }
 
+        private string GetInstanceName()
+        {
+            return _instanceNamePrefix + InstanceName;
+        }
+
+        private string GetNextInstanceId()
+        {
+            var instanceId = 0;
+            while (true)
+            {
+                var mutexName = _mutexPrefix + instanceId;
+                bool createdMutex;
+                // Try to create the mutex with ownership
+                try
+                {
+                    var mutex = new Mutex(true, mutexName, out createdMutex);
+                    if (createdMutex)
+                    {
+                        _instanceNameMutex = mutex;
+                        break;
+                    }
+                }
+                catch (UnauthorizedAccessException)
+                {
+                    // Mutex exists but we don't have access to it
+                }
+                catch (WaitHandleCannotBeOpenedException)
+                {
+                    // Name conflict with another native handle
+                }
+                instanceId++;
+            }
+            return instanceId.ToString();
+        }
+
         public void Dispose()
         {
             _shutDownToken.Cancel(throwOnFirstException: false);
+            _instanceNameMutex.ReleaseMutex();
         }
     }
 }
