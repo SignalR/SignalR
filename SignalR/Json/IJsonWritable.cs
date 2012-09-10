ea60c59737aefa3e08561d7a299dc5de9670caf9
Stephen Halter <shalter@microsoft.com>
2012-09-10 16:31:08 -0700

    Added IJsonWritable interface
    JsonNetSerializer no longer explicitly checks for a PersistentResponse to decide
    whether to perform special serialization logic and instead will execute custom
    serialization for any class that implements the IJsonWritable interface.

---
+++ SignalR/Json/IJsonWritable.cs
@@ -0,0 +1,16 @@
+﻿using System.IO;
+
+namespace SignalR
+{
+    /// <summary>
+    /// Implementations handle their own serialization to JSON.
+    /// </summary>
+    public interface IJsonWritable
+    {
+        /// <summary>
+        /// Serializes itself to JSON via a <see cref="System.IO.TextWriter"/>.
+        /// </summary>
+        /// <param name="writer">The <see cref="System.IO.TextWriter"/> that receives the JSON serialized object.</param>
+        void WriteJson(TextWriter writer);
+    }
+}
