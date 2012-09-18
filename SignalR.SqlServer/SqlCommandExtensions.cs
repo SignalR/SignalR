9dc37137e861d67b9b54a53182fc581d3ec8896a
DamianEdwards <damian@damianedwards.com>
2012-09-17 18:18:00 -0700

    Cleaned up usings

--- SignalR.SqlServer/SqlCommandExtensions.cs
+++ SignalR.SqlServer/SqlCommandExtensions.cs
@@ -1,9 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data;
+﻿using System.Data;
 using System.Data.SqlClient;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace SignalR.SqlServer
--- SignalR.SqlServer/SqlSender.cs
+++ SignalR.SqlServer/SqlSender.cs
@@ -1,9 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Data;
 using System.Data.SqlClient;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace SignalR.SqlServer
