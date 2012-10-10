11388ee24ea49ed463b10c6f615c88c97a40260e
Stephen Halter <halter73@gmail.com>
2012-10-09 23:06:35 -0700

    Made NotAuthorizedException public and Serializable

--- SignalR/Hubs/Pipeline/Auth/NotAuthorizedException.cs
+++ SignalR/Hubs/Pipeline/Auth/NotAuthorizedException.cs
@@ -2,9 +2,15 @@
 
 namespace SignalR.Hubs
 {
-    class NotAuthorizedException : Exception
+    [Serializable]
+    public class NotAuthorizedException : Exception
     {
         public NotAuthorizedException() { }
         public NotAuthorizedException(string message) : base(message) { }
+        public NotAuthorizedException(string message, Exception inner) : base(message, inner) { }
+        protected NotAuthorizedException(
+          System.Runtime.Serialization.SerializationInfo info,
+          System.Runtime.Serialization.StreamingContext context)
+            : base(info, context) { }
     }
 }
