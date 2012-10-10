081a77fd20eb5e7f3a3433eca77f7bb33f21f369
Stephen Halter <halter73@gmail.com>
2012-10-09 23:29:57 -0700

    Added authorization tests to reflect the new sample scenarios
    - Added tests for the new incoming and outgoing AuthorizationModes
    - Added tests where the AuthorizationAttribute is inherited
    - Added tests where there are multiple attributes per hub

--- SignalR.Tests/Server/Hubs/HubAuthFacts.cs
+++ SignalR.Tests/Server/Hubs/HubAuthFacts.cs
@@ -230,6 +230,29 @@ namespace SignalR.Tests
         }
 
         [Fact]
+        public void AuthenticatedUserCanReceiveHubMessagesFromAuthorizedHubs()
+        {
+            var host = new MemoryHost();
+            host.MapHubs();
+            var connection = new Client.Hubs.HubConnection("http://foo/");
+
+            host.User = new GenericPrincipal(new GenericIdentity("test"), new string[] { });
+
+            var hub = connection.CreateProxy("AuthHub");
+            var wh = new ManualResetEvent(false);
+            hub.On<string, string, object>("joined", (id, time, authInfo) =>
+            {
+                Assert.NotNull(id);
+                wh.Set();
+            });
+
+            connection.Start(host).Wait();
+
+            Assert.True(wh.WaitOne(TimeSpan.FromSeconds(3)));
+            connection.Stop();
+        }
+
+        [Fact]
         public void UnauthenticatedUserCannotInvokeMethodsInAuthorizedHubs()
         {
             var host = new MemoryHost();
@@ -255,7 +278,7 @@ namespace SignalR.Tests
         }
 
         [Fact]
-        public void AuthenticatedUserCanReceiveHubMessagesFromAuthorizedHubs()
+        public void AuthenticatedUserCanInvokeMethodsInAuthorizedHubs()
         {
             var host = new MemoryHost();
             host.MapHubs();
@@ -265,6 +288,31 @@ namespace SignalR.Tests
 
             var hub = connection.CreateProxy("AuthHub");
             var wh = new ManualResetEvent(false);
+            hub.On<string, string>("invoked", (id, time) =>
+            {
+                Assert.NotNull(id);
+                wh.Set();
+            });
+
+            connection.Start(host).Wait();
+
+            hub.Invoke("InvokedFromClient").Wait();
+
+            Assert.True(wh.WaitOne(TimeSpan.FromSeconds(3)));
+            connection.Stop();
+        }
+
+        [Fact]
+        public void UnauthenticatedUserCannotReceiveHubMessagesFromHubsInheritingFromAuthorizedHubs()
+        {
+            var host = new MemoryHost();
+            host.MapHubs();
+            var connection = new Client.Hubs.HubConnection("http://foo/");
+
+            host.User = new GenericPrincipal(new GenericIdentity(""), new string[] { });
+
+            var hub = connection.CreateProxy("InheritAuthHub");
+            var wh = new ManualResetEvent(false);
             hub.On<string, string, object>("joined", (id, time, authInfo) =>
             {
                 Assert.NotNull(id);
@@ -272,6 +320,79 @@ namespace SignalR.Tests
             });
 
             connection.Start(host).Wait();
+
+            Assert.False(wh.WaitOne(TimeSpan.FromSeconds(3)));
+            connection.Stop();
+        }
+
+        [Fact]
+        public void AuthenticatedUserCanReceiveHubMessagesFromHubsInheritingFromAuthorizedHubs()
+        {
+            var host = new MemoryHost();
+            host.MapHubs();
+            var connection = new Client.Hubs.HubConnection("http://foo/");
+
+            host.User = new GenericPrincipal(new GenericIdentity("test"), new string[] { });
+
+            var hub = connection.CreateProxy("InheritAuthHub");
+            var wh = new ManualResetEvent(false);
+            hub.On<string, string, object>("joined", (id, time, authInfo) =>
+            {
+                Assert.NotNull(id);
+                wh.Set();
+            });
+
+            connection.Start(host).Wait();
+
+            Assert.True(wh.WaitOne(TimeSpan.FromSeconds(3)));
+            connection.Stop();
+        }
+
+        [Fact]
+        public void UnauthenticatedUserCannotInvokeMethodsInHubsInheritingFromAuthorizedHubs()
+        {
+            var host = new MemoryHost();
+            host.MapHubs();
+            var connection = new Client.Hubs.HubConnection("http://foo/");
+
+            host.User = new GenericPrincipal(new GenericIdentity(""), new string[] { });
+
+            var hub = connection.CreateProxy("InheritAuthHub");
+            var wh = new ManualResetEvent(false);
+            hub.On<string, string>("invoked", (id, time) =>
+            {
+                Assert.NotNull(id);
+                wh.Set();
+            });
+
+            connection.Start(host).Wait();
+
+            Assert.Throws<AggregateException>(() => hub.Invoke("InvokedFromClient").Wait());
+
+            Assert.False(wh.WaitOne(TimeSpan.FromSeconds(3)));
+            connection.Stop();
+        }
+
+        [Fact]
+        public void AuthenticatedUserCanInvokeMethodsInHubsInheritingFromAuthorizedHubs()
+        {
+            var host = new MemoryHost();
+            host.MapHubs();
+            var connection = new Client.Hubs.HubConnection("http://foo/");
+
+            host.User = new GenericPrincipal(new GenericIdentity("test"), new string[] { });
+
+            var hub = connection.CreateProxy("InheritAuthHub");
+            var wh = new ManualResetEvent(false);
+            hub.On<string, string>("invoked", (id, time) =>
+            {
+                Assert.NotNull(id);
+                wh.Set();
+            });
+
+            connection.Start(host).Wait();
+
+            hub.Invoke("InvokedFromClient").Wait();
 
             Assert.True(wh.WaitOne(TimeSpan.FromSeconds(3)));
             connection.Stop();
@@ -391,6 +512,312 @@ namespace SignalR.Tests
             });
 
             connection.Start(host).Wait();
+
+            Assert.True(wh.WaitOne(TimeSpan.FromSeconds(3)));
+            connection.Stop();
+        }
+
+        [Fact]
+        public void AuthenticatedAndAuthorizedUserCanInvokeMethodsInHubsAuthorizedWithRoles()
+        {
+            var host = new MemoryHost();
+            host.MapHubs();
+            var connection = new Client.Hubs.HubConnection("http://foo/");
+
+            host.User = new GenericPrincipal(new GenericIdentity("test"), new string[] { "Admin" });
+
+            var hub = connection.CreateProxy("AdminAuthHub");
+            var wh = new ManualResetEvent(false);
+            hub.On<string, string>("invoked", (id, time) =>
+            {
+                Assert.NotNull(id);
+                wh.Set();
+            });
+
+            connection.Start(host).Wait();
+
+            hub.Invoke("InvokedFromClient").Wait();
+
+            Assert.True(wh.WaitOne(TimeSpan.FromSeconds(3)));
+            connection.Stop();
+        }
+
+        [Fact]
+        public void UnauthorizedUserCannotReceiveHubMessagesFromHubsAuthorizedSpecifyingUserAndRole()
+        {
+            var host = new MemoryHost();
+            host.MapHubs();
+            var connection = new Client.Hubs.HubConnection("http://foo/");
+
+            host.User = new GenericPrincipal(new GenericIdentity("User"), new string[] { "User", "NotAdmin" });
+
+            var hub = connection.CreateProxy("UserAndRoleAuthHub");
+            var wh = new ManualResetEvent(false);
+            hub.On<string, string, object>("joined", (id, time, authInfo) =>
+            {
+                Assert.NotNull(id);
+                wh.Set();
+            });
+
+            connection.Start(host).Wait();
+
+            Assert.False(wh.WaitOne(TimeSpan.FromSeconds(3)));
+            connection.Stop();
+        }
+
+        [Fact]
+        public void UnauthorizedUserCannotInvokeMethodsInHubsAuthorizedSpecifyingUserAndRole()
+        {
+            var host = new MemoryHost();
+            host.MapHubs();
+            var connection = new Client.Hubs.HubConnection("http://foo/");
+
+            host.User = new GenericPrincipal(new GenericIdentity("test"), new string[] { "User", "Admin" });
+
+            var hub = connection.CreateProxy("UserAndRoleAuthHub");
+            var wh = new ManualResetEvent(false);
+            hub.On<string, string>("invoked", (id, time) =>
+            {
+                Assert.NotNull(id);
+                wh.Set();
+            });
+
+            connection.Start(host).Wait();
+
+            Assert.Throws<AggregateException>(() => hub.Invoke("InvokedFromClient").Wait());
+
+            Assert.False(wh.WaitOne(TimeSpan.FromSeconds(3)));
+            connection.Stop();
+        }
+
+        [Fact]
+        public void AuthenticatedAndAuthorizedUserCanReceiveHubMessagesFromHubsAuthorizedSpecifyingUserAndRole()
+        {
+            var host = new MemoryHost();
+            host.MapHubs();
+            var connection = new Client.Hubs.HubConnection("http://foo/");
+
+            host.User = new GenericPrincipal(new GenericIdentity("User"), new string[] { "test", "Admin" });
+
+            var hub = connection.CreateProxy("UserAndRoleAuthHub");
+            var wh = new ManualResetEvent(false);
+            hub.On<string, string, object>("joined", (id, time, authInfo) =>
+            {
+                Assert.NotNull(id);
+                wh.Set();
+            });
+
+            connection.Start(host).Wait();
+
+            Assert.True(wh.WaitOne(TimeSpan.FromSeconds(3)));
+            connection.Stop();
+        }
+
+        [Fact]
+        public void AuthenticatedAndAuthorizedUserCanInvokeMethodsInHubsAuthorizedSpecifyingUserAndRole()
+        {
+            var host = new MemoryHost();
+            host.MapHubs();
+            var connection = new Client.Hubs.HubConnection("http://foo/");
+
+            host.User = new GenericPrincipal(new GenericIdentity("User"), new string[] { "Admin" });
+
+            var hub = connection.CreateProxy("UserAndRoleAuthHub");
+            var wh = new ManualResetEvent(false);
+            hub.On<string, string>("invoked", (id, time) =>
+            {
+                Assert.NotNull(id);
+                wh.Set();
+            });
+
+            connection.Start(host).Wait();
+
+            hub.Invoke("InvokedFromClient").Wait();
+
+            Assert.True(wh.WaitOne(TimeSpan.FromSeconds(3)));
+            connection.Stop();
+        }
+
+        [Fact]
+        public void UnauthenticatedUserCanReceiveHubMessagesFromIncomingAuthorizedHubs()
+        {
+            var host = new MemoryHost();
+            host.MapHubs();
+            var connection = new Client.Hubs.HubConnection("http://foo/");
+
+            host.User = new GenericPrincipal(new GenericIdentity(""), new string[] { "User", "NotAdmin" });
+
+            var hub = connection.CreateProxy("IncomingAuthHub");
+            var wh = new ManualResetEvent(false);
+            hub.On<string, string, object>("joined", (id, time, authInfo) =>
+            {
+                Assert.NotNull(id);
+                wh.Set();
+            });
+
+            connection.Start(host).Wait();
+
+            Assert.True(wh.WaitOne(TimeSpan.FromSeconds(3)));
+            connection.Stop();
+        }
+
+        [Fact]
+        public void UnauthenticatedUserCannotInvokeMethodsInIncomingAuthorizedHubs()
+        {
+            var host = new MemoryHost();
+            host.MapHubs();
+            var connection = new Client.Hubs.HubConnection("http://foo/");
+
+            host.User = new GenericPrincipal(new GenericIdentity(""), new string[] { "User", "NotAdmin" });
+
+            var hub = connection.CreateProxy("IncomingAuthHub");
+            var wh = new ManualResetEvent(false);
+            hub.On<string, string>("invoked", (id, time) =>
+            {
+                Assert.NotNull(id);
+                wh.Set();
+            });
+
+            connection.Start(host).Wait();
+
+            Assert.Throws<AggregateException>(() => hub.Invoke("InvokedFromClient").Wait());
+
+            Assert.False(wh.WaitOne(TimeSpan.FromSeconds(3)));
+            connection.Stop();
+        }
+
+        [Fact]
+        public void AuthenticatedUserCanReceiveHubMessagesFromIncomingAuthorizedHubs()
+        {
+            var host = new MemoryHost();
+            host.MapHubs();
+            var connection = new Client.Hubs.HubConnection("http://foo/");
+
+            host.User = new GenericPrincipal(new GenericIdentity("Admin"), new string[] { });
+
+            var hub = connection.CreateProxy("IncomingAuthHub");
+            var wh = new ManualResetEvent(false);
+            hub.On<string, string, object>("joined", (id, time, authInfo) =>
+            {
+                Assert.NotNull(id);
+                wh.Set();
+            });
+
+            connection.Start(host).Wait();
+
+            Assert.True(wh.WaitOne(TimeSpan.FromSeconds(3)));
+            connection.Stop();
+        }
+
+        [Fact]
+        public void AuthenticatedUserCanInvokeMethodsInIncomingAuthorizedHubs()
+        {
+            var host = new MemoryHost();
+            host.MapHubs();
+            var connection = new Client.Hubs.HubConnection("http://foo/");
+
+            host.User = new GenericPrincipal(new GenericIdentity("User"), new string[] { "Admin" });
+
+            var hub = connection.CreateProxy("IncomingAuthHub");
+            var wh = new ManualResetEvent(false);
+            hub.On<string, string>("invoked", (id, time) =>
+            {
+                Assert.NotNull(id);
+                wh.Set();
+            });
+
+            connection.Start(host).Wait();
+
+            hub.Invoke("InvokedFromClient").Wait();
+
+            Assert.True(wh.WaitOne(TimeSpan.FromSeconds(3)));
+            connection.Stop();
+        }
+
+        [Fact]
+        public void UnauthenticatedUserCannotReceiveHubMessagesFromOutgoingAuthorizedHubs()
+        {
+            var host = new MemoryHost();
+            host.MapHubs();
+            var connection = new Client.Hubs.HubConnection("http://foo/");
+
+            host.User = new GenericPrincipal(new GenericIdentity(""), new string[] { "User", "NotAdmin" });
+
+            var hub = connection.CreateProxy("OutgoingAuthHub");
+            var wh = new ManualResetEvent(false);
+            hub.On<string, string, object>("joined", (id, time, authInfo) =>
+            {
+                Assert.NotNull(id);
+                wh.Set();
+            });
+
+            connection.Start(host).Wait();
+
+            Assert.False(wh.WaitOne(TimeSpan.FromSeconds(3)));
+            connection.Stop();
+        }
+
+        [Fact]
+        public void UnauthenticatedUserCanInvokeMethodsInOutgoingAuthorizedHubs()
+        {
+            var host = new MemoryHost();
+            host.MapHubs();
+            var connection = new Client.Hubs.HubConnection("http://foo/");
+
+            host.User = new GenericPrincipal(new GenericIdentity(""), new string[] { "User", "NotAdmin" });
+
+            var hub = connection.CreateProxy("OutgoingAuthHub");
+
+            connection.Start(host).Wait();
+
+            hub.Invoke("InvokedFromClient").Wait();
+
+            connection.Stop();
+        }
+
+        [Fact]
+        public void AuthenticatedUserCanReceiveHubMessagesFromOutgoingAuthorizedHubs()
+        {
+            var host = new MemoryHost();
+            host.MapHubs();
+            var connection = new Client.Hubs.HubConnection("http://foo/");
+
+            host.User = new GenericPrincipal(new GenericIdentity("Admin"), new string[] { });
+
+            var hub = connection.CreateProxy("OutgoingAuthHub");
+            var wh = new ManualResetEvent(false);
+            hub.On<string, string, object>("joined", (id, time, authInfo) =>
+            {
+                Assert.NotNull(id);
+                wh.Set();
+            });
+
+            connection.Start(host).Wait();
+
+            Assert.True(wh.WaitOne(TimeSpan.FromSeconds(3)));
+            connection.Stop();
+        }
+
+        [Fact]
+        public void AuthenticatedUserCanInvokeMethodsInOutgoingAuthorizedHubs()
+        {
+            var host = new MemoryHost();
+            host.MapHubs();
+            var connection = new Client.Hubs.HubConnection("http://foo/");
+
+            host.User = new GenericPrincipal(new GenericIdentity("User"), new string[] { "Admin" });
+
+            var hub = connection.CreateProxy("OutgoingAuthHub");
+            var wh = new ManualResetEvent(false);
+            hub.On<string, string>("invoked", (id, time) =>
+            {
+                Assert.NotNull(id);
+                wh.Set();
+            });
+
+            connection.Start(host).Wait();
+
+            hub.Invoke("InvokedFromClient").Wait();
 
             Assert.True(wh.WaitOne(TimeSpan.FromSeconds(3)));
             connection.Stop();
