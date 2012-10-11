10371552eda22c2900feb55c105831f0c10f003e
Stephen Halter <halter73@gmail.com>
2012-10-10 19:27:38 -0700

    Hopefully fix Silverlight builds after break following name change.

--- src/Microsoft.AspNet.SignalR.Client.Silverlight5/Properties/AssemblyInfo.cs
+++ src/Microsoft.AspNet.SignalR.Client.Silverlight5/Properties/AssemblyInfo.cs
@@ -2,5 +2,5 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("SignalR.Client.Silverlight5")]
+[assembly: AssemblyTitle("Microsoft.AspNet.SignalR.Client.Silverlight5")]
 [assembly: AssemblyDescription("Silverlight 5 client for SignalR")]
