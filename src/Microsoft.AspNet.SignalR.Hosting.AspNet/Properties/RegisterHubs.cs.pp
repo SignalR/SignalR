d47c59daa883e695cc13dac5bd267ac1de567d0e
Stephen Halter <halter73@gmail.com>
2012-10-16 18:34:58 -0700

    Remove automatic MapHubs call from ASP.NET host
    Fixes Issue #790

---
+++ src/Microsoft.AspNet.SignalR.Hosting.AspNet/Properties/RegisterHubs.cs.pp
@@ -0,0 +1,18 @@
+﻿using System.Web;
+using System.Web.Routing;
+using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hosting.AspNet;
+
+[assembly: PreApplicationStartMethod(typeof($rootnamespace$.RegisterHubs), "Start")]
+
+namespace $rootnamespace$
+{
+    public static class RegisterHubs
+    {
+        public static void Start()
+        {
+            // Register the default hubs route: ~/signalr/hubs
+            RouteTable.Routes.MapHubs();            
+        }
+    }
+}
