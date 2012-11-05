b877760b25c5e897e13fa3c49572c7918a173ed0
David Fowler <davidfowl@gmail.com>
2012-11-05 15:42:09 -0800

    Added xml comments for redis dependency resolver extensions.

--- src/Microsoft.AspNet.SignalR.Redis/DependencyResolverExtensions.cs
+++ src/Microsoft.AspNet.SignalR.Redis/DependencyResolverExtensions.cs
@@ -8,11 +8,30 @@ namespace Microsoft.AspNet.SignalR
 {
     public static class DependencyResolverExtensions
     {
+        /// <summary>
+        /// Use the Redis backplane for SignalR.
+        /// </summary>
+        /// <param name="resolver">The dependency resolver.</param>
+        /// <param name="server">The redis server.</param>
+        /// <param name="port">The redis port.</param>
+        /// <param name="password">The redis server password.</param>
+        /// <param name="eventKeys">The event keys.</param>
+        /// <returns>The dependency resolver</returns>
         public static IDependencyResolver UseRedis(this IDependencyResolver resolver, string server, int port, string password, IEnumerable<string> eventKeys)
         {
             return UseRedis(resolver, server, port, password, db: 0, eventKeys: eventKeys);
         }
 
+        /// <summary>
+        /// Use the Redis backplane for SignalR.
+        /// </summary>
+        /// <param name="resolver">The dependency resolver.</param>
+        /// <param name="server">The redis server.</param>
+        /// <param name="port">The redis port.</param>
+        /// <param name="password">The redis server password.</param>
+        /// <param name="db">The database to use</param>
+        /// <param name="eventKeys">The event keys.</param> 
+        /// <returns></returns>
         public static IDependencyResolver UseRedis(this IDependencyResolver resolver, string server, int port, string password, int db, IEnumerable<string> eventKeys)
         {
             var bus = new Lazy<RedisMessageBus>(() => new RedisMessageBus(server, port, password, db, eventKeys, resolver));
