3b9f9f10e6596656ee27ed7d6305a714e9d57c9b
Stephen Halter <halter73@gmail.com>
2012-11-13 15:59:21 -0800

    Split RunTests build target into RunUnitTests and RunFunctionalTests

--- build.cmd
+++ build.cmd
@@ -1,6 +1,10 @@
 @echo Off
-set config=%1
+set target=%1
+if "%target%" == "" (
+   set target=BuildCmd
+)
+set config=%2
 if "%config%" == "" (
    set config=Debug
 )
-%WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild Build\Build.proj /p:Configuration="%config%" /v:M /fl /flp:LogFile=msbuild.log;Verbosity=Normal /nr:false
+%WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild Build\Build.proj /t:"%target%" /p:Configuration="%config%" /v:M /fl /flp:LogFile=msbuild.log;Verbosity=Normal /nr:false
