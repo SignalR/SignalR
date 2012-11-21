c9fac327171852a298d55adc669d85127c925eb0
David Fowler <davidfowl@gmail.com>
2012-11-20 17:39:39 -0800

    Removed self host because we have OWIN!

--- src/Microsoft.AspNet.SignalR.Hosting.Self/Properties/AssemblyInfo.cs
+++
@@ -1,6 +0,0 @@
-﻿// Copyright (c) Microsoft Open Technologies, Inc. All rights reserved. See License.md in the project root for license information.
-
-using System.Reflection;
-
-[assembly: AssemblyTitle("Microsoft.AspNet.SignalR.Hosting.Self")]
-[assembly: AssemblyDescription("HttpListener host for SignalR")]
