5ec61eb37c16369976ba66f4d5262a0bf345acfc
David Fowler <davidfowl@gmail.com>
2012-11-27 19:23:56 -0800

    Expose Request to the HubCallerContext. - Added functional test to verify owin env in populated.

--- tests/Microsoft.AspNet.SignalR.FunctionalTests/Server/Connections/PersistentConnectionFacts.cs
+++ tests/Microsoft.AspNet.SignalR.FunctionalTests/Server/Connections/PersistentConnectionFacts.cs
@@ -528,23 +528,5 @@ namespace Microsoft.AspNet.SignalR.Tests
                 }
             }
         }
-
-        public class RequestItemsResponse
-        {
-            public string Method {get;set;}
-            public int Count { get; set; }
-            public string[] OwinKeys { get; set; }
-            public string[] Keys { get; set; }
-
-            public override int GetHashCode()
-            {
-                return Method.GetHashCode();
-            }
-
-            public override bool Equals(object obj)
-            {
-                return Method.Equals(((RequestItemsResponse)obj).Method);
-            }
-        }
     }
 }
