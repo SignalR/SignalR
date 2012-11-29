877bcd59c454f3d7ed65269bc6134b46d999afe8
David Fowler <davidfowl@gmail.com>
2012-11-28 20:08:00 -0800

    Removed unused code.

--- src/Microsoft.AspNet.SignalR.Owin/Infrastructure/ParamDictionary.cs
+++ src/Microsoft.AspNet.SignalR.Owin/Infrastructure/ParamDictionary.cs
@@ -9,7 +9,7 @@ using System.Linq;
 namespace Microsoft.AspNet.SignalR.Owin.Infrastructure
 {
     [SuppressMessage("Microsoft.Performance", "CA1812:AvoidUninstantiatedInternalClasses", Justification = "It is instantiated in the static Parse method")]
-    internal sealed class ParamDictionary : IDictionary<string, string>
+    internal sealed class ParamDictionary
     {
         private static readonly char[] DefaultParamSeparators = new[] { '&', ';' };
         private static readonly char[] ParamKeyValueSeparator = new[] { '=' };
@@ -31,106 +31,6 @@ namespace Microsoft.AspNet.SignalR.Owin.Infrastructure
 
                 yield return new KeyValuePair<string, string>(pairKey, pairValue);
             }
-        }
-        
-        private readonly IDictionary<string, string> _values;
-
-        internal ParamDictionary(IDictionary<string, string> values)
-        {
-            _values = values;
-        }
-
-        IEnumerator<KeyValuePair<string, string>> IEnumerable<KeyValuePair<string, string>>.GetEnumerator()
-        {
-            return _values.GetEnumerator();
-        }
-
-        IEnumerator IEnumerable.GetEnumerator()
-        {
-            return _values.GetEnumerator();
-        }
-
-        void ICollection<KeyValuePair<string, string>>.Add(KeyValuePair<string, string> item)
-        {
-            _values.Add(item);
-        }
-
-        void ICollection<KeyValuePair<string, string>>.Clear()
-        {
-            _values.Clear();
-        }
-
-        bool ICollection<KeyValuePair<string, string>>.Contains(KeyValuePair<string, string> item)
-        {
-            return _values.Contains(item);
-        }
-
-        void ICollection<KeyValuePair<string, string>>.CopyTo(KeyValuePair<string, string>[] array, int arrayIndex)
-        {
-            _values.CopyTo(array, arrayIndex);
-        }
-
-        bool ICollection<KeyValuePair<string, string>>.Remove(KeyValuePair<string, string> item)
-        {
-            return _values.Remove(item);
-        }
-
-        int ICollection<KeyValuePair<string, string>>.Count
-        {
-            get { return _values.Count; }
-        }
-
-        bool ICollection<KeyValuePair<string, string>>.IsReadOnly
-        {
-            get { return _values.IsReadOnly; }
-        }
-
-        bool IDictionary<string, string>.ContainsKey(string key)
-        {
-            return _values.ContainsKey(key);
-        }
-
-        void IDictionary<string, string>.Add(string key, string value)
-        {
-            _values.Add(key, value);
-        }
-
-        bool IDictionary<string, string>.Remove(string key)
-        {
-            return _values.Remove(key);
-        }
-
-        bool IDictionary<string, string>.TryGetValue(string key, out string value)
-        {
-            return _values.TryGetValue(key, out value);
-        }
-
-        string IDictionary<string, string>.this[string key]
-        {
-            get
-            {
-                string value;
-                if (_values.TryGetValue(key, out value))
-                {
-                    return value;
-                }
-
-                return null;
-            }
-            set
-            {
-                _values[key] = value;
-            }
-        }
-
-        ICollection<string> IDictionary<string, string>.Keys
-        {
-            get { return _values.Keys; }
-        }
-
-        ICollection<string> IDictionary<string, string>.Values
-        {
-            get { return _values.Values; }
         }
 
         private static string Escape(string value)
