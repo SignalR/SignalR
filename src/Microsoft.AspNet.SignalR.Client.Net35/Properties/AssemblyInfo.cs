a00582f6fa06da1015da1616dcf6df1727755bf4
DamianEdwards <damian@damianedwards.com>
2012-12-07 18:17:58 -0800

    Build clean-up - Merged silverlight, windows phone & WinRT projects into main solution - Deleted .NET 3.5 and Windows Phone 7.5 client & samples

--- src/Microsoft.AspNet.SignalR.Client.Net35/Properties/AssemblyInfo.cs
+++
@@ -1,6 +0,0 @@
-﻿// Copyright (c) Microsoft Open Technologies, Inc. All rights reserved. See License.md in the project root for license information.
-
-using System.Reflection;
-
-[assembly: AssemblyTitle("Microsoft.AspNet.SignalR.Client.Net35")]
-[assembly: AssemblyDescription(".NET 3.5 client for SignalR")]
