cae3c2603138adcf5eb963b7d786c033285af9a5
David Fowler <davidfowl@gmail.com>
2012-12-08 03:40:06 -0800

    Added self host sample using Microsoft.Owin.Hosting.

---
+++ samples/Microsoft.AspNet.Owin.Samples/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.AspNet.SignalR.Hubs;
+using Microsoft.AspNet.SignalR.Samples.Raw;
+using Microsoft.Owin.Hosting;
+using Owin;
+
+namespace Microsoft.AspNet.Owin.Samples
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            using (WebApplication.Start<Program>("http://localhost:8080/"))
+            {
+                Console.WriteLine("Server running at http://localhost:8080/");
+                Console.ReadLine();
+            }
+        }
+
+        public void Configuration(IAppBuilder app)
+        {
+            // add SignalR to pipeline
+            app.MapHubs("/signalr");
+
+            app.MapConnection<RawConnection>("/raw");
+        }
+    }
+
+    public class Chat : Hub
+    {
+        public void Send(string message)
+        {
+            Clients.All.send(message);
+        }
+    }
+
+}
