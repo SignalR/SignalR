965e9aea939b6f8627319832ff8fe3f1eeaf43e2
N. Taylor Mullen <nimullen@microsoft.com>
2012-12-14 12:11:07 -0800

    Changed the build.proj to auto start IIS and utilize a new MSBuild based start mechanism
    - Removed the old StartIIS.ps1 powershell script, this is now handled
    via msbuild.

--- tools/chutzpah/StartIIS.ps1
+++
@@ -1,7 +0,0 @@
-[System.Reflection.Assembly]::LoadFile("C:\Users\nimullen\Documents\GitHub\SignalR\tests\Microsoft.AspNet.SignalR.FunctionalTests\artifacts\Debug\bin\Microsoft.AspNet.SignalR.FunctionalTests.dll");
-[System.Reflection.Assembly]::LoadFile("C:\Users\nimullen\Documents\GitHub\SignalR\tests\Microsoft.AspNet.SignalR.FunctionalTests\artifacts\Debug\bin\Microsoft.AspNet.SignalR.Client.dll");
-[System.IO.Directory]::SetCurrentDirectory("C:\Users\nimullen\Documents\GitHub\SignalR\tests\Microsoft.AspNet.SignalR.Client.JS.Tests")
-
-$myHost = New-Object Microsoft.AspNet.SignalR.FunctionalTests.Infrastructure.IISExpressTestHost "C:\Users\nimullen\Documents\GitHub\SignalR\tests\Microsoft.AspNet.SignalR.Client.JS.Tests"
-
-$myHost.Initialize(15,120,10,1)
