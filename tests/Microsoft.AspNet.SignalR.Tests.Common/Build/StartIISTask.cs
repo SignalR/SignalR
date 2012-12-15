b9c332529b9c5c89990ac12381a13eb4ae913af1
N. Taylor Mullen <nimullen@microsoft.com>
2012-12-14 17:11:15 -0800

    Log error on failure to start iis.

--- tests/Microsoft.AspNet.SignalR.Tests.Common/Build/StartIISTask.cs
+++ tests/Microsoft.AspNet.SignalR.Tests.Common/Build/StartIISTask.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNet.SignalR.FunctionalTests.Infrastructure;
 using Microsoft.Build.Framework;
 using Microsoft.Build.Utilities;
+using System;
 
 namespace Microsoft.AspNet.SignalR.Tests.Common
 {
@@ -11,9 +12,17 @@ namespace Microsoft.AspNet.SignalR.Tests.Common
 
         public override bool Execute()
         {
-            var myHost = new IISExpressTestHost(HostLocation[0].ToString());
+            try
+            {
+                var myHost = new IISExpressTestHost(HostLocation[0].ToString());
 
-            myHost.Initialize(15, 120, 10, 1, false);
+                myHost.Initialize(15, 120, 10, 1, false);
+            }
+            catch(Exception ex)
+            {
+                Log.LogError(ex.ToString());
+                throw;
+            }
 
             return true;
         }
