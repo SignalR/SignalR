b246a7dede63a7e5e118a7b051832bd131f99148
N. Taylor Mullen <nimullen@microsoft.com>
2012-12-17 17:48:45 -0800

    Added a utility js file to allow for commonly used code such as createHubConnection

---
+++ tests/Microsoft.AspNet.SignalR.Client.JS.Tests/Scripts/test.utilities.js
@@ -0,0 +1,8 @@
+﻿testUtilities = {
+	createHubConnection: function () {
+		if (window.document.testUrl !== 'auto') {
+			return $.hubConnection(window.document.testUrl, { useDefaultPath: false });
+		}
+		return $.hubConnection('signalr', { useDefaultPath: false });
+	}
+};
