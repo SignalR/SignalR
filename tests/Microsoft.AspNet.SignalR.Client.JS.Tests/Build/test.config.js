f08463d26aa4aa57fa3afd45dc3a4963982e68cc
N. Taylor Mullen <nimullen@microsoft.com>
2012-12-17 17:47:58 -0800

    Removed functional utility from test.config.js
    - Going to refactor this into a utility file

--- tests/Microsoft.AspNet.SignalR.Client.JS.Tests/Build/test.config.js
+++ tests/Microsoft.AspNet.SignalR.Client.JS.Tests/Build/test.config.js
@@ -2,11 +2,4 @@
     // These values are updated via the csproj based on configuration values passed into its build
     window.document.testUrl = /*URL*/'auto'/*URL*/;
     window.document.commandLineTest = /*CMDLineTest*/false/*CMDLineTest*/;
-
-    window.createHubConnection = function () {
-        if (window.document.testUrl !== 'auto') {
-            return $.hubConnection(window.document.testUrl, { useDefaultPath: false });
-        }
-        return $.hubConnection('signalr', { useDefaultPath: false });
-    };
 })($, window);
