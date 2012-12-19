6794e1fa814742409d74b9a044ff0bb3eff4db58
Stephen Halter <halter73@gmail.com>
2012-12-18 20:13:14 -0800

    Fixed issue where assertions in asyncTimeoutTests could fail later tests
    https://github.com/SignalR/SignalR/pull/1160#discussion_r2459049

--- tests/Microsoft.AspNet.SignalR.Client.JS.Tests/Tests/FunctionalTests/Transports/LongPolling/LPSendFacts.js
+++ tests/Microsoft.AspNet.SignalR.Client.JS.Tests/Tests/FunctionalTests/Transports/LongPolling/LPSendFacts.js
@@ -1,6 +1,6 @@
 ﻿module("Long Polling Facts");
 
-QUnit.asyncTimeoutTest("Long Polling can send ", 5000, function (end) {
+QUnit.asyncTimeoutTest("Long Polling can send ", 5000, function (end, assert) {
     var connection = testUtilities.createHubConnection(),
         proxies = connection.createHubProxies(),
         demo = proxies.demo;
@@ -10,11 +10,11 @@ QUnit.asyncTimeoutTest("Long Polling can send ", 5000, function (end) {
 
     connection.start().done(function () {
         demo.server.overload(6).done(function (val) {
-            equal(val, 6, "Successful return value from server via send");
+            assert.equal(val, 6, "Successful return value from server via send");
             end();
         });
     }).fail(function (reason) {
-        ok(false, "Failed to initiate signalr connection");
+        assert.ok(false, "Failed to initiate signalr connection");
         end();
     });
 
