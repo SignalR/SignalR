f2799610ed3a41a650ea3642dfbdfaff38914ff0
NTaylorMullen <NTaylorMullen@outlook.com>
2012-12-19 18:23:59 -0800

    Added several more reconnecting facts for long polling
    - These are to verify the #1133 fix

--- tests/Microsoft.AspNet.SignalR.Client.JS.Tests/Tests/FunctionalTests/Transports/LongPolling/LPReconnectFacts.js
+++ tests/Microsoft.AspNet.SignalR.Client.JS.Tests/Tests/FunctionalTests/Transports/LongPolling/LPReconnectFacts.js
@@ -40,3 +40,122 @@ QUnit.asyncTimeoutTest("Long Polling transport can reconnect.", 50000, function 
         connection.stop();
     };
 });
+
+QUnit.asyncTimeoutTest("Long Polling transport shifts into reconnecting state.", 50000, function (end) {
+    var connection = testUtilities.createHubConnection(),
+        demo = connection.createHubProxies().demo,
+        tryReconnect = function () {
+            if (connection.pollXhr.readyState !== 1) {
+                setTimeout(tryReconnect, 200);
+            }
+            else {
+                connection.pollXhr.abort("foo");
+            }
+        };
+
+    // Need to have at least one client function in order to be subscribed to a hub
+    demo.client.TestGuid = function () {
+        // Wire up the state changed (while connected) to detect if we connect again
+        // In a later test we'll determine if reconnected gets called
+        connection.stateChanged(function () {
+            if (connection.state == $.signalR.connectionState.reconnecting) {
+                ok(true, "Connection now in reconnecting state.");
+                end();
+            }
+        });
+
+        tryReconnect();
+    };
+
+    connection.start({ transport: 'longPolling' }).done(function () {
+        ok(true, "Connected");
+        // Call a server function and request a message back in order to get a message ID so we can successfully reconnect
+        demo.server.testGuid();
+    }).fail(function (reason) {
+        ok(false, "Failed to initiate signalr connection");
+        end();
+    });
+
+    // Cleanup
+    return function () {
+        connection.stop();
+    };
+});
+
+QUnit.asyncTimeoutTest("Long Polling transport triggers reconnecting.", 50000, function (end) {
+    var connection = testUtilities.createHubConnection(),
+        demo = connection.createHubProxies().demo,
+        tryReconnect = function () {
+            if (connection.pollXhr.readyState !== 1) {
+                setTimeout(tryReconnect, 200);
+            }
+            else {
+                connection.pollXhr.abort("foo");
+            }
+        };
+
+    // Need to have at least one client function in order to be subscribed to a hub
+    demo.client.TestGuid = function () {
+        // Wire up the state changed (while connected) to detect if we connect again
+        // In a later test we'll determine if reconnected gets called
+        connection.reconnecting(function () {
+            ok(true, "Reconnecting triggered!");
+            end();
+        });
+
+        tryReconnect();
+    };
+
+    connection.start({ transport: 'longPolling' }).done(function () {
+        ok(true, "Connected");
+        // Call a server function and request a message back in order to get a message ID so we can successfully reconnect
+        demo.server.testGuid();
+    }).fail(function (reason) {
+        ok(false, "Failed to initiate signalr connection");
+        end();
+    });
+
+    // Cleanup
+    return function () {
+        connection.stop();
+    };
+});
+
+QUnit.asyncTimeoutTest("Long Polling transport triggers reconnected.", 50000, function (end) {
+    var connection = testUtilities.createHubConnection(),
+        demo = connection.createHubProxies().demo,
+        tryReconnect = function () {
+            if (connection.pollXhr.readyState !== 1) {
+                setTimeout(tryReconnect, 200);
+            }
+            else {
+                connection.pollXhr.abort("foo");
+            }
+        };
+
+    // Need to have at least one client function in order to be subscribed to a hub
+    demo.client.TestGuid = function () {
+        // Wire up the state changed (while connected) to detect if we connect again
+        // In a later test we'll determine if reconnected gets called
+        connection.reconnected(function () {
+            ok(true, "Reconnected triggered!");
+            end();
+        });
+
+        tryReconnect();
+    };
+
+    connection.start({ transport: 'longPolling' }).done(function () {
+        ok(true, "Connected");
+        // Call a server function and request a message back in order to get a message ID so we can successfully reconnect
+        demo.server.testGuid();
+    }).fail(function (reason) {
+        ok(false, "Failed to initiate signalr connection");
+        end();
+    });
+
+    // Cleanup
+    return function () {
+        connection.stop();
+    };
+});
