44929cae645c62163a759316a72867302118bd1d
NTaylorMullen <NTaylorMullen@outlook.com>
2012-12-21 15:33:44 -0800

    Adding scoping to the test utilities
    - Will be adding more inside the scope

--- tests/Microsoft.AspNet.SignalR.Client.JS.Tests/Scripts/test.utilities.js
+++ tests/Microsoft.AspNet.SignalR.Client.JS.Tests/Scripts/test.utilities.js
@@ -1,16 +1,20 @@
-﻿testUtilities = {
-    createHubConnection: function () {
-        var connection;
+﻿var testUtilities;
 
-        if (window.document.testUrl !== 'auto') {
-            connection = $.hubConnection(window.document.testUrl, { useDefaultPath: false });
+(function ($, window) {
+    testUtilities = {
+        createHubConnection: function () {
+            var connection;
+
+            if (window.document.testUrl !== 'auto') {
+                connection = $.hubConnection(window.document.testUrl, { useDefaultPath: false });
+            }
+            else {
+                connection = $.hubConnection('signalr', { useDefaultPath: false });
+            }
+
+            connection.logging = true;
+
+            return connection;
         }
-        else {
-            connection = $.hubConnection('signalr', { useDefaultPath: false });
-        }
-
-        connection.logging = true;
-
-        return connection;
-    }
-};
+    };
+})($, window);
