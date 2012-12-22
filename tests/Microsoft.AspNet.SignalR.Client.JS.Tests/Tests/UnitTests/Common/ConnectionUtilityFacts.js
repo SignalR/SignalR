11d242e85d511005fc536c2d6e25e1487654df95
NTaylorMullen <NTaylorMullen@outlook.com>
2012-12-21 16:14:19 -0800

    Modified the isCrossDomain function to override the disabledCrossDomain function to allow it.

--- tests/Microsoft.AspNet.SignalR.Client.JS.Tests/Tests/UnitTests/Common/ConnectionUtilityFacts.js
+++ tests/Microsoft.AspNet.SignalR.Client.JS.Tests/Tests/UnitTests/Common/ConnectionUtilityFacts.js
@@ -8,6 +8,10 @@ QUnit.test("Is Cross Domain functions properly", function () {
             host: "localhost"
         };
 
+    // Turn off the disabled cross domain (this only applies for command line testin)
+    // We want to accurately test the isCrossDomain function so we need to be sure it's
+    // enabled.
+    window.document.crossDomainDisabled = false;
     equal(false, con.fn.isCrossDomain(dummyURLBase + ":80/signalr", against), "Port 80 with stuff after is not cross domain.");
     equal(true, con.fn.isCrossDomain(dummyURLBase + ":8080/signalr", against), "Port 8080 still is cross domain with stuff after it");
     equal(true, con.fn.isCrossDomain(dummyURLBase + ":8080", against), "Port 8080 still is cross domain by itself");
@@ -15,4 +19,5 @@ QUnit.test("Is Cross Domain functions properly", function () {
     equal(false, con.fn.isCrossDomain(dummyURLBase + ":80/", against), "Port 80 with just a slash at the end is not cross domain");
     equal(false, con.fn.isCrossDomain(against.protocol + "//" + against.host, against), "Comparing against the current url should never be cross domain.");
     equal(true, con.fn.isCrossDomain("http://www.microsoft.com", against), "A completely different website should always be cross domain.");
+    window.document.crossDomainDisabled = true;
 });
