8bb682bbc3f4e5e79d9393e157793387e53ed4ad
NTaylorMullen <NTaylorMullen@outlook.com>
2013-01-02 17:10:57 -0800

    Added tests to verify the new 443 https port stripping functionality
    #1156

--- tests/Microsoft.AspNet.SignalR.Client.JS.Tests/Tests/UnitTests/Common/ConnectionUtilityFacts.js
+++ tests/Microsoft.AspNet.SignalR.Client.JS.Tests/Tests/UnitTests/Common/ConnectionUtilityFacts.js
@@ -17,6 +17,11 @@ QUnit.test("Is Cross Domain functions properly", function () {
     equal(true, con.fn.isCrossDomain(dummyURLBase + ":8080", against), "Port 8080 still is cross domain by itself");
     equal(false, con.fn.isCrossDomain(dummyURLBase + ":80", against), "Port 80 by itself is not cross domain");
     equal(false, con.fn.isCrossDomain(dummyURLBase + ":80/", against), "Port 80 with just a slash at the end is not cross domain");
+    equal(false, con.fn.isCrossDomain(dummyURLBase + ":443/signalr", against), "Port 443 with stuff after is not cross domain.");
+    equal(true, con.fn.isCrossDomain(dummyURLBase + ":4430/signalr", against), "Port 443 still is cross domain with stuff after it");
+    equal(true, con.fn.isCrossDomain(dummyURLBase + ":4430", against), "Port 443 still is cross domain by itself");
+    equal(false, con.fn.isCrossDomain(dummyURLBase + ":443", against), "Port 443 by itself is not cross domain");
+    equal(false, con.fn.isCrossDomain(dummyURLBase + ":443/", against), "Port 443 with just a slash at the end is not cross domain");
     equal(false, con.fn.isCrossDomain(against.protocol + "//" + against.host, against), "Comparing against the current url should never be cross domain.");
     equal(true, con.fn.isCrossDomain("http://www.microsoft.com", against), "A completely different website should always be cross domain.");
     window.document.crossDomainDisabled = true;
