b59edb8d20bf556c6a7d54f7b44c86121874c3a9
David Fowler <davidfowl@gmail.com>
2013-01-03 20:56:44 -0800

    Fixed the comments

--- src/Microsoft.AspNet.SignalR.SystemWeb/RouteExtensions.cs
+++ src/Microsoft.AspNet.SignalR.SystemWeb/RouteExtensions.cs
@@ -17,11 +17,8 @@ namespace System.Web.Routing
         /// <param name="routes">The route table</param>
         /// <typeparam name="T">The type of <see cref="PersistentConnection"/></typeparam>
         /// <param name="name">The name of the route</param>
-        /// <param name="url">path pattern of the route. Should end with catch-all parameter.</param>
+        /// <param name="url">path prefix of the route.</param>
         /// <returns>The registered route</returns>
-        /// <example>
-        /// routes.MapConnection{MyConnection}("echo", "echo/{*operation}");
-        /// </example>
         [SuppressMessage("Microsoft.Design", "CA1004:GenericMethodsShouldProvideTypeParameter", Justification = "The type parameter is syntactic sugar")]
         public static RouteBase MapConnection<T>(this RouteCollection routes, string name, string url) where T : PersistentConnection
         {
@@ -34,7 +31,7 @@ namespace System.Web.Routing
         /// <param name="routes">The route table</param>
         /// <typeparam name="T">The type of <see cref="PersistentConnection"/></typeparam>
         /// <param name="name">The name of the route</param>
-        /// <param name="url">path pattern of the route. Should end with catch-all parameter.</param>
+        /// <param name="url">path prefix of the route.</param>
         /// <param name="resolver">The dependency resolver to use for this connection</param>
         /// <returns>The registered route</returns>
         [SuppressMessage("Microsoft.Design", "CA1004:GenericMethodsShouldProvideTypeParameter", Justification = "The type parameter is syntactic sugar")]
@@ -49,7 +46,7 @@ namespace System.Web.Routing
         /// <param name="routes">The route table</param>
         /// <param name="type">The type of <see cref="PersistentConnection"/></param>
         /// <param name="name">The name of the route</param>
-        /// <param name="url">path pattern of the route. Should end with catch-all parameter.</param>
+        /// <param name="url">path prefix of the route.</param>
         /// <returns>The registered route</returns>
         public static RouteBase MapConnection(this RouteCollection routes, string name, string url, Type type)
         {
@@ -81,7 +78,7 @@ namespace System.Web.Routing
         /// Changes the default hub route from /signalr to a specified path.
         /// </summary>
         /// <param name="routes">The route table</param>
-        /// <param name="url">The path of the hubs route. This should *NOT* contain catch-all parameter.</param>
+        /// <param name="url">The path prefix of the hubs route.</param>
         /// <returns>The registered route</returns>
         public static RouteBase MapHubs(this RouteCollection routes, string url)
         {
@@ -92,7 +89,7 @@ namespace System.Web.Routing
         /// Changes the default hub route from /signalr to a specified path.
         /// </summary>
         /// <param name="routes">The route table</param>
-        /// <param name="url">The path of the hubs route. This should *NOT* contain catch-all parameter.</param>
+        /// <param name="url">The path prefix of the hubs route.</param>
         /// <param name="resolver">The dependency resolver to use for the hub connection</param>
         /// <returns>The registered route</returns>
         public static RouteBase MapHubs(this RouteCollection routes, string url, IDependencyResolver resolver)
@@ -124,7 +121,7 @@ namespace System.Web.Routing
         /// </summary>        
         /// <param name="routes">The route table</param>
         /// <param name="name">The name of the route</param>
-        /// <param name="url">path pattern of the route. Should end with catch-all parameter.</param>
+        /// <param name="url">path prefix of the route.</param>
         /// <param name="type">The type of <see cref="PersistentConnection"/></param>
         /// <param name="resolver">The dependency resolver to use for the hub connection</param>
         /// <returns>The registered route</returns>
