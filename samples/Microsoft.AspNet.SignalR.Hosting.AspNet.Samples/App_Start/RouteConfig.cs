da9d04246a9e1b140881e62df51bb52a00d9f7ca
Stephen Halter <halter73@gmail.com>
2013-01-11 21:13:23 -0800

    Updated the ASP.NET sample to enable detailed hub errors.

--- samples/Microsoft.AspNet.SignalR.Hosting.AspNet.Samples/App_Start/RouteConfig.cs
+++ samples/Microsoft.AspNet.SignalR.Hosting.AspNet.Samples/App_Start/RouteConfig.cs
@@ -14,7 +14,7 @@ namespace Microsoft.AspNet.SignalR.Samples
             routes.MapConnection<StreamingConnection>("streaming-connection", "streaming-connection");
 
             // Register the default hubs route /signalr
-            routes.MapHubs();
+            routes.MapHubs(new HubConfiguration() { EnableDetailedHubErrors = true });
         }
     }
 }
