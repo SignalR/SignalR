a8f2f28d3960fcce7468968bf3e01ec2b9332727
David Fowler <davidfowl@gmail.com>
2013-01-12 17:37:38 -0800

    Made scope in DefaultProtectedData configurable. - Default is per User.

--- src/Microsoft.AspNet.SignalR.Core/Infrastructure/DefaultProtectedData.cs
+++ src/Microsoft.AspNet.SignalR.Core/Infrastructure/DefaultProtectedData.cs
@@ -6,13 +6,25 @@ namespace Microsoft.AspNet.SignalR.Infrastructure
 {
     public class DefaultProtectedData : IProtectedData
     {
+        private readonly DataProtectionScope _scope;
+
+        public DefaultProtectedData()
+            : this(DataProtectionScope.CurrentUser)
+        {
+        }
+
+        public DefaultProtectedData(DataProtectionScope scope)
+        {
+            _scope = scope;
+        }
+
         public string Protect(string data, string purpose)
         {
             byte[] purposeBytes = Encoding.UTF8.GetBytes(purpose);
 
             byte[] unprotectedBytes = Encoding.UTF8.GetBytes(data);
 
-            byte[] protectedBytes = ProtectedData.Protect(unprotectedBytes, purposeBytes, DataProtectionScope.LocalMachine);
+            byte[] protectedBytes = ProtectedData.Protect(unprotectedBytes, purposeBytes, _scope);
 
             return Convert.ToBase64String(protectedBytes);
         }
@@ -23,7 +35,7 @@ namespace Microsoft.AspNet.SignalR.Infrastructure
 
             byte[] protectedBytes = Convert.FromBase64String(protectedValue);
 
-            byte[] unprotectedBytes = ProtectedData.Unprotect(protectedBytes, purposeBytes, DataProtectionScope.LocalMachine);
+            byte[] unprotectedBytes = ProtectedData.Unprotect(protectedBytes, purposeBytes, _scope);
 
             return Encoding.UTF8.GetString(unprotectedBytes);
         }
