2b99a204a6b1892179351990dae9ad5765debfcc
NTaylorMullen <NTaylorMullen@outlook.com>
2013-01-18 16:31:37 -0800

    Added ping server facts for all transports
    - Also changed naming of Ping Server Facts module names to group all
    PingServerFacts

--- tests/Microsoft.AspNet.SignalR.Client.JS.Tests/Tests/FunctionalTests/Common/PingServerFacts.js
+++ tests/Microsoft.AspNet.SignalR.Client.JS.Tests/Tests/FunctionalTests/Common/PingServerFacts.js
@@ -1,4 +1,4 @@
-﻿QUnit.module("Ping Server Facts - Long Polling", testUtilities.longPollingEnabled);
+﻿QUnit.module("Ping Server Facts", testUtilities.longPollingEnabled);
 
 QUnit.asyncTimeoutTest("Long Polling transport can initiate Ping Server.", testUtilities.defaultTestTimeout, function (end, assert) {
     var connection = testUtilities.createHubConnection(),
@@ -29,7 +29,38 @@ QUnit.asyncTimeoutTest("Long Polling transport can initiate Ping Server.", testU
     };
 });
 
-QUnit.module("Ping Server Facts - Server Sent Events", testUtilities.serverSentEventsEnabled);
+QUnit.module("Ping Server Facts", testUtilities.foreverFrameEnabled);
+
+QUnit.asyncTimeoutTest("Forever Frame transport can initiate Ping Server.", testUtilities.defaultTestTimeout, function (end, assert) {
+    var connection = testUtilities.createHubConnection(),
+        testPingServer = function () {
+            $.signalR.transports._logic.pingServer(connection, "foreverFrame").done(function () {
+                // Successful ping
+                assert.ok(true, "Successful ping with Forever Frame");
+                end();
+            }).fail(function () {
+                assert.ok(false, "Failed to ping server with Forever Frame");
+                end();
+            });
+        };
+
+    // Starting/Stopping a connection to have it instantiated with all the appropriate variables
+    connection.start({ transport: "foreverFrame" }).done(function () {
+        assert.ok(true, "Connected");
+        connection.stop();
+        testPingServer();
+    }).fail(function (reason) {
+        assert.ok(false, "Failed to initiate signalr connection");
+        end();
+    });
+
+    // Cleanup
+    return function () {
+        connection.stop();
+    };
+});
+
+QUnit.module("Ping Server Facts", testUtilities.serverSentEventsEnabled);
 
 QUnit.asyncTimeoutTest("Server Sent Events transport can initiate Ping Server.", testUtilities.defaultTestTimeout, function (end, assert) {
     var connection = testUtilities.createHubConnection(),
@@ -46,6 +77,37 @@ QUnit.asyncTimeoutTest("Server Sent Events transport can initiate Ping Server.",
 
     // Starting/Stopping a connection to have it instantiated with all the appropriate variables
     connection.start({ transport: "serverSentEvents" }).done(function () {
+        assert.ok(true, "Connected");
+        connection.stop();
+        testPingServer();
+    }).fail(function (reason) {
+        assert.ok(false, "Failed to initiate signalr connection");
+        end();
+    });
+
+    // Cleanup
+    return function () {
+        connection.stop();
+    };
+});
+
+QUnit.module("Ping Server Facts", testUtilities.webSocketsEnabled);
+
+QUnit.asyncTimeoutTest("Web Sockets transport can initiate Ping Server.", testUtilities.defaultTestTimeout, function (end, assert) {
+    var connection = testUtilities.createHubConnection(),
+        testPingServer = function () {
+            $.signalR.transports._logic.pingServer(connection, "webSockets").done(function () {
+                // Successful ping
+                assert.ok(true, "Successful ping with Web Sockets");
+                end();
+            }).fail(function () {
+                assert.ok(false, "Failed to ping server with Web Sockets");
+                end();
+            });
+        };
+
+    // Starting/Stopping a connection to have it instantiated with all the appropriate variables
+    connection.start({ transport: "webSockets" }).done(function () {
         assert.ok(true, "Connected");
         connection.stop();
         testPingServer();
