6d0f11e50abab0f1f395af867d16955b5f5c4fda
NTaylorMullen <NTaylorMullen@outlook.com>
2013-01-21 16:18:43 -0800

    Finished ajax oriented functional and unit based tests for the common transports

---
+++ tests/Microsoft.AspNet.SignalR.Client.JS.Tests/Tests/FunctionalTests/Common/AjaxAbortFacts.js
@@ -0,0 +1,112 @@
+﻿QUnit.module("Transports Common - Ajax Abort Facts", testUtilities.longPollingEnabled);
+
+QUnit.asyncTimeoutTest("Long Polling transport can trigger abort on server via ajaxAbort.", testUtilities.defaultTestTimeout, function (end, assert) {
+    var connection1 = testUtilities.createHubConnection(),
+        connection2 = testUtilities.createHubConnection(),
+        statushub1 = connection1.createHubProxies().StatusHub,
+        statushub2 = connection2.createHubProxies().StatusHub,
+        transport = { transport: "longPolling" };
+
+    // Need to register at least 1 callback in order to subscribe to hub.
+    statushub1.client.foo = function () { };
+
+    statushub2.client.leave = function () {
+        assert.ok(true, "Ajax Abort (disconnect) successfully received on the server");
+        end();
+    };
+
+    // Start both connections
+    connection1.start(transport).done(function () {
+        connection2.start(transport).done(function () {
+            $.signalR.transports._logic.ajaxAbort(connection1);
+        }).fail(function (reason) {
+            assert.ok(false, "Failed to initiate signalr connection2");
+            end();
+        });
+    }).fail(function (reason) {
+        assert.ok(false, "Failed to initiate signalr connection1");
+        end();
+    });
+
+    // Cleanup
+    return function () {
+        connection1.stop();
+        connection2.stop();
+    };
+});
+
+QUnit.module("Transports Common - Ajax Abort Facts", testUtilities.foreverFrameEnabled);
+
+QUnit.asyncTimeoutTest("Forever Frame transport can trigger abort on server via ajaxAbort.", testUtilities.defaultTestTimeout, function (end, assert) {
+    var connection1 = testUtilities.createHubConnection(),
+        connection2 = testUtilities.createHubConnection(),
+        statushub1 = connection1.createHubProxies().StatusHub,
+        statushub2 = connection2.createHubProxies().StatusHub,
+        transport = { transport: "foreverFrame" };
+
+    // Need to register at least 1 callback in order to subscribe to hub.
+    statushub1.client.foo = function () { };
+
+    statushub2.client.leave = function () {
+        assert.ok(true, "Ajax Abort (disconnect) successfully received on the server");
+        end();
+    };
+
+    // Start both connections
+    connection1.start(transport).done(function () {
+        connection2.start(transport).done(function () {
+            $.signalR.transports._logic.ajaxAbort(connection1);
+        }).fail(function (reason) {
+            assert.ok(false, "Failed to initiate signalr connection2");
+            end();
+        });
+    }).fail(function (reason) {
+        assert.ok(false, "Failed to initiate signalr connection1");
+        end();
+    });
+
+    // Cleanup
+    return function () {
+        connection1.stop();
+        connection2.stop();
+    };
+});
+
+QUnit.module("Transports Common - Ajax Abort Facts", testUtilities.serverSentEventsEnabled);
+
+QUnit.asyncTimeoutTest("Server Sent Events transport can trigger abort on server via ajaxAbort.", testUtilities.defaultTestTimeout, function (end, assert) {
+    var connection1 = testUtilities.createHubConnection(),
+        connection2 = testUtilities.createHubConnection(),
+        statushub1 = connection1.createHubProxies().StatusHub,
+        statushub2 = connection2.createHubProxies().StatusHub,
+        transport = { transport: "serverSentEvents" };
+
+    // Need to register at least 1 callback in order to subscribe to hub.
+    statushub1.client.foo = function () { };
+
+    statushub2.client.leave = function () {
+        assert.ok(true, "Ajax Abort (disconnect) successfully received on the server");
+        end();
+    };
+
+    // Start both connections
+    connection1.start(transport).done(function () {
+        connection2.start(transport).done(function () {
+            $.signalR.transports._logic.ajaxAbort(connection1);
+        }).fail(function (reason) {
+            assert.ok(false, "Failed to initiate signalr connection2");
+            end();
+        });
+    }).fail(function (reason) {
+        assert.ok(false, "Failed to initiate signalr connection1");
+        end();
+    });
+
+    // Cleanup
+    return function () {
+        connection1.stop();
+        connection2.stop();
+    };
+});
+
+// Web Sockets uses a duplex stream for sending content, thus does not use the ajax methods
---
+++ tests/Microsoft.AspNet.SignalR.Client.JS.Tests/Tests/UnitTests/Common/AjaxFacts.js
@@ -0,0 +1,48 @@
+﻿QUnit.module("Transports Common - Ajax Abort Facts");
+
+QUnit.test("No transport specified should result in noop behavior for ajaxAbort.", function () {
+    var connection = testUtilities.createHubConnection(),
+        failed = false;
+
+    connection.log = function () {
+        QUnit.ok(false, "ajaxAbort did not noop with an unspecified transport on the connection object.");
+        failed = true;
+    };
+
+    $.signalR.transports._logic.ajaxAbort(connection);
+
+    if (!failed) {
+        QUnit.ok(true, "ajaxAbort noop'd, success!");
+    }
+});
+
+QUnit.test("Asynchronous flag defaults and is used correctly in ajaxAbort", function () {
+    var connection = testUtilities.createHubConnection(),
+        failed = false,
+        // Saved current jquery ajax function so we can replace it once we've finished
+        savedAjax = $.ajax,
+        expectedAsync = true;
+
+    connection.transport = "foo";
+
+    $.ajax = function (obj) {
+        if (obj.async === expectedAsync) {
+            assert.ok(true, "Correct async value passed, expected: " + expectedAsync);
+        }
+        else {
+            assert.ok(false, "Incorrect async value passed, expected: " + expectedAsync);
+        }
+    };
+
+    expectedAsync = true;
+    $.signalR.transports._logic.ajaxAbort(connection);
+
+    expectedAsync = true;
+    $.signalR.transports._logic.ajaxAbort(connection, true);
+
+    expectedAsync = false;
+    $.signalR.transports._logic.ajaxAbort(connection, false);
+
+    // Put the $.ajax function back where it was supposed to be
+    $.ajax = savedAjax;
+});
