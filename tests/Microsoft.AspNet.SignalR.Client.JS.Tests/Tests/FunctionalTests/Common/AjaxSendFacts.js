e0f24a77e567d0ea677171cc8b236fc77ae7014a
NTaylorMullen <NTaylorMullen@outlook.com>
2013-01-30 13:24:56 -0800

    Went through common transports folder and combined all multi-transport tests into singular tests using runWithAllTransports
    #1455

--- tests/Microsoft.AspNet.SignalR.Client.JS.Tests/Tests/FunctionalTests/Common/AjaxSendFacts.js
+++ tests/Microsoft.AspNet.SignalR.Client.JS.Tests/Tests/FunctionalTests/Common/AjaxSendFacts.js
@@ -1,121 +1,43 @@
-﻿QUnit.module("Transports Common - Ajax Send Facts", testUtilities.transports.longPolling.enabled);
+﻿QUnit.module("Transports Common - Ajax Send Facts");
 
-QUnit.asyncTimeoutTest("Long Polling transport can trigger method on server via ajaxSend.", testUtilities.defaultTestTimeout, function (end, assert, testName) {
-    var connection = testUtilities.createHubConnection(testName),
-        demo = connection.createHubProxies().demo;
+testUtilities.runWithTransports(["longPolling", "foreverFrame", "serverSentEvents"], function (transport) {
+    QUnit.asyncTimeoutTest(transport + " transport can trigger method on server via ajaxSend.", testUtilities.defaultTestTimeout, function (end, assert, testName) {
+        var connection = testUtilities.createHubConnection(testName),
+            demo = connection.createHubProxies().demo;
 
-    // Must subscribe to at least one method on client
-    demo.client.foo = function () { };
+        // Must subscribe to at least one method on client
+        demo.client.foo = function () { };
 
-    connection.received(function (result) {
-        if (result.R === 6) {
-            assert.ok(true, "Result successfully received from server via ajaxSend");
-        }
-        else {
-            assert.ok(false, "Invalid result returned from server via ajaxSend");
-        }
+        connection.received(function (result) {
+            if (result.R === 6) {
+                assert.ok(true, "Result successfully received from server via ajaxSend");
+            }
+            else {
+                assert.ok(false, "Invalid result returned from server via ajaxSend");
+            }
 
-        end();
-    });
+            end();
+        });
 
-    connection.start({ transport: "longPolling" }).done(function () {
-        var data = {
-            H: "demo",
-            M: "Overload",
-            A: [6],
-            I: 1
+        connection.start({ transport: transport }).done(function () {
+            var data = {
+                H: "demo",
+                M: "Overload",
+                A: [6],
+                I: 1
+            };
+
+            $.signalR.transports._logic.ajaxSend(connection, JSON.stringify(data));
+        }).fail(function (reason) {
+            assert.ok(false, "Failed to initiate SignalR connection");
+            end();
+        });
+
+        // Cleanup
+        return function () {
+            connection.stop();
         };
-
-        $.signalR.transports._logic.ajaxSend(connection,JSON.stringify(data));
-    }).fail(function (reason) {
-        assert.ok(false, "Failed to initiate SignalR connection");
-        end();
     });
-
-    // Cleanup
-    return function () {
-        connection.stop();
-    };
-});
-
-QUnit.module("Transports Common - Ajax Send Facts", testUtilities.transports.foreverFrame.enabled);
-
-QUnit.asyncTimeoutTest("Forever Frame transport can trigger method on server via ajaxSend.", testUtilities.defaultTestTimeout, function (end, assert, testName) {
-    var connection = testUtilities.createHubConnection(testName),
-        demo = connection.createHubProxies().demo;
-
-    // Must subscribe to at least one method on client
-    demo.client.foo = function () { };
-
-    connection.received(function (result) {
-        if (result.R === 6) {
-            assert.ok(true, "Result successfully received from server via ajaxSend");
-        }
-        else {
-            assert.ok(false, "Invalid result returned from server via ajaxSend");
-        }
-
-        end();
-    });
-
-    connection.start({ transport: "foreverFrame" }).done(function () {
-        var data = {
-            H: "demo",
-            M: "Overload",
-            A: [6],
-            I: 1
-        };
-
-        $.signalR.transports._logic.ajaxSend(connection, JSON.stringify(data));
-    }).fail(function (reason) {
-        assert.ok(false, "Failed to initiate SignalR connection");
-        end();
-    });
-
-    // Cleanup
-    return function () {
-        connection.stop();
-    };
-});
-
-QUnit.module("Transports Common - Ajax Send Facts", testUtilities.transports.serverSentEvents.enabled);
-
-QUnit.asyncTimeoutTest("Server Sent Events transport can trigger method on server via ajaxSend.", testUtilities.defaultTestTimeout, function (end, assert, testName) {
-    var connection = testUtilities.createHubConnection(testName),
-        demo = connection.createHubProxies().demo;
-
-    // Must subscribe to at least one method on client
-    demo.client.foo = function () { };
-
-    connection.received(function (result) {
-        if (result.R === 6) {
-            assert.ok(true, "Result successfully received from server via ajaxSend");
-        }
-        else {
-            assert.ok(false, "Invalid result returned from server via ajaxSend");
-        }
-
-        end();
-    });
-
-    connection.start({ transport: "serverSentEvents" }).done(function () {
-        var data = {
-            H: "demo",
-            M: "Overload",
-            A: [6],
-            I: 1
-        };
-
-        $.signalR.transports._logic.ajaxSend(connection, JSON.stringify(data));
-    }).fail(function (reason) {
-        assert.ok(false, "Failed to initiate SignalR connection");
-        end();
-    });
-
-    // Cleanup
-    return function () {
-        connection.stop();
-    };
 });
 
 // WebSockets uses a duplex stream for sending content, thus does not use the ajax methods
