a066d1ada1c585ac9453fe81245d287d95be484f
NTaylorMullen <NTaylorMullen@outlook.com>
2013-01-30 13:24:09 -0800

    Added a "All" folder under transports and combined all multi-transport tests into singular files which use runWithAllTransports etc.
    - Also renamed the "Auto" folder to "Fallback"
    
    #1455

---
+++ tests/Microsoft.AspNet.SignalR.Client.JS.Tests/Tests/FunctionalTests/Transports/All/ConnectionFacts.js
@@ -0,0 +1,45 @@
+﻿QUnit.module("Connection Facts");
+
+testUtilities.runWithAllTransports(function (transport) {
+    QUnit.asyncTimeoutTest(transport + " transport an connect.", testUtilities.defaultTestTimeout, function (end, assert, testName) {
+        var connection = testUtilities.createHubConnection(testName);
+
+        connection.start({ transport: transport }).done(function () {
+            assert.ok(true, "Connected");
+            end();
+        }).fail(function (reason) {
+            assert.ok(false, "Failed to initiate SignalR connection");
+            end();
+        });
+
+        // Cleanup
+        return function () {
+            connection.stop();
+        };
+    });
+
+    QUnit.asyncTimeoutTest(transport + " transport can receive messages on connect.", testUtilities.defaultTestTimeout, function (end, assert, testName) {
+        var connection = testUtilities.createConnection("multisend", testName),
+            values = [];
+
+        connection.received(function (data) {
+            values.push(data);
+
+            if (values.length === 2) {
+                assert.equal(values[0], "OnConnectedAsync1", "Received OnConnectedAsync1");
+                assert.equal(values[1], "OnConnectedAsync2", "Received OnConnectedAsync2");
+                end();
+            }
+        });
+
+        connection.start({ transport: transport }).fail(function (reason) {
+            assert.ok(false, "Failed to initiate SignalR connection");
+            end();
+        });
+
+        // Cleanup
+        return function () {
+            connection.stop();
+        };
+    });
+});
---
+++ tests/Microsoft.AspNet.SignalR.Client.JS.Tests/Tests/FunctionalTests/Transports/Fallback/FallbackConnectionFacts.js
@@ -0,0 +1,18 @@
+﻿QUnit.module("Fallback Facts");
+
+QUnit.asyncTimeoutTest("Default transports fall back and connect.", testUtilities.defaultTestTimeout, function (end, assert, testName) {
+    var connection = testUtilities.createHubConnection(testName);
+
+    connection.start().done(function () {
+        assert.ok(true, "Connected");
+        end();
+    }).fail(function (reason) {
+        assert.ok(false, "Failed to initiate SignalR connection");
+        end();
+    });
+
+    // Cleanup
+    return function () {
+        connection.stop();
+    };
+});
