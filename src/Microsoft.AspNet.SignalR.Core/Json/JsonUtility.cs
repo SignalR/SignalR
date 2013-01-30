a0e4eb120e11a51f722f6f3173791a596f68f518
DamianEdwards <damian@damianedwards.com>
2013-01-29 19:18:57 -0800

    Validate that JSONP callback method is a JS identifier - #1444

--- src/Microsoft.AspNet.SignalR.Core/Json/JsonUtility.cs
+++ src/Microsoft.AspNet.SignalR.Core/Json/JsonUtility.cs
@@ -1,6 +1,7 @@
 ﻿// Copyright (c) Microsoft Open Technologies, Inc. All rights reserved. See License.md in the project root for license information.
 
 using System;
+using System.CodeDom.Compiler;
 using System.Diagnostics.CodeAnalysis;
 using System.Globalization;
 using System.Linq;
@@ -13,6 +14,8 @@ namespace Microsoft.AspNet.SignalR.Json
     /// </summary>
     public static class JsonUtility
     {
+        private static readonly string[] _jsKeywords = new[] { "abstract", "boolean", "break", "byte", "case", "catch", "char", "class", "const", "continue", "debugger", "default", "delete", "do", "double", "else", "enum", "export", "extends", "false", "final", "finally", "float", "for", "function", "goto", "if", "implements", "import", "in", "instanceof", "int", "interface", "long", "native", "new", "null", "package", "private", "protected", "public", "return", "short", "static", "super", "switch", "synchronized", "this", "throw", "throws", "transient", "true", "try", "typeof", "var", "volatile", "void", "while", "with", "NaN", "Infinity", "undefined" };
+
         /// <summary>
         /// Converts the specified name to camel case.
         /// </summary>
@@ -47,8 +50,74 @@ namespace Microsoft.AspNet.SignalR.Json
         public static string CreateJsonpCallback(string callback, string payload)
         {
             var sb = new StringBuilder();
+            if (!IsValidJavaScriptCallback(callback))
+            {
+                throw new InvalidOperationException();
+            }
             sb.AppendFormat("{0}(", callback).Append(payload).Append(");");
             return sb.ToString();
+        }
+
+        internal static bool IsValidJavaScriptCallback(string callback)
+        {
+            if (String.IsNullOrWhiteSpace(callback))
+            {
+                return false;
+            }
+
+            var identifiers = callback.Split('.');
+
+            // Check each identifier to ensure it's a valid JS identifier
+            foreach (var identifier in identifiers)
+            {
+                if (!IsValidJavaScriptFunctionName(identifier))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        internal static bool IsValidJavaScriptFunctionName(string name)
+        {
+            if (String.IsNullOrWhiteSpace(name) || IsJavaScriptReservedWord(name))
+            {
+                return false;
+            }
+
+            // JavaScript identifier must start with a letter or a '$' or an '_' char
+            var firstChar = name[0];
+            if (!IsValidJavaScriptIdentifierStartChar(firstChar))
+            {
+                return false;
+            }
+
+            for (var i = 1; i < name.Length; i++)
+            {
+                // Characters can be a letter, digit, '$' or '_'
+                if (!IsValidJavaScriptIdenfitierNonStartChar(name[i]))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        private static bool IsValidJavaScriptIdentifierStartChar(char startChar)
+        {
+            return Char.IsLetter(startChar) || startChar == '$' || startChar == '_';
+        }
+
+        private static bool IsValidJavaScriptIdenfitierNonStartChar(char identifierChar)
+        {
+            return Char.IsLetterOrDigit(identifierChar) || identifierChar == '$' || identifierChar == '_';
+        }
+
+        private static bool IsJavaScriptReservedWord(string word)
+        {
+            return _jsKeywords.Contains(word);
         }
     }
 }
--- tests/Microsoft.AspNet.SignalR.Tests/Json/JsonFacts.cs
+++ tests/Microsoft.AspNet.SignalR.Tests/Json/JsonFacts.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.AspNet.SignalR.Json;
+﻿using System;
+using Microsoft.AspNet.SignalR.Json;
 using Xunit;
+using Xunit.Extensions;
 
 namespace Microsoft.AspNet.SignalR.Tests.Json
 {
@@ -43,6 +45,56 @@ namespace Microsoft.AspNet.SignalR.Tests.Json
 
             // Assert
             Assert.Equal("foo(1);", callback);
+        }
+
+        [Fact]
+        public void CreateJsonPCallbackThrowsWithInvalidIdentifier()
+        {
+            Assert.Throws(typeof(InvalidOperationException),() => JsonUtility.CreateJsonpCallback("1nogood", "1"));
+        }
+
+        [Theory]
+        [InlineData("foo")]
+        [InlineData("foo.bar")]
+        [InlineData("foo.bar.baz")]
+        [InlineData("_foo")]
+        [InlineData("foo._bar")]
+        [InlineData("foo.bar._baz")]
+        [InlineData("$foo")]
+        [InlineData("foo.$bar")]
+        [InlineData("foo.bar.$baz")]
+        [InlineData("foo2")]
+        [InlineData("foo.bar2")]
+        [InlineData("foo.bar.baz2")]
+        [InlineData("ۄۺڹ")]
+        [InlineData("ۄۺڹ.bar")]
+        [InlineData("ۄۺڹ.bar.ۄۺڹ")]
+        [InlineData("jQuery18205062005710613621_1359515411213")]
+        public void IsValidJavaScriptCallback(string callback)
+        {
+            // Act
+            var isValid = JsonUtility.IsValidJavaScriptCallback(callback);
+
+            // Assert
+            Assert.True(isValid);
+        }
+
+        [Theory]
+        [InlineData("(function evil(x){moreEvil();})")]
+        [InlineData("1nogood")]
+        [InlineData("<yeahright>")]
+        [InlineData("")]
+        [InlineData(" ")]
+        [InlineData("1ۄۺڹ")]
+        [InlineData("enum")]
+        [InlineData("bar.class.foo")]
+        public void InvalidJavaScriptCallback(string callback)
+        {
+            // Act
+            var isValid = JsonUtility.IsValidJavaScriptCallback(callback);
+
+            // Assert
+            Assert.False(isValid);
         }
     }
 }
