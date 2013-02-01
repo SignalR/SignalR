94bb44ba98bd0dd9c52d6c30ff624609e328b711
NTaylorMullen <NTaylorMullen@outlook.com>
2013-02-01 10:34:58 -0800

    Added a manually restart connection state consistency test for each transport
    - This is to mimic the manually restart c# test but for the JS client

--- tests/Microsoft.AspNet.SignalR.Client.JS.Tests/Tests/FunctionalTests/Transports/All/ConnectionStateFacts.js
+++ tests/Microsoft.AspNet.SignalR.Client.JS.Tests/Tests/FunctionalTests/Transports/All/ConnectionStateFacts.js
@@ -123,4 +123,73 @@ testUtilities.runWithAllTransports(function (transport) {
             connection.stop();
         };
     });
+
+    QUnit.asyncTimeoutTest("Manually restarted client maintains consistent state.", testUtilities.defaultTestTimeout, function (end, assert, testName) {
+        var connection = testUtilities.createHubConnection(testName),
+            demo = connection.createHubProxies().demo,
+            transport = { transport: transport },
+            tryReconnect;
+
+        // LongPolling does not support "lostConnection" so we have to trigger reconnect in a different fashion
+        if (transport !== "longPolling") {
+            tryReconnect = function () {
+                connection.transport.lostConnection(connection);
+            };
+        }
+        else {
+            tryReconnect = function () {
+                // Verify that the polling connection is instantiated before trying to abort it. We want
+                // to cause the transport to error so it must be instantiated first.
+                if (connection.pollXhr.readyState !== 1) {
+                    setTimeout(tryReconnect, 200);
+                }
+                else {
+                    // Passing "foo" forces the longPolling's ajax connection to error and pass "foo" as the 
+                    // reason, the default error (empty) is "abort" which we handle as do not attempt to 
+                    // reconnect. So by passing foo we mimic the behavior of an unintended error occurring, 
+                    // forcing the transport into reconnecting.
+                    connection.pollXhr.abort("foo");
+                }
+            };
+        }
+
+        // Need to have at least one client function in order to be subscribed to a hub
+        demo.client.foo = function () { };
+
+        connection.start(transport).done(function () {
+            setTimeout(function () {
+                // Synchronously stop
+                connection.stop(false);
+
+                assert.ok(true, "Connection manually stopped, now restarting.");
+
+                assert.equal($.signalR.connectionState.disconnected, connection.state, "SignalR state is disconnected prior to (re)start.");
+
+                connection.start(transport).done(function () {
+                    assert.equal($.signalR.connectionState.connected, connection.state, "SignalR state is connected once start callback is called.");
+
+                    // Wire up the state changed (while connected) to detect if we shift into reconnecting
+                    // In a later test we'll determine if reconnected gets called
+                    connection.stateChanged(function () {
+                        if (connection.state == $.signalR.connectionState.reconnecting) {
+                            assert.ok(true, "SignalR state is reconnecting.");
+                            end();
+                        }
+                    });
+
+                    tryReconnect();
+                }).fail(function (reason) {
+                    assert.ok(false, "Failed to initiate SignalR connection");
+                    end();
+                });
+
+                assert.equal($.signalR.connectionState.connecting, connection.state, "SignalR state is connecting prior to start deferred resolve.");
+            }, 250);
+        });
+
+        // Cleanup
+        return function () {
+            connection.stop();
+        };
+    });
 });
