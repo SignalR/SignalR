5a636431ebc061f2c75b6f734e6bc637e1cc33e2
NTaylorMullen <NTaylorMullen@outlook.com>
2013-02-07 15:28:09 -0800

    Renamed ConnectionFacts to HubConnectionFacts and added a new ConnectionFacts
    - Hub Connection facts are for hub connections while connection facts
    are for persistent connections
    - Also added a new test for connectino facts to validate latest jquery
    issue

---
+++ tests/Microsoft.AspNet.SignalR.Client.JS.Tests/Tests/FunctionalTests/Transports/All/HubConnectionFacts.js
@@ -0,0 +1,20 @@
+﻿QUnit.module("Hub Connection Facts");
+
+testUtilities.runWithAllTransports(function (transport) {
+    QUnit.asyncTimeoutTest(transport + " transport an connect.", testUtilities.defaultTestTimeout, function (end, assert, testName) {
+        var connection = testUtilities.createHubConnection(testName);
+
+        connection.start({ transport: transport }).done(function () {
+            assert.ok(true, "Connected");
+            end();
+        }).fail(function (reason) {
+            assert.ok(false, "Failed to initiate SignalR connection");
+            end();
+        });
+
+        // Cleanup
+        return function () {
+            connection.stop();
+        };
+    });    
+});
