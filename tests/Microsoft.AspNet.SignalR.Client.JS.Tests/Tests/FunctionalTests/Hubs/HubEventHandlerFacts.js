78ea402f8b06ad4c06197347d313956dc4cac6b2
NTaylorMullen <NTaylorMullen@outlook.com>
2013-02-07 12:32:49 -0800

    Added HubEventHandlerFacts that test the hub event bindings for the JS client

---
+++ tests/Microsoft.AspNet.SignalR.Client.JS.Tests/Tests/FunctionalTests/Hubs/HubEventHandlerFacts.js
@@ -0,0 +1,104 @@
+﻿QUnit.module("Hub Event Handler Facts");
+
+testUtilities.runWithAllTransports(function (transport) {
+    QUnit.asyncTimeoutTest(transport + ": Hub Event Handler gets called.", testUtilities.defaultTestTimeout, function (end, assert) {
+        var connection = testUtilities.createHubConnection(),
+            echoHub = connection.createHubProxies().echoHub;
+
+        echoHub.client.echo = function (value) {
+            assert.equal(value, "hello", "Successful received message");
+        };
+
+        handler = function (value) {
+            assert.ok(true, "Successful call handler");
+            end();
+        };
+
+        echoHub.on('echo', handler);
+
+        connection.start({ transport: transport }).done(function () {
+            assert.ok(true, "Connected");
+            echoHub.server.echoCallback("hello");
+        }).fail(function (reason) {
+            assert.ok(false, "Failed to initiate signalr connection");
+            end();
+        });
+
+        // Cleanup
+        return function () {
+            connection.stop();
+        };
+    });
+
+    QUnit.asyncTimeoutTest(transport + ": Hub Event Handler can be removed.", testUtilities.defaultTestTimeout, function (end, assert) {
+        var connection = testUtilities.createHubConnection(),
+            echoHub = connection.createHubProxies().echoHub;
+
+        echoHub.client.echo = function (value) {
+            assert.equal(value, "hello", "Successful received message");
+        };
+
+        handler = function (value) {
+            assert.ok(false, "Failed to remove handler");
+            end();
+        };
+
+        echoHub.on('echo', handler);
+        echoHub.off('echo', handler);
+
+        connection.start({ transport: transport }).done(function () {
+            assert.ok(true, "Connected");
+            echoHub.server.echoCallback("hello");
+
+            setTimeout(function () {
+                assert.ok(true, "Handler was not called, success!");
+                end();
+            }, 2000);
+        }).fail(function (reason) {
+            assert.ok(false, "Failed to initiate signalr connection");
+            end();
+        });
+
+        // Cleanup
+        return function () {
+            connection.stop();
+        };
+    });
+
+    QUnit.asyncTimeoutTest(transport + ": Hub Event can be removed.", testUtilities.defaultTestTimeout, function (end, assert) {
+        var connection = testUtilities.createHubConnection(),
+            echoHub = connection.createHubProxies().echoHub;
+
+        echoHub.client.echo = function (value) {
+            assert.ok(false, "Failed to remove event");
+            end();
+        };
+
+        handler = function (value) {
+            assert.ok(false, "Failed to remove handler");
+            end();
+        };
+
+        connection.start({ transport: transport }).done(function () {
+            assert.ok(true, "Connected");
+            echoHub.on('echo', handler);
+            echoHub.on('echo', handler);
+            echoHub.on('echo', handler);
+            echoHub.off('echo');
+            echoHub.server.echoCallback("hello");
+
+            setTimeout(function () {
+                assert.ok(true, "Handler was not called, success!");
+                end();
+            }, 2000);
+        }).fail(function (reason) {
+            assert.ok(false, "Failed to initiate signalr connection");
+            end();
+        });
+
+        // Cleanup
+        return function () {
+            connection.stop();
+        };
+    });
+});
