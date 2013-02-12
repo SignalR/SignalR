746615d933670f0938c038f45ce19d26be8acce2
NTaylorMullen <NTaylorMullen@outlook.com>
2013-02-11 16:57:46 -0800

    Added a connection ID and connection token verification tests to verify that they've changed after stop/start

--- tests/Microsoft.AspNet.SignalR.Client.JS.Tests/Tests/FunctionalTests/Transports/All/HubConnectionFacts.js
+++ tests/Microsoft.AspNet.SignalR.Client.JS.Tests/Tests/FunctionalTests/Transports/All/HubConnectionFacts.js
@@ -16,5 +16,52 @@ testUtilities.runWithAllTransports(function (transport) {
         return function () {
             connection.stop();
         };
-    });    
+    });
+
+    QUnit.asyncTimeoutTest(transport + ": connection.id and connection.token have new value after stop and start.", testUtilities.defaultTestTimeout * 2, function (end, assert) {
+        var connection = testUtilities.createHubConnection(),
+            echoHub = connection.createHubProxies().echoHub,
+            run,
+            oldConnectionID,
+            oldConnectionToken;
+
+        run = function (echo, connection) {
+            var deferred = $.Deferred();
+
+            echoHub.client.echo = function (value) {
+                assert.equal(value, echo, "Successfuly received message " + echo);
+                deferred.resolve();
+            };
+
+            connection.start({ transport: transport }).done(function () {
+                echoHub.server.echoCallback(echo).done(function () {
+                    assert.ok(true, "Successfuly called server method");
+                });
+            }).fail(function (reason) {
+                assert.ok(false, "Failed to initiate signalr connection");
+                end();
+            });
+
+            return deferred.promise();
+        };
+
+        run("hello", connection).done(function () {
+            oldConnectionID = connection.id;
+            oldConnectionToken = connection.token;
+
+            connection.stop();
+            setTimeout(function () {
+                run("hello2", connection).done(function () {
+                    assert.ok(!(oldConnectionID === connection.id), "Connection.id has new value after stop and start");
+                    assert.ok(!(oldConnectionToken === connection.token), "Connection.token has new value after stop and start");
+                    setTimeout(end, 500);
+                });
+            })
+        });
+
+        // Cleanup
+        return function () {
+            connection.stop();
+        };
+    });
 });
