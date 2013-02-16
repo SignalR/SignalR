7202df1b57cad33474a3d5ff075bb984d18eaa50
NTaylorMullen <NTaylorMullen@outlook.com>
2013-02-15 17:10:23 -0800

    Added a Masking network mock piece that allows for generic functions to be mocked.
    Either through:
    - create  : Creates a mask that handles errors and messages
    - subscribe : Masks an individual function to only be called when the network is up.

---
+++ tests/Microsoft.AspNet.SignalR.Client.JS.Tests/NetworkMock/jquery.network.mock.mask.js
@@ -0,0 +1,90 @@
+﻿// Masks network mock
+(function ($, window) {
+    var network = $.network,
+        maskData = {},
+        maskIds = 0,
+        sleeping = false;
+
+    network.mask = {
+        create: function (maskBase, onErrorProperty, onMessageProperty, destroyOnError) {
+            /// <summary>Wires the maskBase's onError and onMessage properties to be affected by network loss</summary>
+            /// <param name="maskBase" type="Object">An object with properties indicated by onErrorProperty and onMessageProperty</param>
+            /// <param name="onErrorProperty" type="String">The string representation of the onError method of the maskBase</param>
+            /// <param name="onMessageProperty" type="String">The string representation of the onMessage method of the maskBase</param>
+            /// <param name="destroyOnError" type="Boolean">Represents if we destroy our created mask object onError.</param>
+            var savedOnError = maskBase[onErrorProperty],
+                savedOnMessage = maskBase[onMessageProperty],
+                id = maskIds++;
+
+            if (!savedOnError || !savedOnMessage) {
+                throw new Error("maskBase must have an onErrorProperty AND an onMessageProperty property");
+            }
+
+            maskBase[onErrorProperty] = function () {
+                if (!sleeping) {
+                    return savedOnError.apply(this, arguments);
+                }
+
+                if (destroyOnError) {
+                    // Reset the maskBase to its original setup
+                    maskBase[onErrorProperty] = savedOnError;
+                    maskBase[onMessageProperty] = savedOnMessage;
+                    delete maskData[id];
+                }
+            };
+
+            maskBase[onMessageProperty] = function () {
+                if (!sleeping) {
+                    return savedOnMessage.apply(this, arguments);
+                }
+            };
+
+            maskData[id] =
+            {
+                onError: maskBase[onErrorProperty],
+                onMessage: maskBase[onMessageProperty]
+            };
+        },
+        subscribe: function (maskBase, functionName, onBadAttempt) {
+            /// <summary>Subscribes a function to only be called when the network is up</summary>
+            /// <param name="maskBase" type="Object">An object with properties indicated by functionName</param>
+            /// <param name="functionName" type="String">Function name that should be replaced on the maskBase</param>
+            /// <param name="onBadAttempt" type="Function">Function to only be called when an attempt to call the functionName function is called when network is down</param>
+            /// <returns type="Object">Object with an unsubscribe method used to remove the network connection monitoring.</returns>
+            var savedFunction = maskBase[functionName];
+
+            maskBase[functionName] = function () {
+                if (!sleeping) {
+                    return savedFunction.apply(this, arguments);
+                }
+                else if (onBadAttempt) {
+                    onBadAttempt();
+                }
+            };
+
+            return {
+                unsubscribe: function () {
+                    maskBase[functionName] = savedFunction;
+                }
+            };
+        },
+        disconnect: function (soft) {
+            /// <summary>Disconnects the network so javascript transport methods are unable to communicate with a server.</summary>
+            /// <param name="soft" type="Boolean">Whether the disconnect should be soft.  A soft disconnect indicates that transport methods are not notified of disconnect.</param>
+            if (!soft) {
+                for (var key in maskData) {
+                    var data = maskData[key];
+
+                    data.onError();
+                }
+            }
+            else {
+                sleeping = true;
+            }
+        },
+        connect: function () {
+            /// <summary>Connects the network so javascript methods can continue utilizing the network.</summary>
+            sleeping = false;
+        }
+    };
+})($, window);
