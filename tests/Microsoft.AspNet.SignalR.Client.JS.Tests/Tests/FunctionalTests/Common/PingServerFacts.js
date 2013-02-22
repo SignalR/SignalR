70027b66a0dc5a3cfc4ebdead687b9bf5d9fde55
NTaylorMullen <NTaylorMullen@outlook.com>
2013-02-22 14:25:46 -0800

    Added a ping server test that verifies that custom query string variables are passed
    #1556

--- tests/Microsoft.AspNet.SignalR.Client.JS.Tests/Tests/FunctionalTests/Common/PingServerFacts.js
+++ tests/Microsoft.AspNet.SignalR.Client.JS.Tests/Tests/FunctionalTests/Common/PingServerFacts.js
@@ -29,4 +29,57 @@ testUtilities.runWithAllTransports(function (transport) {
             connection.stop();
         };
     });
+
+    QUnit.asyncTimeoutTest(transport + " transport calls Ping Server with custom query string in url", testUtilities.defaultTestTimeout, function (end, assert, testName) {
+        var connection = testUtilities.createHubConnection(testName),
+            expectedQs = window.encodeURIComponent(testName),
+            savedAjax = $.ajax,
+            testPingServer = function () {
+                $.signalR.transports._logic.pingServer(connection, transport).done(function () {
+                    // Successful ping
+                    assert.ok(true, "Successful ping with " + transport);
+                }).fail(function () {
+                    assert.ok(false, "Failed to ping server with " + transport);
+                    end();
+                });
+            };
+
+        // Verify that the query string parameter was set
+        assert.ok(connection.qs.indexOf(expectedQs) >= 0, "Query string contains the test name prior to ping server.");
+
+        // For long polling this ajax request will execute before testPingServer because longPolling
+        // utilizes the pingServer method.
+        $.ajax = function (url, settings) {
+            if (!settings) {
+                settings = url;
+                url = settings.url;
+            }
+
+            // Check if it's the ping request;
+            if (url.indexOf("/ping") >= 0) {
+                // Verify that the query string parameter on the connection is passed via the ajax request
+                assert.ok(url.indexOf(expectedQs) >= 0, "Query string parameters were passed in ping server");
+                // Let the ajax request finish out
+                setTimeout(end, 0);
+            }
+
+            // Persist the request through to the original ajax request
+            savedAjax.call(this, url, settings);
+        };
+
+        // Starting/Stopping a connection to have it instantiated with all the appropriate variables
+        connection.start({ transport: transport }).done(function () {
+            assert.ok(true, "Connected");
+            connection.stop();
+            testPingServer();
+        }).fail(function (reason) {
+            assert.ok(false, "Failed to initiate SignalR connection");
+            end();
+        });
+
+        return function () {
+            $.ajax = savedAjax;
+            connection.stop();
+        };
+    });
 });
