3d0738d418d638b5966730da91ce3ba0007a96ec
Bjarte Skogøy <bjarte.skogoy@gmail.com>
2013-02-22 13:00:39 +0100

    Even next patch

--- src/Microsoft.AspNet.SignalR.Client/IConnection.cs
+++ src/Microsoft.AspNet.SignalR.Client/IConnection.cs
@@ -44,6 +44,5 @@ namespace Microsoft.AspNet.SignalR.Client
         void UpdateLastKeepAlive();
 
         JsonSerializer JsonSerializer { get; set; }
-        string JsonSerializeObject(object value);
     }
 }
