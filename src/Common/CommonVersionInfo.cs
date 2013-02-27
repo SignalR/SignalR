ccd8b5e57ea42b92f053d03eb6c7bb9ce742dcd1
David Fowler <davidfowl@gmail.com>
2013-02-27 01:14:46 -0800

    Don't update patch for assembly version.

--- src/Common/CommonVersionInfo.cs
+++ src/Common/CommonVersionInfo.cs
@@ -1,7 +1,7 @@
-﻿// Copyright (c) Microsoft Open Technologies, Inc. All rights reserved. See License.md in the project root for license information.
+// Copyright (c) Microsoft Open Technologies, Inc. All rights reserved. See License.md in the project root for license information.
 
 using System.Reflection;
 
 [assembly: AssemblyVersion("1.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: AssemblyInformationalVersion("1.0.0")]
+[assembly: AssemblyInformationalVersion("1.0.1")]
