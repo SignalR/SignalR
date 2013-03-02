01d0f9a6e10a7ee639cf19c5f56574657580fb06
David Fowler <davidfowl@gmail.com>
2013-03-02 01:49:15 -0800

    Improved tracing again. - Added System.Net tracing - Improved the test names.

--- tests/Microsoft.AspNet.SignalR.Tests.Common/Infrastructure/HostedTest.cs
+++ tests/Microsoft.AspNet.SignalR.Tests.Common/Infrastructure/HostedTest.cs
@@ -1,11 +1,14 @@
 ﻿using System;
-using System.Linq;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Reflection;
 using Microsoft.AspNet.SignalR.Client.Http;
 using Microsoft.AspNet.SignalR.Client.Hubs;
 using Microsoft.AspNet.SignalR.Client.Transports;
 using Microsoft.AspNet.SignalR.Hosting.Memory;
+using Microsoft.AspNet.SignalR.Infrastructure;
 using Xunit;
 using Xunit.Extensions;
 
@@ -13,10 +16,24 @@ namespace Microsoft.AspNet.SignalR.FunctionalTests.Infrastructure
 {
     public abstract class HostedTest : IDisposable
     {
+        private IDisposable _systemNetLogging;
+
         protected ITestHost CreateHost(HostType hostType, TransportType transportType)
         {
             string testName = GetTestName() + "." + transportType;
             ITestHost host = null;
+
+            string logBasePath = Path.Combine(Directory.GetCurrentDirectory(), "..");
+            string clientDebugPath = Path.Combine(logBasePath, testName + ".client.debug.log");
+            string clientNetworkPath = Path.Combine(logBasePath, testName + ".client.network.log");
+
+            var clientDebugTraceListener = new TextWriterTraceListener(clientDebugPath);
+
+            Debug.Listeners.Clear();
+            Debug.Listeners.Add(clientDebugTraceListener);
+
+            // Enable system new logging to this path
+            _systemNetLogging = SystemNetLogging.Enable(clientNetworkPath);
 
             switch (hostType)
             {
@@ -65,7 +82,12 @@ namespace Microsoft.AspNet.SignalR.FunctionalTests.Infrastructure
                     let anyFactsAttributes = m.GetCustomAttributes(typeof(FactAttribute), true).Length > 0
                     let anyTheories = m.GetCustomAttributes(typeof(TheoryAttribute), true).Length > 0
                     where anyFactsAttributes || anyTheories
-                    select m.DeclaringType.Name + "." + m.Name).First();
+                    select GetName(m)).First();
+        }
+
+        private string GetName(MethodBase m)
+        {
+            return m.DeclaringType.FullName.Substring(m.DeclaringType.Namespace.Length).TrimStart('.') + "." + m.Name;
         }
 
         protected IClientTransport CreateTransport(TransportType transportType)
@@ -107,6 +129,65 @@ namespace Microsoft.AspNet.SignalR.FunctionalTests.Infrastructure
         public virtual void Dispose()
         {
             Dispose(true);
+
+            if (_systemNetLogging != null)
+            {
+                _systemNetLogging.Dispose();
+            }
+
+            Debug.Close();
+            Debug.Listeners.Clear();
+        }
+
+        private static class SystemNetLogging
+        {
+            private static readonly Lazy<Logging> _logging = new Lazy<Logging>(() => new Logging());
+
+            public static IDisposable Enable(string path)
+            {
+                var listener = new TextWriterTraceListener(path);
+                _logging.Value.Sources.ForEach(s => s.Listeners.Add(listener));
+
+                return new DisposableAction(() =>
+                {
+                    _logging.Value.Sources.ForEach(s => s.Listeners.Remove(listener));
+                });
+            }
+
+            private class Logging
+            {
+                private readonly Type _loggingType;
+
+                public List<TraceSource> Sources = new List<TraceSource>();
+
+                public Logging()
+                {
+                    _loggingType = Type.GetType("System.Net.Logging, System, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089");
+
+                    if (_loggingType == null)
+                    {
+                        return;
+                    }
+
+                    var webProperty = _loggingType.GetProperty("Web", BindingFlags.NonPublic | BindingFlags.Static);
+                    if (webProperty != null)
+                    {
+                        Sources.Add((TraceSource)webProperty.GetValue(null));
+                    }
+
+                    var socketsProperty = _loggingType.GetProperty("Sockets", BindingFlags.NonPublic | BindingFlags.Static);
+                    if (socketsProperty != null)
+                    {
+                        Sources.Add((TraceSource)socketsProperty.GetValue(null));
+                    }
+
+                    var webSocketsProperty = _loggingType.GetProperty("WebSockets", BindingFlags.NonPublic | BindingFlags.Static);
+                    if (webSocketsProperty != null)
+                    {
+                        Sources.Add((TraceSource)webSocketsProperty.GetValue(null));
+                    }
+                }
+            }
         }
     }
 }
