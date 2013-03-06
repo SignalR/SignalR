ccdade4488efacd184b2731b18203daa88c9845e
Abhishek Nanda <abnanda@microsoft.com>
2013-03-06 14:20:35 -0800

    Added a null check for the Send and Group methods in Hub and Persistent Connection
    - Added the corresponding unit tests for the null cases
    
    #1265

---
+++ tests/Microsoft.AspNet.SignalR.Tests/Core/Hubs/HubConnectionContextFacts.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.AspNet.SignalR.Hubs;
+using Xunit;
+
+namespace Microsoft.AspNet.SignalR.Tests.Core.Hubs
+{
+    public class HubConnectionContextFacts
+    {
+        [Fact]
+        public void GroupThrowsNullExceptionWhenGroupNameIsNull()
+        {
+            var hubConContext = new HubConnectionContext();
+
+            try
+            {
+                hubConContext.Group(null);
+            }
+            catch (Exception ex)
+            {
+                Assert.IsType(typeof(ArgumentNullException), ex);
+            }
+        }
+
+        [Fact]
+        public void ClientThrowsNullExceptionWhenClientIdIsNull()
+        {
+            var hubConContext = new HubConnectionContext();
+
+            try
+            {
+                hubConContext.Client(null);
+            }
+            catch (Exception ex)
+            {
+                Assert.IsType(typeof(ArgumentNullException), ex);
+            }
+        }
+    }
+}
