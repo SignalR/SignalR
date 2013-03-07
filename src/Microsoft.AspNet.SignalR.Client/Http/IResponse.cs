94b30baa7d9720551a239923490136e52f62eae9
NTaylorMullen <NTaylorMullen@outlook.com>
2013-03-07 15:28:16 -0800

    Modified the IResponse object to only contain a GetStream member and moved the ReadAsString method to an extension
    - Also Changed the ReadAsString method to return a task of string.
    #1536

--- src/Microsoft.AspNet.SignalR.Client/Http/IResponse.cs
+++ src/Microsoft.AspNet.SignalR.Client/Http/IResponse.cs
@@ -1,5 +1,6 @@
 ﻿// Copyright (c) Microsoft Open Technologies, Inc. All rights reserved. See License.md in the project root for license information.
 
+using System;
 using System.Diagnostics.CodeAnalysis;
 using System.IO;
 
@@ -8,24 +9,13 @@ namespace Microsoft.AspNet.SignalR.Client.Http
     /// <summary>
     /// The http response.
     /// </summary>
-    public interface IResponse
+    public interface IResponse : IDisposable
     {
-        /// <summary>
-        /// Reads the response body as a string.
-        /// </summary>
-        /// <returns></returns>
-        string ReadAsString();
-
         /// <summary>
         /// Gets the steam that represents the response body.
         /// </summary>
         /// <returns></returns>
         [SuppressMessage("Microsoft.Design", "CA1024:UsePropertiesWhereAppropriate", Justification = "This could be expensive.")]
-        Stream GetResponseStream();
-
-        /// <summary>
-        /// Closes the response.
-        /// </summary>
-        void Close();
+        Stream GetStream();
     }
 }
---
+++ src/Microsoft.AspNet.SignalR.Client/Http/IResponseExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microsoft.AspNet.SignalR.Client.Http
+{
+    public static class IResponseExtensions
+    {
+        public static Task<string> ReadAsString(this IResponse response)
+        {
+            using (Stream stream = response.GetStream())
+            {
+                var reader = new StreamReader(stream);
+
+                //TODODODODODODO
+                return TaskAsyncHelper.FromResult<string>(reader.ReadToEnd());
+            }
+        }
+    }
+}
