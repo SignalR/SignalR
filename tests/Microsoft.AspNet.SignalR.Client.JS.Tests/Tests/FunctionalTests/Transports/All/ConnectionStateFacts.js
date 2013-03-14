225bf539f2d2154cb84596d88744e8aa89831c5b
Stephen Halter <halter73@gmail.com>
2013-03-13 22:54:54 -0700

    Test ensuring JS transports attempt reconnecting multiple times
    #1684

--- tests/Microsoft.AspNet.SignalR.Client.JS.Tests/Tests/FunctionalTests/Transports/All/ConnectionStateFacts.js
+++ tests/Microsoft.AspNet.SignalR.Client.JS.Tests/Tests/FunctionalTests/Transports/All/ConnectionStateFacts.js
@@ -192,4 +192,75 @@ testUtilities.runWithAllTransports(function (transport) {
             }
         };
     });
+
+    QUnit.asyncTimeoutTest(transport + " transport will attempt to reconnect multiple times.", testUtilities.defaultTestTimeout, function (end, assert, testName) {
+        var connection = testUtilities.createHubConnection(end, assert, testName),
+            reconnectAttempts = 0,
+            savedConnectionReconnectDelay = connection.reconnectDelay,
+            savedLongPollingReconnectDelay = $.connection.transports.longPolling.reconnectDelay,
+            savedReconnect = $.connection.transports[transport].reconnect,
+            savedPingServer = $.connection.transports._logic.pingServer;
+
+        function connectIfSecondReconnectAttempt() {
+            if (++reconnectAttempts === 2) {
+                $.network.connect();
+            }
+        }
+
+        // Shorten timeouts that slow down reconnect attempts ensure transports attempt reconnecting several times.
+        connection.reconnectDelay = 10;
+        $.connection.transports.longPolling.reconnectDelay = 10;
+
+        connection.reconnecting(function () {
+            assert.equal(connection.state, $.signalR.connectionState.reconnecting, "Transport started reconnecting.");
+        });
+
+        // "longPolling will immediately raise the reconnected event, irrespective of the ajax response
+        if (transport === "longPolling") {
+            connection.reconnected(function () {
+                if (reconnectAttempts > 1) {
+                    assert.equal(connection.state, $.signalR.connectionState.connected, "Transport reconnected.");
+                    end();
+                }
+            });
+        } else {
+            connection.reconnected(function () {
+                assert.ok(reconnectAttempts > 1, "Transport attempted reconnecting multiple times");
+                assert.equal(connection.state, $.signalR.connectionState.connected, "Transport reconnected.");
+                end();
+            });
+        }
+
+        connection.start({ transport: transport }).done(function () {
+            assert.equal(connection.state, $.signalR.connectionState.connected, "Connection started.");
+
+            if (transport === "longPolling") {
+                // FIX: The longPolling transport currently needs to receive a message with a message ID before it can "reconnect"
+                connection.messageId = connection.messageId || "";
+                $.connection.transports._logic.pingServer = function (connection, transport) {
+                    return savedPingServer.call($.connection.transports._logic, connection, transport).fail(function () {
+                        connectIfSecondReconnectAttempt();
+                    });
+                }
+            } else {
+                $.connection.transports[transport].reconnect = function (connection) {
+                    savedReconnect.call($.connection.transports[transport], connection);
+                    connectIfSecondReconnectAttempt();
+                }
+            }
+
+            $.network.disconnect();
+        });
+
+        return function () {
+            connection.reconnectDelay = savedConnectionReconnectDelay;
+            $.connection.transports.longPolling.reconnectDelay = savedLongPollingReconnectDelay;
+
+            $.connection.transports[transport].reconnect = savedReconnect;
+            $.connection.transports._logic.pingServer = savedPingServer;
+
+            $.network.connect();
+            connection.stop();
+        }
+    });
 });
