3b2a9ea24e06f30618a0db47260f1e1a0f6aaa02
Stephen Halter <halter73@gmail.com>
2013-04-08 16:24:13 -0700

    .NET test to ensure "reconnect" is appended to reconnect requests

---
+++ tests/Microsoft.AspNet.SignalR.Tests.Common/Connections/ExamineReconnectPath.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Microsoft.AspNet.SignalR.FunctionalTests
+{
+    public class ExamineReconnectPath : PersistentConnection
+    {
+        protected override Task OnReconnected(IRequest request, string connectionId)
+        {
+            return Connection.Send(connectionId, request.Url.AbsolutePath.EndsWith("/reconnect"));
+        }
+    }
+}
