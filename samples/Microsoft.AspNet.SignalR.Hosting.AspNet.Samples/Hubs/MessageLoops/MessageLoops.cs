b4a465f74455b8137230e4dfb28abc7c8946f745
Xiaohongt <xiaota@microsoft.com>
2013-04-16 21:02:51 -0700

    made change for code review feedback: update for message loops

--- samples/Microsoft.AspNet.SignalR.Hosting.AspNet.Samples/Hubs/MessageLoops/MessageLoops.cs
+++ samples/Microsoft.AspNet.SignalR.Hosting.AspNet.Samples/Hubs/MessageLoops/MessageLoops.cs
@@ -9,7 +9,7 @@ namespace Microsoft.AspNet.SignalR.Samples.Hubs.DemoHub.MessageLoops
     public class MessageLoops : Hub
     {
         public int SendMessageCount(int message, string connectionId)
-        {            
+        {
             Thread.Sleep(5000);
             Clients.All.displayMessagesCount(++message, connectionId);
             return message;
