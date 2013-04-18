5414a94d0811fb6679626075146ac7976d677335
David Fowler <davidfowl@gmail.com>
2013-04-17 17:26:30 -0700

    Remove retry on failure flag. - Changed ScaleoutTaskQueue to use a lockObject - Intercept sends and make sure we're in the buffering state - Updated unit tests.

--- src/Microsoft.AspNet.SignalR.Core/Messaging/ScaleoutConfiguration.cs
+++ src/Microsoft.AspNet.SignalR.Core/Messaging/ScaleoutConfiguration.cs
@@ -10,19 +10,6 @@ namespace Microsoft.AspNet.SignalR.Messaging
     public class ScaleoutConfiguration
     {
         /// <summary>
-        /// When set to true, the scale-out message bus will buffer outgoing messages until the backplane
-        /// is available again. Provide a callback via <see cref="Microsoft.AspNet.SignalR.Messaging.ScaleoutConfiguration.OnError">OnError</see>
-        /// to be notified when an error occurs. Defaults to false.
-        /// </summary>
-        public bool RetryOnError { get; set; }
-
-        /// <summary>
-        /// A callback to be invoked when an error occurs in a scale-out message bus.
-        /// The exception that caused the error is passed.
-        /// </summary>
-        public Action<Exception> OnError { get; set; }
-        
-        /// <summary>
         /// The number of messages to include in a batch before delivering to the scale-out message bus.
         /// Use in conjunction with the BatchTimeout property to configure message batching.
         /// Set this property to 0 to disable batching (the default).
