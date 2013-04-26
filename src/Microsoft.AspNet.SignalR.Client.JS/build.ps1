0ee029ddb590f6fa9825b589c37ea5f41fe5496e
Stephen Halter <halter73@gmail.com>
2013-04-26 16:40:38 -0700

    Add CopyArtifacts target to Build.proj and add nuspecs to /nuspecs
    - Automatically pick up new projects in Build.proj
    - Add a package-src directory to artifacts
    - Group artifacts by target platform
    - Publish nuspecs to artifacts
    - Publish FxCop logs to artifacts
    - Publish jquery.signalR.js and jquery.signalR.min.js to artifacts
    - Build Asp.Net samples from Build.proj and publish to artifacts
    - Publish artifacts grouped by project to artifacts/projects
    
    This is meant to make signing and localization simpler
    
    #1896 #1897

--- src/Microsoft.AspNet.SignalR.Client.JS/build.ps1
+++ src/Microsoft.AspNet.SignalR.Client.JS/build.ps1
@@ -1,3 +1,7 @@
+Param(
+    [string]$outputPath
+)
+
 # Files in the order they must be combined
 $files = 
     "jquery.signalR.core.js",
@@ -6,15 +10,15 @@ $files =
     "jquery.signalR.transports.serverSentEvents.js",
     "jquery.signalR.transports.foreverFrame.js",
     "jquery.signalR.transports.longPolling.js",
-	"jquery.signalR.hubs.js",
-	"jquery.signalR.version.js"
+    "jquery.signalR.hubs.js",
+    "jquery.signalR.version.js"
 
 $versionHolder = "##VERSION##"
 $versionInfo = ([xml](Get-Content -Path "..\..\build\Microsoft.AspNet.SignalR.versions.targets")).Project.PropertyGroup
 
 If($versionInfo.BuildQuality -ne "")
 {
-	$versionInfo.BuildQuality = "-" + $versionInfo.BuildQuality;
+    $versionInfo.BuildQuality = "-" + $versionInfo.BuildQuality;
 }
 
 $version = %{'{0}.{1}.{2}{3}' -f $versionInfo.MajorVersion, $versionInfo.MinorVersion, $versionInfo.PatchVersion, $versionInfo.BuildQuality}
@@ -36,11 +40,11 @@ foreach ($file in $files) {
 
 # Combine all files into jquery.signalR.js
 if (!(Test-Path -path "bin")) {
-	New-Item "bin" -Type Directory | Out-Null
+    New-Item "bin" -Type Directory | Out-Null
 }
 
 Write-Host "Building bin\jquery.signalR.js... " -NoNewline -ForegroundColor Yellow
-$filePath = "bin\jquery.signalR.js"
+$filePath = "$outputPath\jquery.signalR.js"
 Remove-Item $filePath -Force -ErrorAction SilentlyContinue
 foreach ($file in $files) {
     Add-Content -Path $filePath -Value "/* $file */"
@@ -50,7 +54,7 @@ Write-Host "done" -ForegroundColor Green
 
 # Minify to jquery.signalR.min.js
 Write-Host "Building bin\jquery.signalR.min.js... " -NoNewline -ForegroundColor Yellow
-& "..\..\tools\ajaxmin\AjaxMin.exe" bin\jquery.signalR.js -out bin\jquery.signalR.min.js -clobber > $output
+& "..\..\tools\ajaxmin\AjaxMin.exe" $outputPath\jquery.signalR.js -out $outputPath\jquery.signalR.min.js -clobber > $output
 (Get-Content $output)[6] | Write-Host -ForegroundColor Green
 
 Remove-Item $output -Force
