506a1b7617adf1dccf074394d71d45d7c5966833
Xiaohongt <xiaota@microsoft.com>
2013-04-29 12:15:23 -0700

    update for input sleep time like 0.1 can work too

--- samples/Microsoft.AspNet.SignalR.Hosting.AspNet.Samples/Hubs/MessageLoops/MessageLoops.cs
+++ samples/Microsoft.AspNet.SignalR.Hosting.AspNet.Samples/Hubs/MessageLoops/MessageLoops.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading;
+﻿using System;
+using System.Threading;
 
 namespace Microsoft.AspNet.SignalR.Samples.Hubs.DemoHub.MessageLoops
 {
@@ -16,29 +17,29 @@ namespace Microsoft.AspNet.SignalR.Samples.Hubs.DemoHub.MessageLoops
             return connectionId + " removed from " + groupName;
         }
 
-        public int SendMessageCountToAll(int messageCount, int sleep)
+        public int SendMessageCountToAll(int messageCount, string sleep)
         {
-            if (sleep > 0)
-                Thread.Sleep(sleep * 1000);
+            if (Convert.ToDouble(sleep) > 0)
+                Thread.Sleep(Convert.ToInt32(Convert.ToDouble(sleep) * 1000));
 
             Clients.All.displayMessagesCount(++messageCount, Context.ConnectionId).Wait();
             return messageCount;
         }
 
-        public int SendMessageCountToGroup(int messageCount, string groupName, int sleep)
+        public int SendMessageCountToGroup(int messageCount, string groupName, string sleep)
         {
-            if (sleep > 0)
-                Thread.Sleep(sleep * 1000);
+            if (Convert.ToDouble(sleep) > 0)
+                Thread.Sleep(Convert.ToInt32(Convert.ToDouble(sleep) * 1000));
 
             Clients.Group(groupName).displayMessagesCount(++messageCount, Context.ConnectionId).Wait();
             return messageCount;
         }
 
 
-        public int SendMessageCountToCaller(int messageCount, int sleep)
+        public int SendMessageCountToCaller(int messageCount, string sleep)
         {
-            if (sleep > 0)
-                Thread.Sleep(sleep * 1000);
+            if (Convert.ToDouble(sleep) > 0)
+                Thread.Sleep(Convert.ToInt32(Convert.ToDouble(sleep) * 1000));
 
             Clients.Caller.displayMessagesCount(++messageCount, Context.ConnectionId).Wait();
             return messageCount;
