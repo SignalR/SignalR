bca4442741ddb87450dbf6ea50bdf35856b84995
NTaylorMullen <NTaylorMullen@outlook.com>
2013-05-02 17:00:55 -0700

    Added Functional and Unit tests to verify the init message functionality on the JS client
    #1889

---
+++ tests/Microsoft.AspNet.SignalR.Tests.Common/Hubs/OnConnectedBufferHub.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Microsoft.AspNet.SignalR.Tests.Common.Hubs
+{
+    public class OnConnectedBufferHub : Hub
+    {
+        public static int bufferMeValues = 0;
+
+        public override Task OnConnected()
+        {
+            Clients.Caller.bufferMe(Interlocked.Increment(ref bufferMeValues));
+            Clients.Caller.bufferMe(Interlocked.Increment(ref bufferMeValues));
+
+            Thread.Sleep(TimeSpan.FromSeconds(2));
+
+            return base.OnConnected();
+        }
+
+        public void Ping()
+        {
+            Clients.Caller.pong();
+        }
+    }
+}
