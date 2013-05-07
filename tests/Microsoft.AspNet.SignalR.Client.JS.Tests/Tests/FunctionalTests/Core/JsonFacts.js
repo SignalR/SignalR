ab19fddc8ab50f09e8a32a088f918f1ac6426b9b
DamianEdwards <damian@damianedwards.com>
2013-05-06 22:07:52 -0700

    Run JS client JSON unit test for connection & hubConnection

--- tests/Microsoft.AspNet.SignalR.Client.JS.Tests/Tests/FunctionalTests/Core/JsonFacts.js
+++ tests/Microsoft.AspNet.SignalR.Client.JS.Tests/Tests/FunctionalTests/Core/JsonFacts.js
@@ -3,28 +3,32 @@
 (function ($, window) {
     testUtilities.runWithAllTransports(function (transport) {
         QUnit.asyncTimeoutTest(transport + " transport uses custom JSON object.", testUtilities.defaultTestTimeout, function (end, assert, testName) {
-            var connection = testUtilities.createHubConnection(end, assert, testName),
-                customJsonCalled = false;
+            var connections = [testUtilities.createConnection("signalr", end, assert, testName),
+                               testUtilities.createHubConnection(end, assert, testName)];
             
-            connection.json = {
-                parse: function () {
-                    customJsonCalled = true;
-                    return window.JSON.parse.apply(window, arguments);
-                },
-                stringify: function () {
-                    customJsonCalled = true;
-                    return window.JSON.stringify.apply(window, arguments);
-                }
-            };
+            $.each(connections, function (_, connection) {
+                var customJsonCalled = false;
 
-            connection.start({ transport: transport }).done(function () {
-                assert.ok(customJsonCalled, "Should use custom JSON object.");
-                end();
+                connection.json = {
+                    parse: function () {
+                        customJsonCalled = true;
+                        return window.JSON.parse.apply(window, arguments);
+                    },
+                    stringify: function () {
+                        customJsonCalled = true;
+                        return window.JSON.stringify.apply(window, arguments);
+                    }
+                };
+
+                connection.start({ transport: transport }).done(function () {
+                    assert.ok(customJsonCalled, "Should use custom JSON object.");
+                    end();
+                });
             });
-
-            return function () {
+            
+            $.each(connections, function (_, connection) {
                 connection.stop();
-            };
+            });
         });
     });
 })($, window);
