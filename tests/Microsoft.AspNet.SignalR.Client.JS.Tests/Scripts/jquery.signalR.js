493ba2e505a80f27e162198a886d9cb33ba03d36
David Fowler <davidfowl@gmail.com>
2013-05-15 01:13:38 -0700

    Updated to use the right version.

--- tests/Microsoft.AspNet.SignalR.Client.JS.Tests/Scripts/jquery.signalR.js
+++ tests/Microsoft.AspNet.SignalR.Client.JS.Tests/Scripts/jquery.signalR.js
@@ -1,7 +1,7 @@
 /* jquery.signalR.core.js */
 /*global window:false */
 /*!
- * ASP.NET SignalR JavaScript Library v2.0.0-beta1
+ * ASP.NET SignalR JavaScript Library v2.0.0-beta2
  * http://signalr.net/
  *
  * Copyright Microsoft Open Technologies, Inc. All rights reserved.
