96483f5ad4e12832e58e24c47c956d0d095ca37b
David Fowler <davidfowl@gmail.com>
2013-05-16 03:10:10 -0700

    Fixed issues with group token. - Make sure group token is properly updated on changes - Made diffset just detect changes in groups. - Serialize group token even if groups are empty. - Added tests for the new diffset.
    #2040

--- tests/Microsoft.AspNet.SignalR.Tests/Server/Connections/ConnectionFacts.cs
+++ tests/Microsoft.AspNet.SignalR.Tests/Server/Connections/ConnectionFacts.cs
@@ -38,7 +38,7 @@ namespace Microsoft.AspNet.SignalR.Tests
                 var groupSet = new DiffSet<string>(new string[] { "a", "b", "c" });
 
                 // Get the first diff
-                groupSet.GetDiff();
+                groupSet.DetectChanges();
 
                 var serializer = JsonUtility.CreateDefaultSerializer();
                 var protectedData = new Mock<IProtectedData>();
@@ -59,7 +59,7 @@ namespace Microsoft.AspNet.SignalR.Tests
                 var response = new PersistentResponse();
                 var groupSet = new DiffSet<string>(new string[] { "a:1", "b:2", "c", "d" });
 
-                groupSet.GetDiff();
+                groupSet.DetectChanges();
 
                 groupSet.Add("g");
 
@@ -87,6 +87,42 @@ namespace Microsoft.AspNet.SignalR.Tests
                 Assert.True(results.Contains("c"));
                 Assert.True(results.Contains("d"));
                 Assert.True(results.Contains("g"));
+            }
+
+            [Fact]
+            public void GroupTokenIsNotNullWhenGroupsChangeToEmpty()
+            {
+                var response = new PersistentResponse();
+                var groupSet = new DiffSet<string>(new string[] { "b", "d" });
+
+                groupSet.DetectChanges();
+
+                groupSet.Remove("b");
+                groupSet.Remove("d");
+
+                var serializer = new Mock<IJsonSerializer>();
+                HashSet<string> results = null;
+                serializer.Setup(m => m.Serialize(It.IsAny<object>(), It.IsAny<TextWriter>()))
+                          .Callback<object, TextWriter>((obj, tw) =>
+                          {
+                              results = new HashSet<string>((IEnumerable<string>)obj);
+                              var jsonNet = new JsonNetSerializer();
+                              jsonNet.Serialize(obj, tw);
+                          });
+                var protectedData = new Mock<IProtectedData>();
+                protectedData.Setup(m => m.Protect(It.IsAny<string>(), It.IsAny<string>()))
+                    .Returns<string, string>((value, purpose) => value);
+
+                protectedData.Setup(m => m.Unprotect(It.IsAny<string>(), It.IsAny<string>()))
+                             .Returns<string, string>((value, purpose) => value);
+
+                Connection.PopulateResponseState(response, groupSet, serializer.Object, protectedData.Object, connectionId: "myconnection");
+
+                Assert.NotNull(response.GroupsToken);
+                var parts = response.GroupsToken.Split(new[] { ':' }, 2);
+                Assert.Equal(2, parts.Length);
+                Assert.Equal("myconnection", parts[0]);
+                Assert.Equal(0, results.Count);
             }
         }
     }
