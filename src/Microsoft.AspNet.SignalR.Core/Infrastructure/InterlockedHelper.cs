089311d943a46cb4c9f2ee30fc3ab5d802b3a480
NTaylorMullen <NTaylorMullen@outlook.com>
2013-05-23 17:19:14 -0700

    Fixed code analysis issues

--- src/Microsoft.AspNet.SignalR.Core/Infrastructure/InterlockedHelper.cs
+++ src/Microsoft.AspNet.SignalR.Core/Infrastructure/InterlockedHelper.cs
@@ -1,10 +1,16 @@
 ﻿using System.Threading;
 
-public static class InterlockedHelper
+namespace Microsoft.AspNet.SignalR.Infrastructure
 {
-    public static bool CompareExchangeOr(ref int location, int value, int comparandA, int comparandB)
+
+    public static class InterlockedHelper
     {
-        return Interlocked.CompareExchange(ref location, value, comparandA) == comparandA ||
-               Interlocked.CompareExchange(ref location, value, comparandB) == comparandB;
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1045:DoNotPassTypesByReference", MessageId = "0#", Justification="This is an interlocked helper...")]
+        public static bool CompareExchangeOr(ref int location, int value, int comparandA, int comparandB)
+        {
+            return Interlocked.CompareExchange(ref location, value, comparandA) == comparandA ||
+                   Interlocked.CompareExchange(ref location, value, comparandB) == comparandB;
+        }
     }
+
 }
