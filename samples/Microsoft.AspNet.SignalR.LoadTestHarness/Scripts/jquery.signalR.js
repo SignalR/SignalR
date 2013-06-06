30e282fc8c92aaf982ef70e08c8104238b64ca24
NTaylorMullen <NTaylorMullen@outlook.com>
2013-06-06 12:11:51 -0700

    Added logic to ensure connections can be started and stopped repeatedly without error.
    - Also added logic to ensure that calling start multiple times works correctly
    
    #1873

--- samples/Microsoft.AspNet.SignalR.LoadTestHarness/Scripts/jquery.signalR.js
+++ samples/Microsoft.AspNet.SignalR.LoadTestHarness/Scripts/jquery.signalR.js
@@ -328,6 +328,9 @@
                 deferred = connection._deferral || $.Deferred(), // Check to see if there is a pre-existing deferral that's being built on, if so we want to keep using it
                 parser = window.document.createElement("a");
 
+            // Persist the deferral so that if start is called multiple times the same deferral is used.
+            connection._deferral = deferred;
+
             if (!connection.json) {
                 // no JSON!
                 throw new Error("SignalR: No JSON parser found. Please ensure json2.js is referenced before the SignalR.js file if you need to support clients without native JSON parsing support, e.g. IE<8.");
@@ -354,7 +357,6 @@
             // If waitForPageLoad is true we then want to re-direct function call to the window load event
             if (!_pageLoaded && config.waitForPageLoad === true) {
                 _pageWindow.load(function () {
-                    connection._deferral = deferred;
                     connection.start(options, callback);
                 });
                 return deferred.promise();
@@ -362,10 +364,16 @@
 
             configureStopReconnectingTimeout(connection);
 
-            if (changeState(connection,
+            // If we're already connecting just return the same deferral as the original connection start
+            if (connection.state === signalR.connectionState.connecting) {
+                return deferred.promise();
+            }                
+            else if (changeState(connection,
                             signalR.connectionState.disconnected,
                             signalR.connectionState.connecting) === false) {
-                // Already started, just return
+                // We're not connecting so try and transition into connecting.
+                // If we fail to transition then we're either in connected or reconnecting.
+
                 deferred.resolve(connection);
                 return deferred.promise();
             }
@@ -439,6 +447,11 @@
                     return;
                 }
 
+                // The connection was aborted
+                if (connection.state === signalR.connectionState.disconnected) {
+                    return;
+                }
+
                 var transportName = transports[index],
                     transport = $.type(transportName) === "object" ? transportName : signalR.transports[transportName],
                     initializationComplete = false,
@@ -465,6 +478,12 @@
                     }, connection.transportConnectTimeout);
 
                     transport.start(connection, function () { // success
+                    // The connection was aborted while initializing transports
+                    if (connection.state === signalR.connectionState.disconnected) {
+                        transport.stop(connection);
+                        return;
+                    }
+
                         if (!initializationComplete) {
                             initializationComplete = true;
 
@@ -717,11 +736,22 @@
             /// <returns type="signalR" />
             var connection = this;
 
+            if (!_pageLoaded) {
+                // Can only stop connections after the page has loaded
+                _pageWindow.load(function () {
+                    connection.stop(async, notifyServer);
+                });
+
+                return;
+            }
+
             if (connection.state === signalR.connectionState.disconnected) {
                 return;
             }
 
             try {
+                connection.log("SignalR: Stopping connection.");
+
                 // Clear this no matter what
                 window.clearTimeout(connection._.onFailedTimeoutHandle);
 
