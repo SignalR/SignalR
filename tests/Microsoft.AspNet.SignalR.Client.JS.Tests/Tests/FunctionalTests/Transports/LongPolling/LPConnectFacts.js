fa97122ac44a7384d48bdd707aa5127c4e550a34
NTaylorMullen <NTaylorMullen@outlook.com>
2013-06-20 14:34:25 -0700

    Added test to verify starting and stopping doesn't result in multiple client side connections.
    - Also had to fix up some past tests that were not returning their ajax handles in replaced ajax functions.
    
    #2160

---
+++ tests/Microsoft.AspNet.SignalR.Client.JS.Tests/Tests/FunctionalTests/Transports/LongPolling/LPConnectFacts.js
@@ -0,0 +1,47 @@
+﻿QUnit.module("Long Polling Facts", testUtilities.transports.longPolling.enabled);
+
+QUnit.asyncTimeoutTest("Starting and stopping repeatedly doesn't result in multiple active ajax requests.", testUtilities.defaultTestTimeout, function (end, assert, testName) {
+    var connection = testUtilities.createHubConnection(end, assert, testName, undefined, false),
+        transport = { transport: "longPolling" },
+        savedAjax = $.ajax,
+        pollCount = 0,
+        repeatCount = 3,
+        connectionsStarted = 0;
+
+    function ajaxReplacement(url, settings) {
+        if (!settings) {
+            settings = url;
+            url = settings.url;
+        }
+
+        if (url.indexOf("/poll") >= 0) {
+            pollCount++;
+        }
+
+        // Persist the request through to the original ajax request
+        return savedAjax.call(this, url, settings);
+    };
+
+    $.ajax = ajaxReplacement;
+
+    for (var i = 0; i < repeatCount; i++) {
+        connection.start(transport).done(function () {
+            if (connectionsStarted === repeatCount) {
+                setTimeout(function () {
+                    assert.equal(pollCount, 1, "Poll was only called once (for the init message).");
+                    end();
+                }, 3000);
+            }
+        });
+
+        if (++connectionsStarted !== repeatCount) {
+            connection.stop();
+        }
+    }
+
+    // Cleanup
+    return function () {
+        $.ajax = savedAjax;
+        connection.stop();
+    };
+});
