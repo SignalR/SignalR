be337d559d270e48e32613e82130cb6ca7ba37b7
NTaylorMullen <NTaylorMullen@outlook.com>
2013-06-28 11:46:04 -0700

    Removed timeout connecting check from server sent events start logic.
    - This is legacy code, this logic used to ignore reconnect events when a connection was initially started to prevent multiple connections from spawning, now that connection timeout's are handled at the core level and not the transport level this is not needed.
    
    #1946

--- samples/Microsoft.AspNet.SignalR.Samples/Scripts/jquery.signalR.js
+++ samples/Microsoft.AspNet.SignalR.Samples/Scripts/jquery.signalR.js
@@ -1404,8 +1404,7 @@
                     if (opened === false) {
                         // If we're reconnecting and the event source is attempting to connect,
                         // don't keep retrying. This causes duplicate connections to spawn.
-                        if (connection.eventSource.readyState !== window.EventSource.CONNECTING &&
-                            connection.eventSource.readyState !== window.EventSource.OPEN) {
+                        if (connection.eventSource.readyState !== window.EventSource.OPEN) {
                             // If we were reconnecting, rather than doing initial connect, then try reconnect again
                             that.reconnect(connection);
                         }
