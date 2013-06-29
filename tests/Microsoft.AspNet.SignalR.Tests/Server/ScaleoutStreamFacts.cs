22dc13dde8f31002f6a63fde6887baaca9d9bcd5
David Fowler <davidfowl@gmail.com>
2013-06-28 21:42:45 -0700

    Stop test timeouts

--- tests/Microsoft.AspNet.SignalR.Tests/Server/ScaleoutStreamFacts.cs
+++ tests/Microsoft.AspNet.SignalR.Tests/Server/ScaleoutStreamFacts.cs
@@ -46,7 +46,7 @@ namespace Microsoft.AspNet.SignalR.Tests.Server
         }
 
 
-        [Fact(Timeout = 1000)]
+        [Fact(Timeout = 2000)]
         public void OpenAfterErrorRunsQueue()
         {
             int x = 0;
