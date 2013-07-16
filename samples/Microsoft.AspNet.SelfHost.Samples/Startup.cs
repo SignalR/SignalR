6f840ede845b1a8c016fe13449c13b0d1baca40b
David Fowler <davidfowl@gmail.com>
2013-07-15 21:49:18 -0700

    Updated samples to use new cors middleware.

--- samples/Microsoft.AspNet.SelfHost.Samples/Startup.cs
+++ samples/Microsoft.AspNet.SelfHost.Samples/Startup.cs
@@ -1,7 +1,9 @@
 ﻿using System.Diagnostics;
+using System.Web.Cors;
 using Microsoft.AspNet.SignalR;
 using Microsoft.AspNet.SignalR.Samples;
 using Microsoft.AspNet.SignalR.Tracing;
+using Microsoft.Owin.Cors;
 using Owin;
 
 namespace Microsoft.AspNet.SelfHost.Samples
@@ -12,6 +14,17 @@ namespace Microsoft.AspNet.SelfHost.Samples
         {
             app.Map("/raw-connection", subApp =>
             {
+                subApp.UseCors(new CorsOptions
+                {
+                    CorsPolicy = new CorsPolicy
+                    {
+                        AllowAnyHeader = true,
+                        AllowAnyMethod = true,
+                        AllowAnyOrigin = true,
+                        SupportsCredentials = true
+                    }
+                });
+
                 subApp.UseConnection<RawConnection>();
             });
 
--- tests/Microsoft.AspNet.SignalR.Tests.Common/App_Start/Initializer.cs
+++ tests/Microsoft.AspNet.SignalR.Tests.Common/App_Start/Initializer.cs
@@ -4,6 +4,7 @@ using System.Diagnostics;
 using System.IO;
 using System.Threading.Tasks;
 using System.Web;
+using System.Web.Cors;
 using System.Web.Routing;
 using Microsoft.AspNet.SignalR.Infrastructure;
 using Microsoft.AspNet.SignalR.StressServer.Connections;
@@ -11,6 +12,7 @@ using Microsoft.AspNet.SignalR.Tests.Common;
 using Microsoft.AspNet.SignalR.Tests.Common.Connections;
 using Microsoft.AspNet.SignalR.Tests.Common.Handlers;
 using Microsoft.Owin;
+using Microsoft.Owin.Cors;
 using Owin;
 
 [assembly: PreApplicationStartMethod(typeof(Initializer), "Start")]
@@ -98,12 +100,34 @@ namespace Microsoft.AspNet.SignalR.Tests.Common
 
             app.Map("/multisend", subApp =>
             {
+                app.UseCors(new CorsOptions
+                {
+                    CorsPolicy = new CorsPolicy
+                    {
+                        AllowAnyHeader = true,
+                        AllowAnyMethod = true,
+                        AllowAnyOrigin = true,
+                        SupportsCredentials = true
+                    }
+                });
+
                 subApp.UseConnection<MySendingConnection>(config);
             });
 
             app.Map("/autoencodedjson", subApp =>
             {
-                subApp.MapConnection<AutoEncodedJsonConnection>("/autoencodedjson", config);
+                app.UseCors(new CorsOptions
+                {
+                    CorsPolicy = new CorsPolicy
+                    {
+                        AllowAnyHeader = true,
+                        AllowAnyMethod = true,
+                        AllowAnyOrigin = true,
+                        SupportsCredentials = true
+                    }
+                });
+
+                subApp.UseConnection<AutoEncodedJsonConnection>(config);
             });
 
             app.MapConnection<MyBadConnection>("/ErrorsAreFun", config);
