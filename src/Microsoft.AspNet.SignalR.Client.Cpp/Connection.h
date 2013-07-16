d04f2c4de8366b89fc07f752e98b9bac521093ea
John Luo <jt.luo@mail.utoronto.ca>
2013-07-15 18:27:27 -0700

    Added copyright headers in all the files in the c++ client

--- src/Microsoft.AspNet.SignalR.Client.Cpp/Connection.h
+++ src/Microsoft.AspNet.SignalR.Client.Cpp/Connection.h
@@ -1,6 +1,10 @@
+//Copyright (c) Microsoft Corporation
+//
+//All rights reserved.
+//
+//THIS CODE IS PROVIDED ON AN *AS IS* BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING WITHOUT LIMITATION ANY IMPLIED WARRANTIES OR CONDITIONS OF TITLE, FITNESS FOR A PARTICULAR PURPOSE, MERCHANTABLITY, OR NON-INFRINGEMENT.
+
 //  Variable Prefix Convention
-//  http://stackoverflow.com/questions/1228161/why-use-prefixes-on-member-variables-in-c-classes third answer
-//  I use:
 //
 //  m for members
 //  p for member shared_ptr or unique_ptr
--- src/Microsoft.AspNet.SignalR.Client.Cpp/TransportAbortHandler.h
+++ src/Microsoft.AspNet.SignalR.Client.Cpp/TransportAbortHandler.h
@@ -4,6 +4,12 @@
 #include "IHttpClient.h"
 #include "Connection.h"
 #include <agents.h>
+//Copyright (c) Microsoft Corporation
+//
+//All rights reserved.
+//
+//THIS CODE IS PROVIDED ON AN *AS IS* BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING WITHOUT LIMITATION ANY IMPLIED WARRANTIES OR CONDITIONS OF TITLE, FITNESS FOR A PARTICULAR PURPOSE, MERCHANTABLITY, OR NON-INFRINGEMENT.
+
 #include "TaskAsyncHelper.h"
 #include "TransportHelper.h"
 
