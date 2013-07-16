02e649d02f6ce9f81a2fbf10a48087c28f2d5a94
NTaylorMullen <NTaylorMullen@outlook.com>
2013-07-16 11:17:58 -0700

    Added a ping interval on the client to ensure that auth tickets do not expire when there's lack of activity.
    - This also involved adding a new configuration option 'pingInterval' which can be set via seconds

--- src/Microsoft.AspNet.SignalR.Client.JS/jquery.signalR.core.js
+++ src/Microsoft.AspNet.SignalR.Client.JS/jquery.signalR.core.js
@@ -87,6 +87,21 @@
             return connection.state === signalR.connectionState.disconnected;
         },
 
+        configurePingInterval = function (connection) {
+            var config = connection._.config,
+                onFail = function () {
+                    $(connection).triggerHandler(events.onError, ["SignalR: Pinging server failed, current user may no longer be authenticated, stopping the connection."]);
+                    // If we fail then we should kill the connection
+                    connection.stop();
+                };
+
+            if (!config.pingIntervalId && config.pingInterval) {
+                connection._.pingIntervalId = window.setInterval(function () {
+                    signalR.transports._logic.pingServer(connection).fail(onFail);
+                }, config.pingInterval);
+            }
+        },
+
         configureStopReconnectingTimeout = function (connection) {
             var stopReconnectingTimeout,
                 onReconnectTimeout;
@@ -363,6 +378,7 @@
             /// <param name="callback" type="Function">A callback function to execute when the connection has started</param>
             var connection = this,
                 config = {
+                    pingInterval: 300000,
                     waitForPageLoad: true,
                     transport: "auto",
                     jsonp: false,
@@ -409,8 +425,6 @@
                 return deferred.promise();
             }
 
-            configureStopReconnectingTimeout(connection);
-
             // If we're already connecting just return the same deferral as the original connection start
             if (connection.state === signalR.connectionState.connecting) {
                 return deferred.promise();
@@ -423,6 +437,8 @@
                 deferred.resolve(connection);
                 return deferred.promise();
             }
+
+            configureStopReconnectingTimeout(connection);
 
             // Resolve the full url
             parser.href = connection.url;
@@ -538,6 +554,10 @@
                             if (transport.supportsKeepAlive && connection.keepAliveData.activated) {
                                 signalR.transports._logic.monitorKeepAlive(connection);
                             }
+
+                            // Used to ensure low activity clients maintain their authentication.
+                            // Must be configured once a transport has been decided to perform valid ping requests.
+                            configurePingInterval(connection);
 
                             changeState(connection,
                                         signalR.connectionState.connecting,
@@ -823,6 +843,7 @@
 
                 // Clear this no matter what
                 window.clearTimeout(connection._.onFailedTimeoutHandle);
+                window.clearInterval(connection._.pingIntervalId);
 
                 if (connection.transport) {
                     if (notifyServer !== false) {
@@ -853,6 +874,7 @@
                 delete connection.id;
                 delete connection._deferral;
                 delete connection._.config;
+                delete connection._.pingIntervalId;
 
                 // Clear out our message buffer
                 connection._.connectingMessageBuffer.clear();
