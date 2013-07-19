2ed61b94eeb0b46c88ab12197939e873e5d94509
John Luo <jt.luo@mail.utoronto.ca>
2013-07-19 14:36:06 -0700

    Updated Casablanca version

--- samples/Microsoft.AspNet.SignalR.Client.Samples/Program.cs
+++ samples/Microsoft.AspNet.SignalR.Client.Samples/Program.cs
@@ -13,14 +13,6 @@ namespace Microsoft.AspNet.SignalR.Client.Samples
             var client = new CommonClient(writer);
             client.RunAsync("http://localhost:40476/").Wait();
 
-            // var hubConnection = new HubConnection("http://localhost:40476/");
-
-            // RunDemoHub(hubConnection);
-
-            RunStreamingSample();
-
-            //RunStatusHub();
-
             Console.ReadKey();
         }
     }
