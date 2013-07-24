56c0223f8501ed9ad7c354488ed93d357e1b0da0
David Fowler <davidfowl@gmail.com>
2013-07-23 23:13:26 -0700

    Use async/await in the Subscription.

--- tests/Microsoft.AspNet.SignalR.Tests/Server/SubscriptionFacts.cs
+++ tests/Microsoft.AspNet.SignalR.Tests/Server/SubscriptionFacts.cs
@@ -22,7 +22,7 @@ namespace Microsoft.AspNet.SignalR.Tests.Server
                 return tcs.Task;
             };
 
-            var subscription = new Mock<TestSubscription>("TestSub", new[] { "a" }, callback)
+            var subscription = new Mock<TestSubscription>("TestSub", new[] { "a" }, callback, 1)
             {
                 CallBase = true
             };
@@ -51,7 +51,7 @@ namespace Microsoft.AspNet.SignalR.Tests.Server
                 return await tcs.Task;
             };
 
-            var subscription = new Mock<TestSubscription>("TestSub", new[] { "a" }, callback)
+            var subscription = new Mock<TestSubscription>("TestSub", new[] { "a" }, callback, 1)
             {
                 CallBase = true
             };
@@ -73,7 +73,7 @@ namespace Microsoft.AspNet.SignalR.Tests.Server
                 return false;
             };
 
-            var subscription = new Mock<TestSubscription>("TestSub", new[] { "a" }, callback)
+            var subscription = new Mock<TestSubscription>("TestSub", new[] { "a" }, callback, 1)
             {
                 CallBase = true
             };
@@ -101,7 +101,7 @@ namespace Microsoft.AspNet.SignalR.Tests.Server
                 return false;
             };
 
-            var subscription = new Mock<TestSubscription>("TestSub", new[] { "a" }, callback)
+            var subscription = new Mock<TestSubscription>("TestSub", new[] { "a" }, callback, 1)
             {
                 CallBase = true
             };
@@ -114,18 +114,69 @@ namespace Microsoft.AspNet.SignalR.Tests.Server
             }
         }
 
+        [Fact(Timeout = 5000)]
+        public void NoItemsCompletesTask()
+        {
+            Func<MessageResult, object, Task<bool>> callback = (result, state) =>
+            {
+                return TaskAsyncHelper.False;
+            };
+
+            var subscription = new Mock<TestSubscription>("TestSub", new[] { "a" }, callback, 0)
+            {
+                CallBase = true
+            };
+
+            using (subscription.Object)
+            {
+                Task task = subscription.Object.Work();
+                Assert.True(task.IsCompleted);
+            }
+        }
+
+        [Fact(Timeout = 5000)]
+        public void ReturningFalseFromCallbackCompletesTaskAndDisposesSubscription()
+        {
+            Func<MessageResult, object, Task<bool>> callback = (result, state) =>
+            {
+                return TaskAsyncHelper.False;
+            };
+
+            var subscription = new Mock<TestSubscription>("TestSub", new[] { "a" }, callback, 1)
+            {
+                CallBase = true
+            };
+
+            var disposableMock = new Mock<IDisposable>();
+            subscription.Object.Disposable = disposableMock.Object;
+            disposableMock.Setup(m => m.Dispose()).Verifiable();
+
+            using (subscription.Object)
+            {
+                Task task = subscription.Object.Work();
+                Assert.True(task.IsCompleted);
+                task.Wait();
+                disposableMock.Verify(m => m.Dispose(), Times.Once());
+            }
+        }
+
         public class TestSubscription : Subscription
         {
-            public TestSubscription(string identity, IList<string> eventKeys, Func<MessageResult, object, Task<bool>> callback)
+            private readonly int _itemCount;
+            public TestSubscription(string identity, IList<string> eventKeys, Func<MessageResult, object, Task<bool>> callback, int itemCount)
                 : base(identity, eventKeys, callback, 10, GetCounters(), state: null)
             {
-
+                _itemCount = itemCount;
             }
 
             protected override void PerformWork(IList<ArraySegment<Message>> items, out int totalCount, out object state)
             {
-                items.Add(new ArraySegment<Message>());
-                totalCount = 1;
+                for (int i = 0; i < _itemCount; i++)
+                {
+                    items.Add(new ArraySegment<Message>());
+                }
+
+                totalCount = _itemCount;
                 state = null;
             }
 
