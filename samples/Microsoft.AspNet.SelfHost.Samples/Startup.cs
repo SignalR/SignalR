7832fa5da58e058d10495c7b772589e222c803be
David Fowler <davidfowl@gmail.com>
2013-07-28 13:50:54 -0700

    Made some changes to Owin/OwinExtensions - Added UseHubs with no arguments. - Added doc comments to IAppBuilder extension methods. - Added Diagnostics package to self host sample. - Added cross domain support to self host sample for hubs.

--- samples/Microsoft.AspNet.SelfHost.Samples/Startup.cs
+++ samples/Microsoft.AspNet.SelfHost.Samples/Startup.cs
@@ -12,13 +12,31 @@ namespace Microsoft.AspNet.SelfHost.Samples
     {
         public void Configuration(IAppBuilder app)
         {
+            app.UseErrorPage();
+
             app.Map("/raw-connection", map =>
             {
+                // Turns cors support on allowing everything
+                // In real applications, the origins should be locked down
                 map.UseCors(CorsOptions.AllowAll)
                    .UseConnection<RawConnection>();
             });
 
-            app.MapHubs();
+            app.Map("/signalr", map =>
+            {
+                var config = new HubConfiguration
+                {
+                    // You can enable JSONP by uncommenting this line
+                    // JSONP requests are insecure but some older browsers (and some
+                    // versions of IE) require JSONP to work cross domain
+                    // EnableJSONP = true
+                };
+
+                // Turns cors support on allowing everything
+                // In real applications, the origins should be locked down
+                map.UseCors(CorsOptions.AllowAll)
+                   .UseHubs(config);
+            });
 
             // Turn tracing on programmatically
             GlobalHost.TraceManager.Switch.Level = SourceLevels.Information;
