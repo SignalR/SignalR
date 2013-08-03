8c9c555d86fca36c46683e40db268858893544cb
Abhishek Nanda <abnanda@microsoft.com>
2013-08-02 17:33:19 -0700

    Added default values to intellisense for timeout settings
    #2370

--- src/Microsoft.AspNet.SignalR.Core/Configuration/IConfigurationManager.cs
+++ src/Microsoft.AspNet.SignalR.Core/Configuration/IConfigurationManager.cs
@@ -13,22 +13,26 @@ namespace Microsoft.AspNet.SignalR.Configuration
         /// <summary>
         /// Gets or sets a <see cref="TimeSpan"/> representing the amount of time a client should allow to connect before falling
         /// back to another transport or failing.
+        /// The default value is 5 seconds.
         /// </summary>
         TimeSpan TransportConnectTimeout { get; set; }
 
         /// <summary>
         /// Gets or sets a <see cref="TimeSpan"/> representing the amount of time to leave a connection open before timing out.
+        /// The default value is 110 seconds.
         /// </summary>
         TimeSpan ConnectionTimeout { get; set; }
 
         /// <summary>
         /// Gets or sets a <see cref="TimeSpan"/> representing the amount of time to wait after a connection goes away before raising the disconnect event.
+        /// The default value is 30 seconds.
         /// </summary>
         TimeSpan DisconnectTimeout { get; set; }
 
         /// <summary>
         /// Gets or sets a <see cref="TimeSpan"/> representing the amount of time between send keep alive messages.
         /// If enabled, this value must be at least two seconds. Set to null to disable.
+        /// The default value is 10 seconds.
         /// </summary>
         TimeSpan? KeepAlive { get; set; }
 
