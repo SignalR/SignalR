4dbdef4dfc3685a2f3d19fde9f3a8f6260d7a266
David Fowler <davidfowl@gmail.com>
2013-08-18 18:31:24 -0700

    Bring back GetHttpContext extension on IRequest.

---
+++ src/Microsoft.AspNet.SignalR.SystemWeb/RequestExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System.Web;
+
+namespace Microsoft.AspNet.SignalR
+{
+    public static class RequestExtensions
+    {
+        /// <summary>
+        /// Returns the <see cref="HttpContextBase"/> for this <see cref="IRequest"/>.
+        /// </summary>
+        /// <param name="request">The request</param>
+        public static HttpContextBase GetHttpContext(this IRequest request)
+        {
+            object httpContextBaseValue;
+            if (request.Environment.TryGetValue(typeof(HttpContextBase).FullName, out httpContextBaseValue))
+            {
+                return httpContextBaseValue as HttpContextBase;
+            }
+
+            return null;
+        }
+    }
+}
