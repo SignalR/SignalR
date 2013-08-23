6fcfeb6f38cb837cdf60bcd66afa7009b5deee51
Gustavo Armenta <gustavo_armenta@yahoo.com>
2013-08-23 15:29:48 -0700

    test nuget packages on all feeds

---
+++ tests/Microsoft.AspNet.SignalR.DailyTests/NuGet/OwinFacts.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using Xunit.Extensions;
+
+namespace Microsoft.AspNet.SignalR.FunctionalTests.NuGet
+{
+    public class OwinFacts
+    {
+        public static IEnumerable<string[]> Feeds
+        {
+            get
+            {
+                return new List<string[]>()
+                {
+                    new []{"OwinTeamCityDev"},
+                    new []{"OwinTeamCityRelease"},
+                    new []{"aspnetwebstacknightly"},
+                    new []{"aspnetwebstacknightlyrelease"},
+                    new []{"Staging"},
+                    new []{"Production"},
+                };
+            }
+        }        
+
+        [Theory]
+        [PropertyData("Feeds")]
+        public void OwinCors(string name)
+        {
+            CommonNuGet.Run("Microsoft.Owin.Cors", name);
+        }
+    }
+}
