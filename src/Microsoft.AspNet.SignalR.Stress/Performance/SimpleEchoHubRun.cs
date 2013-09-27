7e4a335eca193835c0444f0f1d85e999095c5b90
Christy Henriksson <chenriks@microsoft.com>
2013-09-27 14:27:14 -0700

    Fixing Stress SimpleEchoHub latency tests; PR feedback -use async/await for callback completions -use proxy handler generic arg for long cast -changing back to ms units

--- src/Microsoft.AspNet.SignalR.Stress/Performance/SimpleEchoHubRun.cs
+++ src/Microsoft.AspNet.SignalR.Stress/Performance/SimpleEchoHubRun.cs
@@ -1,10 +1,9 @@
 ﻿// Copyright (c) Microsoft Open Technologies, Inc. All rights reserved. See License.md in the project root for license information.
 
 using System;
-using System.Collections.Generic;
+using System.Collections.Concurrent;
 using System.ComponentModel.Composition;
 using System.Diagnostics;
-using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.AspNet.SignalR.Client;
 using Microsoft.AspNet.SignalR.Client.Hubs;
@@ -18,11 +17,8 @@ namespace Microsoft.AspNet.SignalR.Stress.Performance
         private IHubProxy[] _proxies;
         private bool _sampling;
         private string _categoryString;
-        private static List<ManualResetEvent> _callbacks;
-
-#if !PERFRUN
-        List<long> _latencySamples = new List<long>();
-#endif
+        private TaskCompletionSource<bool>[] _callbacks;
+        private ConcurrentBag<long> _latencySamples = new ConcurrentBag<long>();
 
         [ImportingConstructor]
         public SimpleEchoHubRun(RunData runData)
@@ -30,18 +26,17 @@ namespace Microsoft.AspNet.SignalR.Stress.Performance
         {
             Debug.Assert(Connections == Senders);
 
-            _categoryString = String.Format("{0};{1}", ScenarioName, "Latency in Microseconds");
+            _categoryString = String.Format("{0};{1}", ScenarioName, "Latency;Milliseconds");
             _connections = new HubConnection[Connections];
             _proxies = new HubProxy[Connections];
+            _callbacks = new TaskCompletionSource<bool>[Connections];
         }
 
         public override void Initialize()
         {
-            _callbacks = new List<ManualResetEvent>(Connections);
             for (int i = 0; i < Connections; i++)
             {
                 _connections[i] = new HubConnection(RunData.Url);
-                _callbacks.Add(new ManualResetEvent(true));
             }
 
             base.Initialize();
@@ -53,18 +48,17 @@ namespace Microsoft.AspNet.SignalR.Stress.Performance
             HubConnection connection = _connections[connectionIndex];
             IHubProxy proxy = connection.CreateHubProxy("SimpleEchoHub");
 
-            proxy.On<string>("echo", startTicks =>
+            proxy.On<long>("echo", startTicks =>
             {
                 if (_sampling)
                 {
-                    var elapsedMicroseconds = (double)(DateTime.Now.Ticks - long.Parse(startTicks)) / TimeSpan.TicksPerMillisecond * 1000;
+                    var elapsedMilliseconds = Math.Round((double)(DateTime.Now.Ticks - startTicks) / TimeSpan.TicksPerMillisecond);
 #if PERFRUN
-                    Microsoft.VisualStudio.Diagnostics.Measurement.MeasurementBlock.Mark((ulong)elapsedMicroseconds, _categoryString);
-#else
-                    _latencySamples.Add((long)elapsedMicroseconds);
+                    Microsoft.VisualStudio.Diagnostics.Measurement.MeasurementBlock.Mark((ulong)elapsedMilliseconds, _categoryString);
 #endif
+                    _latencySamples.Add((long)elapsedMilliseconds);
                 }
-                _callbacks[connectionIndex].Set();
+                _callbacks[connectionIndex].SetResult(true);
             });
 
             connection.Start(Host.TransportFactory()).Wait();
@@ -78,10 +72,7 @@ namespace Microsoft.AspNet.SignalR.Stress.Performance
         {
             _sampling = false;
             base.Record();
-
-#if !PERFRUN
             base.RecordAggregates(_categoryString, _latencySamples.ToArray());
-#endif
         }
 
         public override void Sample()
@@ -91,11 +82,11 @@ namespace Microsoft.AspNet.SignalR.Stress.Performance
             base.Sample();
         }
 
-        // client sends message. Note senderIndex could be smaller than the #connections
-        protected override Task Send(int senderIndex, string source)
+        protected override async Task Send(int senderIndex, string source)
         {
-            _callbacks[senderIndex].Reset();
-            return _proxies[senderIndex].Invoke("Echo", DateTime.Now.Ticks).ContinueWith(_ => _callbacks[senderIndex].WaitOne());
+            _callbacks[senderIndex] = new TaskCompletionSource<bool>();
+            await _proxies[senderIndex].Invoke("Echo", DateTime.Now.Ticks);
+            await _callbacks[senderIndex].Task;
         }
     }
 }
