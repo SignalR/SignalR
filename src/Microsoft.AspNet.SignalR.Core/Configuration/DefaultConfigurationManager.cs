c79b3ffbd7418aa1d6652e0161afc268e6be6e9e
NTaylorMullen <NTaylorMullen@outlook.com>
2013-09-30 10:52:51 -0700

    Addressed code review comments.
    #2484

--- src/Microsoft.AspNet.SignalR.Core/Configuration/DefaultConfigurationManager.cs
+++ src/Microsoft.AspNet.SignalR.Core/Configuration/DefaultConfigurationManager.cs
@@ -22,8 +22,6 @@ namespace Microsoft.AspNet.SignalR.Configuration
 
         public DefaultConfigurationManager()
         {
-            // Make the connection timeout equivalent to the disconnect timeout so on the client we know when the
-            // server "may" have forgotten about us for the Long Polling transport.
             ConnectionTimeout = TimeSpan.FromSeconds(110);
             DisconnectTimeout = TimeSpan.FromSeconds(30);
             DefaultMessageBufferSize = 1000;
