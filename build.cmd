772caa60cd45306fca8761f9395dcb5f2afd5229
DamianEdwards <damian@damianedwards.com>
2013-10-14 11:33:36 -0700

    Fixed cmd line build for VS2013 only machines - Ensure we pass through the VisualStudioVersion property to sub-builds - Remove the hard-coded path to msbuild, means you need to run build.cmd from a VS dev cmd prompt

--- build.cmd
+++ build.cmd
@@ -7,4 +7,4 @@ set config=%2
 if "%config%" == "" (
    set config=Debug
 )
-%WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild Build\Build.proj /t:"%target%" /p:Configuration="%config%" /m /fl /flp:LogFile=msbuild.log;Verbosity=Normal /nr:false
+msbuild Build\Build.proj /t:"%target%" /p:Configuration="%config%" /m /fl /flp:LogFile=msbuild.log;Verbosity=Normal /nr:false
