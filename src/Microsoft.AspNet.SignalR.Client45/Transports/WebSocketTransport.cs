018015f4d5e4d5dced346e96dda72a29426764a0
N. Taylor Mullen <nimullen@microsoft.com>
2013-11-04 15:51:30 -0800

    Added VerifyReconnect steps to the MarkActive function of connection.
    - Added a test to verify the behavior
    - Also fixed an issue that caused "Stop" to throw if the connection had not already been started.
    
    #2528

--- src/Microsoft.AspNet.SignalR.Client45/Transports/WebSocketTransport.cs
+++ src/Microsoft.AspNet.SignalR.Client45/Transports/WebSocketTransport.cs
@@ -187,7 +187,7 @@ namespace Microsoft.AspNet.SignalR.Client.Transports
 
         private async void DoReconnect()
         {
-            while (TransportHelper.VerifyReconnect(_connectionInfo.Connection) && _connectionInfo.Connection.EnsureReconnecting())
+            while (TransportHelper.VerifyLastActive(_connectionInfo.Connection) && _connectionInfo.Connection.EnsureReconnecting())
             {
                 try
                 {
