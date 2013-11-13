eac42d76fdf41084c879f93ce17d9118068b95c6
Abhishek Nanda <abnanda@microsoft.com>
2013-11-12 18:40:26 -0800

    Modified build, added sample for Xamarin component and made changes as per code review feedback

---
+++ xamarin/SignalRPackage/component/samples/SignalRSample/MainActivity.cs
@@ -0,0 +1,14 @@
+using System;
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace SignalRSample
+{
+	// Please refer to github.com/signalr/signalr for iOS and Android samples
+}
+
+
