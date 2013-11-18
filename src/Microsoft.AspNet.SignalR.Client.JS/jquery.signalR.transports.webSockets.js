cb550e18a8af3050eb0644889d3decb2b44a072b
N. Taylor Mullen <nimullen@microsoft.com>
2013-11-18 13:11:33 -0800

    Added a try-catch to the WebSocket transport send.
    - This helps catch errors that can be thrown from a websocket being in an invalid state.
    - Also added a test to verify that we catch errors thrown by a socket send.
    
    #2582

--- src/Microsoft.AspNet.SignalR.Client.JS/jquery.signalR.transports.webSockets.js
+++ src/Microsoft.AspNet.SignalR.Client.JS/jquery.signalR.transports.webSockets.js
@@ -18,7 +18,18 @@
 
         send: function (connection, data) {
             var payload = transportLogic.stringifySend(connection, data);
-            connection.socket.send(payload);
+
+            try {
+                connection.socket.send(payload);
+            } catch (ex) {
+                $(connection).triggerHandler(events.onError,
+                    [signalR._.transportError(
+                        signalR.resources.webSocketsInvalidState,
+                        connection.transport,
+                        ex,
+                        connection.socket
+                    )]);
+            }
         },
 
         start: function (connection, onSuccess, onFailed) {
---
+++ tests/Microsoft.AspNet.SignalR.Client.JS.Tests/Tests/FunctionalTests/Transports/WebSocketsFacts/WebSocketsFacts.js
@@ -0,0 +1,25 @@
+﻿QUnit.module("WebSockets Facts", testUtilities.transports.webSockets.enabled);
+
+QUnit.asyncTimeoutTest("WebSocket invalid state sends trigger connection error.", testUtilities.defaultTestTimeout, function (end, assert, testName) {
+    var connection = testUtilities.createConnection("signalr", end, assert, testName),
+        customErrorText = "Ouch!";
+
+    connection.error(function (error) {
+        assert.equal(error.message, $.signalR.resources.webSocketsInvalidState, "Web socket transport catches thrown errors from the socket send.");
+        assert.equal(error.source.message, customErrorText, "Web socket transport throws correct error message");
+        end();
+    });
+
+    connection.start({ transport: "webSockets" }).done(function () {
+        connection.socket.send = function () {
+            throw new Error(customErrorText);
+        };
+
+        connection.send("foo");
+    });
+
+    // Cleanup
+    return function () {
+        connection.stop();
+    };
+});
