af35e6445e046345b6345a61d56db72fd2dc879a
Gustavo Armenta <gustavo.armenta@gmail.com>
2014-01-27 11:22:20 -0800

    #2796 allow JsonSerializer.DateParseHandling.None

---
+++ tests/Microsoft.AspNet.SignalR.Tests.Common/Hubs/DateAsStringHub.cs
@@ -0,0 +1,19 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNet.SignalR.Hubs;
+
+namespace Microsoft.AspNet.SignalR.Tests.Common.Hubs
+{
+    public class TypeWithDateAsString
+    {
+        public string DateAsString { get; set; }
+    }
+
+    public class DateAsStringHub : Hub
+    {
+        public TypeWithDateAsString Invoke(TypeWithDateAsString value)
+        {
+            Clients.Caller.Callback(value);
+            return value;
+        }
+    }
+}
