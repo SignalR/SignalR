a088b0c8cf532def474cdb689228eb4148e99a21
Peter Heiberg <peter@tretton37.com>
2014-01-31 14:22:36 +0100

    NullMethodDescriptor now lists potential matching methods #981

--- src/Microsoft.AspNet.SignalR.Core/Hubs/Lookup/Descriptors/NullMethodDescriptor.cs
+++ src/Microsoft.AspNet.SignalR.Core/Hubs/Lookup/Descriptors/NullMethodDescriptor.cs
@@ -3,6 +3,7 @@
 using System;
 using System.Collections.Generic;
 using System.Globalization;
+using System.Linq;
 
 namespace Microsoft.AspNet.SignalR.Hubs
 {
@@ -11,11 +12,13 @@ namespace Microsoft.AspNet.SignalR.Hubs
         private static readonly IEnumerable<Attribute> _attributes = new List<Attribute>();
         private static readonly IList<ParameterDescriptor> _parameters = new List<ParameterDescriptor>();
 
-        private string _methodName;
+        private readonly string _methodName;
+        private readonly IEnumerable<MethodDescriptor> _availableMethods;
 
-        public NullMethodDescriptor(string methodName)
+        public NullMethodDescriptor(string methodName, IEnumerable<MethodDescriptor> availableMethods)
         {
             _methodName = methodName;
+            _availableMethods = availableMethods;
         }
 
         public override Func<IHub, object[], object> Invoker
@@ -24,9 +27,19 @@ namespace Microsoft.AspNet.SignalR.Hubs
             {
                 return (emptyHub, emptyParameters) =>
                 {
-                    throw new InvalidOperationException(String.Format(CultureInfo.CurrentCulture, Resources.Error_MethodCouldNotBeResolved, _methodName));
+                    IEnumerable<string> availableMethodSignatures = GetAvailableMethodSignatures().ToArray();
+                    var message = availableMethodSignatures.Any() ? 
+                        String.Format(CultureInfo.CurrentCulture, Resources.Error_MethodCouldNotBeResolvedCandidates, _methodName, "\n" + String.Join("\n", availableMethodSignatures)) :
+                        String.Format(CultureInfo.CurrentCulture, Resources.Error_MethodCouldNotBeResolved, _methodName);
+                        
+                    throw new InvalidOperationException(String.Format(CultureInfo.CurrentCulture, message));
                 };
             }
+        }
+
+        private IEnumerable<string> GetAvailableMethodSignatures()
+        {
+            return _availableMethods.Select(m => m.Name + "(" + string.Join(", ", m.Parameters.Select(p => p.Name + ":" + p.ParameterType.Name)) + "):" + m.ReturnType.Name);
         }
 
         public override IList<ParameterDescriptor> Parameters
