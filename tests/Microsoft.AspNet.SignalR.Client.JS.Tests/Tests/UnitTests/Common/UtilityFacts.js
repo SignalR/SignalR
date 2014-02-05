49046d7095c1d7de6fb3452eaa2a9fe3ed2f60d1
Stephen Halter <halter73@gmail.com>
2014-02-04 16:10:54 -0800

    JS unit test to verify that active pings are canceled on stop.
    #2840

--- tests/Microsoft.AspNet.SignalR.Client.JS.Tests/Tests/UnitTests/Common/UtilityFacts.js
+++ tests/Microsoft.AspNet.SignalR.Client.JS.Tests/Tests/UnitTests/Common/UtilityFacts.js
@@ -39,3 +39,55 @@ QUnit.test("markActive stops connection if called after extended period of time.
 
     QUnit.equal(stopCalled, true, "Stop was called.");
 });
+
+QUnit.test("Ping XHRs in flight get aborted and deleted on connection stop.", function () {
+    var connection = testUtilities.createConnection(),
+        savedAjax = $.ajax,
+        pingSucceeded = false,
+        pingFailed = false;
+
+    $.ajax = function (settings) {
+        return {
+            abort: function (statusText) {
+                settings.error({}, statusText);
+                settings.complete();
+            },
+            resolve: function (result) {
+                settings.success(result);
+                settings.complete();
+            }
+        };
+    };
+
+    connection.state = $.signalR.connectionState.connected;
+
+    $.signalR.transports._logic.pingServer(connection).done(function () {
+        pingSucceeded = true;
+    });
+
+    $.signalR.transports._logic.pingServer(connection).fail(function () {
+        pingFailed = true;
+    });
+
+    $.signalR.transports._logic.pingServer(connection).done(function () {
+        QUnit.fail("Last ping should neither succeed or fail, because it should be aborted on stop.");
+    }).fail(function () {
+        QUnit.fail("Last ping should neither succeed or fail, because it should be aborted on stop.");
+    });
+
+
+    connection._.activePings[0].resolve('{"Response": "pong"}');
+    QUnit.isTrue(pingSucceeded, "The first ping should succeed.");
+    QUnit.isNotSet(connection._.activePings[0], "The first ping should be deleted after it succeeds.");
+
+    // Without a statusText, the following should not be confused for an abort in stop.
+    connection._.activePings[1].abort();
+    QUnit.isTrue(pingFailed, "The second ping should fail.");
+    QUnit.isNotSet(connection._.activePings[1], "The first second ping should be deleted after it fails.");
+
+    QUnit.isSet(connection._.activePings[2], "The last ping shouldn't complete until stop aborts it.");
+    connection.stop();
+    QUnit.isNotSet(connection._.activePings[2], "The last ping should be deleted after stop aborts it.")
+
+    $.ajax = savedAjax;
+});
