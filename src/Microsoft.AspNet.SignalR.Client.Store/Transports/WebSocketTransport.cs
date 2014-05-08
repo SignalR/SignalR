c5964793f709a2458998cfdad93d4f194afe7354
moozzyk <pawelka@microsoft.com>
2014-05-08 10:52:09 -0700

    97%There - Creating projects for Store WebSocketTransport
    Adding VS projects for supporting  WebSockets on Windows Phone 8.1.
    Note:
     - Test project requires beta version of xunit (and a corresponding pre-release runner)
     - Nuget.exe had to be updated (Updating NuGet.exe to 2.8.2-beta)
     - Build.proj will be updated seperately
    
    # Please enter the commit message for your changes. Lines starting
    # with '#' will be ignored, and an empty message aborts the commit.
    # On branch feature-StoreWebSocks
    # Changes to be committed:
    #	modified:   .nuget/NuGet.exe
    #	modified:   .nuget/packages.config
    #	modified:   Microsoft.AspNet.SignalR.sln
    #	modified:   src/Microsoft.AspNet.SignalR.Client.Portable/Microsoft.AspNet.SignalR.Client.Portable.csproj
    #	modified:   src/Microsoft.AspNet.SignalR.Client.Portable/packages.config
    #	new file:   src/Microsoft.AspNet.SignalR.Client.Store/Microsoft.AspNet.SignalR.Client.Store.csproj
    #	new file:   src/Microsoft.AspNet.SignalR.Client.Store/Properties/AssemblyInfo.cs
    #	new file:   src/Microsoft.AspNet.SignalR.Client.Store/Transports/WebSocketTransport.cs
    #	new file:   src/Microsoft.AspNet.SignalR.Client.Store/app.config
    #	new file:   src/Microsoft.AspNet.SignalR.Client.Store/packages.config
    #	modified:   src/Microsoft.AspNet.SignalR.Client.WinRT/Resources.resw
    #	new file:   tests/Microsoft.AspNet.SignalR.Client.Store.Tests/Images/UnitTestLogo.scale-100.png
    #	new file:   tests/Microsoft.AspNet.SignalR.Client.Store.Tests/Images/UnitTestSmallLogo.scale-100.png
    #	new file:   tests/Microsoft.AspNet.SignalR.Client.Store.Tests/Images/UnitTestSplashScreen.scale-100.png
    #	new file:   tests/Microsoft.AspNet.SignalR.Client.Store.Tests/Images/UnitTestStoreLogo.scale-100.png
    #	new file:   tests/Microsoft.AspNet.SignalR.Client.Store.Tests/Microsoft.AspNet.SignalR.Client.Store.Tests.csproj
    #	new file:   tests/Microsoft.AspNet.SignalR.Client.Store.Tests/Microsoft.AspNet.SignalR.Client.Store.Tests_TemporaryKey.pfx
    #	new file:   tests/Microsoft.AspNet.SignalR.Client.Store.Tests/Package.appxmanifest
    #	new file:   tests/Microsoft.AspNet.SignalR.Client.Store.Tests/Properties/AssemblyInfo.cs
    #	new file:   tests/Microsoft.AspNet.SignalR.Client.Store.Tests/Transports/WebSocketTransportTests.cs
    #	new file:   tests/Microsoft.AspNet.SignalR.Client.Store.Tests/packages.config
    #

---
+++ src/Microsoft.AspNet.SignalR.Client.Store/Transports/WebSocketTransport.cs
@@ -0,0 +1,51 @@
+﻿// Copyright (c) Microsoft Open Technologies, Inc. All rights reserved. See License.md in the project root for license information.
+
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Microsoft.AspNet.SignalR.Client.Transports
+{
+    public class WebSocketTransport : IClientTransport
+    {
+        public string Name
+        {
+            get { return "webSockets"; }
+        }
+
+        public bool SupportsKeepAlive
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public Task<NegotiationResponse> Negotiate(IConnection connection, string connectionData)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Start(IConnection connection, string connectionData, CancellationToken disconnectToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Send(IConnection connection, string data, string connectionData)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Abort(IConnection connection, TimeSpan timeout, string connectionData)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void LostConnection(IConnection connection)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ tests/Microsoft.AspNet.SignalR.Client.Store.Tests/Transports/WebSocketTransportTests.cs
@@ -0,0 +1,16 @@
+﻿// Copyright (c) Microsoft Open Technologies, Inc. All rights reserved. See License.md in the project root for license information.
+
+using Microsoft.AspNet.SignalR.Client.Transports;
+using Xunit;
+
+namespace Microsoft.AspNet.SignalR.Client.Store.Transports
+{
+    public class WebSocketTransportTests
+    {
+        [Fact]
+        public void NameReturnsCorrectTransportName()
+        {
+            Assert.Equal("webSockets", new WebSocketTransport().Name);
+        }
+    }
+}
