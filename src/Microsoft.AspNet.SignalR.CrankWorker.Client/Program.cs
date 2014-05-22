ecf8c9b61d2fc21a9b481d76d4e5c0227cb1969a
U-REDMOND\mikary <mikary@mikary-desktop.redmond.corp.microsoft.com>
2014-05-22 15:00:54 -0700

    Added CrankWorker projects back into solution, corrected SignalR references.

---
+++ src/Microsoft.AspNet.SignalR.CrankWorker.Client/Program.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNet.SignalR.CrankWorker.WorkerRole;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microsoft.AspNet.SignalR.CrankWorker.Client
+{
+    class Program
+    {
+        private const string TEST_MANAGER_HOST = "http://localhost:38683/";
+
+        static void Main(string[] args)
+        {
+            var path = @"crank.exe";
+
+            var host = (args.Length > 0) ? args[0] : TEST_MANAGER_HOST;
+
+            new Runner().Run(host, path);
+        }
+    }
+}
---
+++ src/Microsoft.AspNet.SignalR.CrankWorker.WorkerRole/WorkerRole.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Threading;
+using Microsoft.WindowsAzure;
+using Microsoft.WindowsAzure.Diagnostics;
+using Microsoft.WindowsAzure.ServiceRuntime;
+using Microsoft.WindowsAzure.Storage;
+using System.IO;
+using Microsoft.Win32;
+
+namespace Microsoft.AspNet.SignalR.CrankWorker.WorkerRole
+{
+    public class WorkerRole : RoleEntryPoint
+    {
+        private const string TEST_MANAGER_HOST = "http://localhost:38683/";
+        //private const string TEST_MANAGER_HOST = "http://perf-demo-mgr.perf-demo.net";
+
+        private const string TCPIP_PARAMETERS = @"HKEY_LOCAL_MACHINE\System\CurrentControlSet\Services\Tcpip\Parameters";
+        private const string MAX_USER_PORT_VALUE_NAME = "MaxUserPort";
+        private const int MAX_USER_PORT_VALUE = 65534;
+
+        public override void Run()
+        {
+            var path = Path.Combine(Environment.GetEnvironmentVariable("RoleRoot") + @"\", @"approot\crank.exe");
+            new Runner().Run(TEST_MANAGER_HOST, path);
+        }
+
+        public override bool OnStart()
+        {
+            // Set the maximum number of concurrent connections 
+            ServicePointManager.DefaultConnectionLimit = 12;
+
+            var maxUserPort = (int?)Registry.GetValue(TCPIP_PARAMETERS, MAX_USER_PORT_VALUE_NAME, null);
+
+            if (!maxUserPort.HasValue || maxUserPort.Value != MAX_USER_PORT_VALUE)
+            {
+                Registry.SetValue(TCPIP_PARAMETERS, MAX_USER_PORT_VALUE_NAME, MAX_USER_PORT_VALUE, RegistryValueKind.DWord);
+
+                ProcessStartInfo restartInfo = new ProcessStartInfo();
+                restartInfo.FileName = "shutdown.exe";
+                restartInfo.Arguments = "-r";
+                Process.Start(restartInfo);
+                while (true) ;
+            }
+
+            // For information on handling configuration changes
+            // see the MSDN topic at http://go.microsoft.com/fwlink/?LinkId=166357.
+
+            return base.OnStart();
+        }
+    }
+}
