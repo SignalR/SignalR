b1ca9be7cec001b7d93fd74bf8d9833af6a8fe58
Stephen Halter <halter73@gmail.com>
2014-07-07 18:51:47 -0700

    Rewrite ProcessStartRequests to not use async/await to avoid deadlocks
    - Deadlocks would occur in the old async/await code if someone attached
      to the PreSendRequestHeaders event.
    
    #3116

--- src/Microsoft.AspNet.SignalR.Core/PersistentConnection.cs
+++ src/Microsoft.AspNet.SignalR.Core/PersistentConnection.cs
@@ -524,11 +524,12 @@ namespace Microsoft.AspNet.SignalR
             return SendJsonResponse(context, JsonSerializer.Stringify(payload));
         }
 
-        private async Task ProcessStartRequest(HostContext context, string connectionId)
+        // Avoid async/await in SignalR 2.X due to https://github.com/SignalR/SignalR/issues/3116
+        private Task ProcessStartRequest(HostContext context, string connectionId)
         {
-            await OnConnected(context.Request, connectionId).OrEmpty();
-            await SendJsonResponse(context, StartJsonPayload);
-            Counters.ConnectionsConnected.Increment();
+            return OnConnected(context.Request, connectionId).OrEmpty()
+                .Then(c => SendJsonResponse(c, StartJsonPayload), context)
+                .Then(c => c.ConnectionsConnected.Increment(), Counters);
         }
 
         private static Task SendJsonResponse(HostContext context, string jsonPayload)
