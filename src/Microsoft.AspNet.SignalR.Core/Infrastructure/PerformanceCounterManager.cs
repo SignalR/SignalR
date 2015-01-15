8a75f335e3c7cef5d24b53ebe2ac292b9098a752
BrannonKing <countprimes@gmail.com>
2015-01-15 11:54:38 -0700

    don't throw an exception if the counters were not installed

--- src/Microsoft.AspNet.SignalR.Core/Infrastructure/PerformanceCounterManager.cs
+++ src/Microsoft.AspNet.SignalR.Core/Infrastructure/PerformanceCounterManager.cs
@@ -438,6 +438,14 @@ namespace Microsoft.AspNet.SignalR.Infrastructure
             // and when they are thrown. 
             try
             {
+                if (!PerformanceCounterCategory.Exists(categoryName))
+                {
+#if !UTILS
+                    _trace.TraceEvent(TraceEventType.Warning, 0, "Performance counter failed to load. The counters are not installed.");
+#endif
+                    return null;
+                }
+
                 var counter = new PerformanceCounter(categoryName, counterName, instanceName, isReadOnly);
 
                 // Initialize the counter sample
