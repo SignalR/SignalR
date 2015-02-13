f59ea8c741634d380b04c49db9ee6a1bcaef7bb2
BrennanConroy <brecon@microsoft.com>
2015-02-13 11:37:27 -0800

    VB Client

---
+++ samples/Microsoft.AspNet.SignalR.ClientVB.Samples/Main.vb
@@ -0,0 +1,12 @@
+﻿Imports System
+
+Module Main
+
+    Sub Main()
+        Dim writer = Console.Out
+        Dim client = New Client(writer)
+        'Connection string
+        client.Run("http://localhost:8080/")
+    End Sub
+
+End Module
---
+++ samples/Microsoft.AspNet.SignalR.ClientVB.Samples/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("Microsoft.AspNet.SignalR.Client.VBSamples")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("Hewlett-Packard Company")> 
+<Assembly: AssemblyProduct("Microsoft.AspNet.SignalR.Client.VBSamples")> 
+<Assembly: AssemblyCopyright("Copyright © Hewlett-Packard Company 2015")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("5b020690-d9fb-4424-8dae-baad6ef4acbf")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
