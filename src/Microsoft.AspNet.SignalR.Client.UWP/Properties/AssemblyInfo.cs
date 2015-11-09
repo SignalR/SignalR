aa5dbd156c539f33b8067e536bee6555251f955b
Brandon H <brandon@bc3tech.net>
2015-11-09 12:00:16 -0600

    create UWP project & folder

---
+++ src/Microsoft.AspNet.SignalR.Client.UWP/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+﻿// Copyright (c) Microsoft Open Technologies, Inc. All rights reserved. See License.md in the project root for license information.
+
+using System.Reflection;
+
+[assembly: AssemblyTitle("Microsoft.AspNet.SignalR.Client.WinRT")]
+[assembly: AssemblyDescription("Windows RT client for SignalR")]
