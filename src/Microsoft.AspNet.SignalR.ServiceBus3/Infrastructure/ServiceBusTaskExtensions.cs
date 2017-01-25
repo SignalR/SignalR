0dab7a760ba3d6cbeaf9b219bd913b1ab0f9e9b9
moozzyk <pawelka@microsoft.com>
2017-01-25 14:47:00 -0800

    ServiceBus 3 project skeleton

---
+++ src/Microsoft.AspNet.SignalR.ServiceBus3/Infrastructure/ServiceBusTaskExtensions.cs
@@ -0,0 +1,16 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System.Threading.Tasks;
+using Microsoft.ServiceBus.Messaging;
+
+namespace Microsoft.AspNet.SignalR.ServiceBus.Infrastructure
+{
+    public static class ServiceBusTaskExtensions
+    {
+        public static Task SendAsync(this TopicClient client, BrokeredMessage message)
+        {
+            return Task.Factory.FromAsync((cb, state) => client.BeginSend((BrokeredMessage)state, cb, null), client.EndSend, message);
+        }
+    }
+}
