0923c03e8a790199e1c7d6cecd1cbd430e745d09
Tim Uy <tim@loqu8.com>
2017-01-31 15:25:55 -0800

    initial Mac client

---
+++ src/Microsoft.AspNet.SignalR.Client.Mac/Properties/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+
+// Information about this assembly is defined by the following attributes. 
+// Change them to the values specific to your project.
+
+[assembly: AssemblyTitle("Microsoft.AspNet.SignalR.Client.Mac")]
+[assembly: AssemblyDescription("Mac client for SignalR")]
