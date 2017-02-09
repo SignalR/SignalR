a485e25bb0badf21bd7bfa94b9b96464eccf413d
Andrew Stanton-Nurse <andrew@andrewnurse.net>
2017-02-09 13:43:00 -0800

    fix #3093 by cleaning up Redis connection state
    also: add some tracing to redis message bus code

--- src/Microsoft.AspNet.SignalR.Redis/RedisConnection.cs
+++ src/Microsoft.AspNet.SignalR.Redis/RedisConnection.cs
@@ -17,7 +17,11 @@ namespace Microsoft.AspNet.SignalR.Redis
 
         public async Task ConnectAsync(string connectionString, TraceSource trace)
         {
-            _connection = await ConnectionMultiplexer.ConnectAsync(connectionString);
+            _connection = await ConnectionMultiplexer.ConnectAsync(connectionString, new TraceTextWriter("ConnectionMultiplexer: ", trace));
+            if(!_connection.IsConnected)
+            {
+                throw new InvalidOperationException("Failed to connect to Redis");
+            }
 
             _connection.ConnectionFailed += OnConnectionFailed;
             _connection.ConnectionRestored += OnConnectionRestored;
@@ -31,6 +35,7 @@ namespace Microsoft.AspNet.SignalR.Redis
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1026:DefaultParametersShouldNotBeUsed")]
         public void Close(string key, bool allowCommandsToComplete = true)
         {
+            _trace.TraceInformation("Closing key: " + key);
             if (_redisSubscriber != null)
             {
                 _redisSubscriber.Unsubscribe(key);
@@ -46,6 +51,7 @@ namespace Microsoft.AspNet.SignalR.Redis
 
         public async Task SubscribeAsync(string key, Action<int, RedisMessage> onMessage)
         {
+            _trace.TraceInformation("Subscribing to key: " + key);
             await _redisSubscriber.SubscribeAsync(key, (channel, data) =>
             {
                 var message = RedisMessage.FromBytes(data, _trace);
@@ -119,18 +125,21 @@ namespace Microsoft.AspNet.SignalR.Redis
 
         private void OnConnectionFailed(object sender, ConnectionFailedEventArgs args)
         {
+            _trace.TraceWarning("Connection failed. Reason: " + args.FailureType.ToString() + " Exception: " + args.Exception.ToString());
             var handler = ConnectionFailed;
             handler(args.Exception);
         }
 
         private void OnConnectionRestored(object sender, ConnectionFailedEventArgs args)
         {
+            _trace.TraceInformation("Connection restored");
             var handler = ConnectionRestored;
             handler(args.Exception);
         }
 
         private void OnError(object sender, RedisErrorEventArgs args)
         {
+            _trace.TraceWarning("Redis Error: " + args.Message);
             var handler = ErrorMessage;
             handler(new InvalidOperationException(args.Message));
         }
