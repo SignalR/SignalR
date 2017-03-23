4f7c22d2ffe7b1564c860c030f41d4c2f80b4310
moozzyk <pawelka@microsoft.com>
2017-03-23 12:23:41 -0700

    Updating xunit version in Daily tests

--- tests/Microsoft.AspNet.SignalR.DailyTests/NuGet/SignalRLocFacts.cs
+++ tests/Microsoft.AspNet.SignalR.DailyTests/NuGet/SignalRLocFacts.cs
@@ -2,6 +2,7 @@
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using System.Collections.Generic;
+using Xunit;
 using Xunit.Extensions;
 
 namespace Microsoft.AspNet.SignalR.FunctionalTests.NuGet
@@ -23,56 +24,56 @@ namespace Microsoft.AspNet.SignalR.FunctionalTests.NuGet
         }
 
         [Theory]
-        [PropertyData("Feeds")]
+        [MemberData(nameof(Feeds))]
         public void SignalRi18n(string name)
         {
             CommonNuGet.Run("Microsoft.AspNet.SignalR.i18n", name);
         }
 
         [Theory]
-        [PropertyData("Feeds")]
+        [MemberData(nameof(Feeds))]
         public void SignalR(string name)
         {
             CommonNuGet.Run("Microsoft.AspNet.SignalR.cs", name);
         }
 
         [Theory]
-        [PropertyData("Feeds")]
+        [MemberData(nameof(Feeds))]
         public void SignalRClient(string name)
         {
             CommonNuGet.Run("Microsoft.AspNet.SignalR.Client.cs", name);
         }
 
         [Theory]
-        [PropertyData("Feeds")]
+        [MemberData(nameof(Feeds))]
         public void SignalRCore(string name)
         {
             CommonNuGet.Run("Microsoft.AspNet.SignalR.Core.cs", name);
-        }        
+        }
 
         [Theory]
-        [PropertyData("Feeds")]
+        [MemberData(nameof(Feeds))]
         public void SignalRRedis(string name)
         {
             CommonNuGet.Run("Microsoft.AspNet.SignalR.Redis.cs", name);
         }
 
         [Theory]
-        [PropertyData("Feeds")]
+        [MemberData(nameof(Feeds))]
         public void SignalRServiceBus(string name)
         {
             CommonNuGet.Run("Microsoft.AspNet.SignalR.ServiceBus.cs", name);
         }
 
         [Theory]
-        [PropertyData("Feeds")]
+        [MemberData(nameof(Feeds))]
         public void SignalRSqlServer(string name)
         {
             CommonNuGet.Run("Microsoft.AspNet.SignalR.SqlServer.cs", name);
         }
 
         [Theory]
-        [PropertyData("Feeds")]
+        [MemberData(nameof(Feeds))]
         public void SignalRSystemWeb(string name)
         {
             CommonNuGet.Run("Microsoft.AspNet.SignalR.SystemWeb.cs", name);
