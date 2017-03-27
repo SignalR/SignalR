52e45d0f8e6b41f92568956bd1ec5ef69ce7da88
moozzyk <pawelka@microsoft.com>
2017-03-27 09:42:58 -0700

    Updating Xunit version in functional tests

--- tests/Microsoft.AspNet.SignalR.FunctionalTests/Properties/AssemblyInfo.cs
+++ tests/Microsoft.AspNet.SignalR.FunctionalTests/Properties/AssemblyInfo.cs
@@ -4,6 +4,7 @@
 using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
+using Xunit;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
@@ -37,3 +38,5 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+
+[assembly: CollectionBehavior(DisableTestParallelization = true)]
