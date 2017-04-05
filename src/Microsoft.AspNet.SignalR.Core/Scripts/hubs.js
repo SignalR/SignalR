b4491daaf8e61a1bd5a1ea894f0e7cbbd7d3e323
moozzyk <pawelka@microsoft.com>
2017-04-05 13:45:59 -0700

    Update version to 2.2.2-preview1

--- src/Microsoft.AspNet.SignalR.Core/Scripts/hubs.js
+++ src/Microsoft.AspNet.SignalR.Core/Scripts/hubs.js
@@ -1,5 +1,5 @@
 /*!
- * ASP.NET SignalR JavaScript Library v2.2.1-pre
+ * ASP.NET SignalR JavaScript Library v2.2.2-preview1
  * http://signalr.net/
  *
  * Copyright (c) .NET Foundation. All rights reserved.
