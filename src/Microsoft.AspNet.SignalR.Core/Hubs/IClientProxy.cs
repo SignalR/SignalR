ef0847be19f04c2906eca6370f1f73860e3d952f
Bert Temme <btemme@hotmail.com>
2018-03-04 21:45:11 +0100

    Update IClientProxy.cs
    fix typo

--- src/Microsoft.AspNet.SignalR.Core/Hubs/IClientProxy.cs
+++ src/Microsoft.AspNet.SignalR.Core/Hubs/IClientProxy.cs
@@ -14,7 +14,7 @@ namespace Microsoft.AspNet.SignalR.Hubs
         /// Invokes a method on the connection(s) represented by the <see cref="IClientProxy"/> instance.
         /// </summary>
         /// <param name="method">name of the method to invoke</param>
-        /// <param name="args">argumetns to pass to the client</param>
+        /// <param name="args">arguments to pass to the client</param>
         /// <returns>A task that represents when the data has been sent to the client.</returns>
         Task Invoke(string method, params object[] args);
     }
