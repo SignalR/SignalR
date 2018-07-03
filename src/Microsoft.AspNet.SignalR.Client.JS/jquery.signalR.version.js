b9eff191ff5eb6e19dd76909f961f161afb7b1a3
Andrew Stanton-Nurse <andrew@andrewnurse.net>
2018-07-03 11:16:16 -0700

    netstandard client

--- src/Microsoft.AspNet.SignalR.Client.JS/jquery.signalR.version.js
+++
@@ -1,9 +0,0 @@
-// Copyright (c) .NET Foundation. All rights reserved.
-// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
-
-
-/*global window:false */
-/// <reference path="jquery.signalR.core.js" />
-(function ($, undefined) {
-    $.signalR.version = "2.3.0-rtm";
-}(window.jQuery));
--- src/Microsoft.AspNet.SignalR.Client.NetStandard/Properties/AssemblyInfo.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Resources;
-using System.Reflection;
-
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("Microsoft.AspNet.SignalR.Client.NetStandard")]
-[assembly: AssemblyDescription("")]
