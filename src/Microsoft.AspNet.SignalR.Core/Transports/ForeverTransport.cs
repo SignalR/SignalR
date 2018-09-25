99e2c4e94a78344286ab73099d2bf8de275f01b6
Andrew Stanton-Nurse <andrew@stanton-nurse.com>
2018-09-25 16:15:59 -0700

    fix build failure

--- src/Microsoft.AspNet.SignalR.Core/Transports/ForeverTransport.cs
+++ src/Microsoft.AspNet.SignalR.Core/Transports/ForeverTransport.cs
@@ -98,7 +98,7 @@ namespace Microsoft.AspNet.SignalR.Transports
 
         protected Task ProcessRequestCore(ITransportConnection connection)
         {
-            Trace.TraceVerbose($"ProcessRequestCore(Connection: {ConnectionId})");
+            Trace?.TraceVerbose($"ProcessRequestCore(Connection: {ConnectionId})");
             Connection = connection;
 
             if (IsSendRequest)
@@ -107,7 +107,7 @@ namespace Microsoft.AspNet.SignalR.Transports
             }
             else if (IsAbortRequest)
             {
-                Trace.TraceVerbose($"ProcessAbortRequest(Connection: {ConnectionId})");
+                Trace?.TraceVerbose($"ProcessAbortRequest(Connection: {ConnectionId})");
                 return Connection.Abort(ConnectionId);
             }
             else
@@ -146,7 +146,7 @@ namespace Microsoft.AspNet.SignalR.Transports
 
         protected virtual async Task ProcessSendRequest()
         {
-            Trace.TraceVerbose($"ProcessSendRequest(Connection: {ConnectionId})");
+            Trace?.TraceVerbose($"ProcessSendRequest(Connection: {ConnectionId})");
             INameValueCollection form = await Context.Request.ReadForm().PreserveCulture();
             string data = form["data"];
 
@@ -159,7 +159,7 @@ namespace Microsoft.AspNet.SignalR.Transports
         [SuppressMessage("Microsoft.Design", "CA1031:DoNotCatchGeneralExceptionTypes", Justification = "Exceptions are flowed to the caller.")]
         private Task ProcessReceiveRequest(ITransportConnection connection)
         {
-            Trace.TraceVerbose($"ProcessReceiveRequest(Connection: {ConnectionId})");
+            Trace?.TraceVerbose($"ProcessReceiveRequest(Connection: {ConnectionId})");
             Func<Task> initialize = null;
 
             // If this transport isn't replacing an existing transport, oldConnection will be null.
