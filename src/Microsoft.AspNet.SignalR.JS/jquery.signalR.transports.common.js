48f50f3be8f53fad7cd7e8066f9c7bf9fb0ed777
Stephen Halter <halter73@gmail.com>
2022-01-06 15:13:33 -0800

    Update jquery.signalR.transports.common.js

--- src/Microsoft.AspNet.SignalR.JS/jquery.signalR.transports.common.js
+++ src/Microsoft.AspNet.SignalR.JS/jquery.signalR.transports.common.js
@@ -457,7 +457,7 @@
                     method: "POST",
                     keepalive: true,
                     headers: requestHeaders,
-                    credentials: "include"
+                    credentials: connection.withCredentials === true ? "include" : "same-origin",
                 });
             }
             else { 
