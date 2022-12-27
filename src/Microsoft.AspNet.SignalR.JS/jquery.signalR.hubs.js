760b529c9c0480888190774d4a12b5a22784be2d
Oleksandr Tolstikov <Oleksandr.Tolstikov@selerant.com>
2022-12-27 15:54:19 +0200

    Fix hub.off function.

--- src/Microsoft.AspNet.SignalR.JS/jquery.signalR.hubs.js
+++ src/Microsoft.AspNet.SignalR.JS/jquery.signalR.hubs.js
@@ -183,6 +183,8 @@
                         if (callbackSpace[i].guid === callbackIdentity._signalRGuid || (isFromOldGeneratedHubProxy && callbackSpace[i].isFromOldGeneratedHubProxy)) {
                             callbackIndex = i;
                             callbackRegistration = callbackSpace[i];
+
+                            break;
                         }
                     }
 
@@ -194,7 +196,7 @@
                         }
 
                         // Remove the registration from the list
-                        callbackSpace.splice(i, 1);
+                        callbackSpace.splice(callbackIndex, 1);
 
                         // Check if there are any registrations left, if not we need to destroy it.
                         if (callbackSpace.length === 0) {
